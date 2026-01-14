6e25377053fa3618f697e17252abfd663cd28dca
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-13 21:56:33 -0500

    Set up an entry point into terrain generation

--- mods/terrain/init.lua
+++ mods/terrain/init.lua
@@ -1,1 +1,12 @@
 -- Terrain generation.
+
+--- @type number
+local chunk_size = core.get_mapgen_chunksize()
+
+--- This is the terrain generation entry point.
+---@param minp number
+---@param maxp number
+---@param blockseed number
+core.register_on_generated(function(minp, maxp, blockseed)
+	print(minp, maxp, blockseed)
+end)
