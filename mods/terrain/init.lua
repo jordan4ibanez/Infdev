2591fc72ebaa3b06c8be02311cf420d560638de8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-15 20:05:33 -0500

    Fix singlenode lighting

--- mods/terrain/init.lua
+++ mods/terrain/init.lua
@@ -1,5 +1,7 @@
 -- Terrain generation entry point.
 
+core.set_mapgen_setting("mg_flags", "nolight", true)
+
 core.register_mapgen_script(
 	core.get_modpath(core.get_current_modname()) .. "/mapgen.lua"
 )
