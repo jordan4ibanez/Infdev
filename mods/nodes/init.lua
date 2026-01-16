8dc8c00af1e75625e2783507f89cb08ea6af86dc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-15 19:34:11 -0500

    Add water

--- mods/nodes/init.lua
+++ mods/nodes/init.lua
@@ -28,3 +28,99 @@ infdev.register_node("grass", {
 		}
 	},
 })
+
+infdev.register_node("water_source", {
+	description = "Water Source",
+	drawtype = "liquid",
+	waving = 3,
+	tiles = {
+		{
+			name = "default_water_source_animated.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+		{
+			name = "default_water_source_animated.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 1,
+	liquidtype = "source",
+	liquid_alternative_flowing = "infdev:water_flowing",
+	liquid_alternative_source = "infdev:water_source",
+	liquid_viscosity = 1,
+	post_effect_color = { a = 103, r = 30, g = 60, b = 90 },
+	groups = { water = 3, liquid = 3, cools_lava = 1 },
+	-- sounds = default.node_sound_water_defaults(),
+})
+
+
+infdev.register_node("water_flowing", {
+	description = "Flowing Water",
+	drawtype = "flowingliquid",
+	waving = 3,
+	tiles = { "default_water.png" },
+	special_tiles = {
+		{
+			name = "default_water_flowing_animated.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 0.5,
+			},
+		},
+		{
+			name = "default_water_flowing_animated.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 0.5,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	paramtype2 = "flowingliquid",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 1,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "infdev:water_flowing",
+	liquid_alternative_source = "infdev:water_source",
+	liquid_viscosity = 1,
+	post_effect_color = { a = 103, r = 30, g = 60, b = 90 },
+	groups = {
+		water = 3,
+		liquid = 3,
+		not_in_creative_inventory = 1,
+		cools_lava = 1
+	},
+	-- sounds = default.node_sound_water_defaults(),
+})
