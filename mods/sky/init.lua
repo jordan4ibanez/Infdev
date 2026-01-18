fd30fda246ec405d51c56e57c7b12e3b7eb59d42
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-18 10:26:20 -0500

    Raise cloud height

--- mods/sky/init.lua
+++ mods/sky/init.lua
@@ -1,2 +1,9 @@
 -- Sky control.
 
+core.register_on_joinplayer(function(player, last_login)
+	local cloud_settings = player:get_clouds()
+
+	cloud_settings.height = 128
+
+	player:set_clouds(cloud_settings)
+end)
