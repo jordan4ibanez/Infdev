59a1e6697b2b3725fab76b688325ce93afa8d063
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-18 18:48:16 -0500

    Time to generate some tools

--- mods/items/init.lua
+++ mods/items/init.lua
@@ -1,2 +1,17 @@
 -- The items in the game.
 
+-- Generation of pickaxe, shovel, axe, sword, hoe, shears, and paxel.
+
+local item_material = {
+	"wood",
+	"coal",
+	"stone",
+	"iron",
+	"gold",
+	"diamond",
+	"ruby",
+	"sapphire",
+	"emerald",
+	"lapis",
+	"moonstone"
+}
