00e230b73ddfd023948ea20692c4f2b8750bd8a7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-19 14:07:29 -0500

    Make ores drop correct items

--- mods/nodes/ores.lua
+++ mods/nodes/ores.lua
@@ -1,6 +1,7 @@
 infdev.register_node("coal_ore", {
 	tiles = { "default_stone.png^default_mineral_coal.png" },
-	groups = { [infdev.groups.stone] = 2 }
+	groups = { [infdev.groups.stone] = 2 },
+	drop = "infdev:coal"
 })
 
 infdev.register_node("iron_ore", {
@@ -15,30 +16,36 @@ infdev.register_node("gold_ore", {
 
 infdev.register_node("diamond_ore", {
 	tiles = { "default_stone.png^default_mineral_diamond.png" },
-	groups = { [infdev.groups.stone] = 4 }
+	groups = { [infdev.groups.stone] = 4 },
+	drop = "infdev:diamond"
 })
 
 infdev.register_node("ruby_ore", {
 	tiles = { "default_stone.png^(default_mineral_diamond.png^[colorize:red:190)" },
-	groups = { [infdev.groups.stone] = 5 }
+	groups = { [infdev.groups.stone] = 5 },
+	drop = "infdev:ruby"
 })
 
 infdev.register_node("sapphire_ore", {
 	tiles = { "default_stone.png^(default_mineral_diamond.png^[colorize:blue:190)" },
-	groups = { [infdev.groups.stone] = 6 }
+	groups = { [infdev.groups.stone] = 6 },
+	drop = "infdev:sapphire"
 })
 
 infdev.register_node("emerald_ore", {
 	tiles = { "default_stone.png^(default_mineral_diamond.png^[colorize:lime:190)" },
-	groups = { [infdev.groups.stone] = 7 }
+	groups = { [infdev.groups.stone] = 7 },
+	drop = "infdev:emerald"
 })
 
 infdev.register_node("lapis_ore", {
 	tiles = { "default_stone.png^((default_mineral_coal.png^[invert:rgb^[contrast:100:-70)^[colorize:darkblue:150)" },
-	groups = { [infdev.groups.stone] = 8 }
+	groups = { [infdev.groups.stone] = 8 },
+	drop = "infdev:lapis"
 })
 
 infdev.register_node("moonstone_ore", {
 	tiles = { "default_stone.png^(default_mineral_iron.png^[colorize:lightblue:200)" },
-	groups = { [infdev.groups.stone] = 9 }
+	groups = { [infdev.groups.stone] = 9 },
+	drop = "infdev:moonstone"
 })
