796b1cc6378eabf89621ddd6fc02033632653a37
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-18 19:52:54 -0500

    Create the root color scheme to match the material

--- mods/items/init.lua
+++ mods/items/init.lua
@@ -4,37 +4,37 @@
 
 local __item_material = {
 	wood = {
-		color_mod = ""
+		color_mod = "#966919"
 	},
 	coal = {
-		color_mod = ""
+		color_mod = "#000000"
 	},
 	stone = {
-		color_mod = ""
+		color_mod = "#505050"
 	},
 	iron = {
-		color_mod = ""
+		color_mod = "#D0D0D0"
 	},
 	gold = {
-		color_mod = ""
+		color_mod = "#EFBF04"
 	},
 	diamond = {
-		color_mod = ""
+		color_mod = "#00BFFF"
 	},
 	ruby = {
-		color_mod = ""
+		color_mod = "#FF0000"
 	},
 	sapphire = {
-		color_mod = ""
+		color_mod = "#0000FF"
 	},
 	emerald = {
-		color_mod = ""
+		color_mod = "#00FF00"
 	},
 	lapis = {
-		color_mod = ""
+		color_mod = "#00008B"
 	},
 	moonstone = {
-		color_mod = ""
+		color_mod = "#ADD8E6"
 	}
 }
 
@@ -43,7 +43,8 @@ for material, definition in pairs(__item_material) do
 	level = level + 1
 
 	core.register_tool(":infdev:" .. material .. "_axe", {
-		inventory_image = "default_tool_" .. material .. "pick.png",
+		inventory_image = "default_tool_axe_head.png^[colorize:" ..
+			definition.color_mod .. ":200^default_tool_axe_handle.png",
 		tool_capabilities = {
 			full_punch_interval = 1.2,
 			max_drop_level = 0,
