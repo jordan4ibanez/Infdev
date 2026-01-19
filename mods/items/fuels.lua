2b812c5390cc365749db6a25ffb1db4b126e4beb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-19 15:39:17 -0500

    Create a way to easily register fuels

--- mods/items/fuels.lua
+++ mods/items/fuels.lua
@@ -1,1 +1,27 @@
 -- All fuels are registered here.
+
+infdev = infdev or {}
+
+local string_group = infdev.string_group
+
+---Simplify fuel registration.
+---@param item string
+---@param burn_time number
+function infdev.register_fuel(item, burn_time)
+	local new_item = item
+
+	if (new_item:sub(1, string.len("group:")) ~= "group:") then
+		new_item = "infdev:" .. new_item
+	end
+
+	core.register_craft({
+		type = "fuel",
+		recipe = new_item,
+		burntime = burn_time,
+	})
+end
+
+infdev.register_fuel("coal", 40)
+
+infdev.register_fuel(string_group(infdev.groups.tree), 30)
+infdev.register_fuel(string_group(infdev.groups.planks), 20)
