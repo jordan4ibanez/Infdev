3973b97200e3c7d2422c35b2d72338c08646d788
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-19 13:12:17 -0500

    Add coal and use new registration system

--- mods/items/init.lua
+++ mods/items/init.lua
@@ -7,12 +7,19 @@ local mod_path = core.get_modpath(core.get_current_modname())
 dofile(mod_path .. "/tools.lua")
 dofile(mod_path .. "/crafting.lua")
 
-core.register_craftitem(":infdev:stick", {
 function infdev.register_craftitem(name, def)
 	name = ":infdev:" .. name
 	core.register_craftitem(name, def)
 end
+
+infdev.register_craftitem("stick", {
 	description = "Stick",
 	inventory_image = "default_stick.png",
 	groups = { stick = 1, flammable = 2 },
+})
+
+infdev.register_craftitem("coal", {
+	description = "Coal",
+	inventory_image = "default_coal_lump.png",
+	groups = { coal = 1, flammable = 1 }
 })
