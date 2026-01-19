36fc8f982f8eb7b76f793bac9dcaf34fc9ef69bc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-18 19:19:11 -0500

    Create a basic unfunctioning hackjob to test tools

--- mods/items/init.lua
+++ mods/items/init.lua
@@ -2,7 +2,7 @@
 
 -- Generation of pickaxe, shovel, axe, sword, hoe, shears, and paxel.
 
-local item_material = {
+local __item_material = {
 	"wood",
 	"coal",
 	"stone",
@@ -15,3 +15,19 @@ local item_material = {
 	"lapis",
 	"moonstone"
 }
+
+for _, material in ipairs(__item_material) do
+	core.register_tool(":infdev:" .. material .. "_axe", {
+		inventory_image = "default_tool_" .. material .. "pick.png",
+		tool_capabilities = {
+			full_punch_interval = 1.2,
+			max_drop_level = 0,
+			groupcaps = {
+				cracky = { times = { [3] = 1.60 }, uses = 10, maxlevel = 1 },
+			},
+			damage_groups = { fleshy = 2 },
+		},
+		sound = { breaks = "default_tool_breaks" },
+		groups = { pickaxe = 1, flammable = 2 }
+	})
+end
