31a49b0b9e2c48f47066f74c9b58dc4c08bb2492
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-18 22:44:31 -0500

    Add wood crafting basics

---
+++ mods/items/crafting.lua
@@ -0,0 +1,23 @@
+-- Craft recipes are stored here.
+
+core.register_craft({
+	output = "infdev:oak_planks 4",
+	recipe = {
+		{ "infdev:oak_tree" },
+	}
+})
+
+core.register_craft({
+	output = "infdev:birch_planks 4",
+	recipe = {
+		{ "infdev:birch_tree" },
+	}
+})
+
+core.register_craft({
+	output = "infdev:stick 4",
+	recipe = {
+		{ "group:" .. infdev.groups.planks },
+		{ "group:" .. infdev.groups.planks },
+	}
+})
--- mods/items/init.lua
+++ mods/items/init.lua
@@ -5,6 +5,7 @@ infdev = infdev or {}
 local mod_path = core.get_modpath(core.get_current_modname())
 
 dofile(mod_path .. "/tools.lua")
+dofile(mod_path .. "/crafting.lua")
 
 core.register_craftitem(":infdev:stick", {
 	description = "Stick",
