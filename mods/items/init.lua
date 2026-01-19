f2b04fc95d85e10ca36d78949395c9b6e1b6fcdd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-18 20:23:11 -0500

    Create a nice tech tree

--- mods/items/init.lua
+++ mods/items/init.lua
@@ -1,59 +1,98 @@
 -- The items in the game.
 
+infdev = infdev or {}
+
 -- Generation of pickaxe, shovel, axe, sword, hoe, shears, and paxel.
 
 local __item_material = {
-	wood = {
-		color_mod = "#966919"
+	{
+		name = "wood",
+		color_mod = "#966919",
+		level = 1,
 	},
-	coal = {
-		color_mod = "#000000"
+	{
+		name = "coal",
+		color_mod = "#000000",
+		level = 2,
 	},
-	stone = {
-		color_mod = "#505050"
+	{
+		name = "stone",
+		color_mod = "#505050",
+		level = 2,
 	},
-	iron = {
-		color_mod = "#D0D0D0"
+	{
+		name = "gold",
+		color_mod = "#EFBF04",
+		uses = 64,
+		level = 3,
 	},
-	gold = {
-		color_mod = "#EFBF04"
+	{
+		name = "iron",
+		color_mod = "#D0D0D0",
+		level = 4,
 	},
-	diamond = {
-		color_mod = "#00BFFF"
+	{
+		name = "diamond",
+		color_mod = "#00BFFF",
+		level = 5,
 	},
-	ruby = {
-		color_mod = "#FF0000"
+	{
+		name = "ruby",
+		color_mod = "#FF0000",
+		level = 6,
 	},
-	sapphire = {
-		color_mod = "#0000FF"
+	{
+		name = "sapphire",
+		color_mod = "#0000FF",
+		level = 7,
 	},
-	emerald = {
-		color_mod = "#00FF00"
+	{
+		name = "emerald",
+		color_mod = "#00FF00",
+		level = 8,
 	},
-	lapis = {
-		color_mod = "#00008B"
+	{
+		name = "lapis",
+		color_mod = "#00008B",
+		level = 9,
 	},
-	moonstone = {
-		color_mod = "#ADD8E6"
+	{
+		name = "moonstone",
+		color_mod = "#ADD8E6",
+		level = 10,
 	}
 }
 
-local level = 0
-for material, definition in pairs(__item_material) do
-	level = level + 1
+local level = 1
+local uses = 64
+
+-- Maxes out at the highest tool tier.
+infdev.level_max = 10
+
+
+for _, definition in ipairs(__item_material) do
+	local material = definition.material
+
+	print(material)
 
 	core.register_tool(":infdev:" .. material .. "_axe", {
 		inventory_image = "default_tool_axe_head.png^[colorize:" ..
 			definition.color_mod .. ":200^default_tool_axe_handle.png",
 		tool_capabilities = {
-			full_punch_interval = 1.2,
-			max_drop_level = 0,
+			max_drop_level = level,
 			groupcaps = {
-				cracky = { times = { [3] = 1.60 }, uses = 10, maxlevel = 1 },
+				cracky = {
+					times = { [3] = 1.60 },
+					uses = definition.uses or uses,
+					maxlevel = 100
+				},
 			},
 			damage_groups = { fleshy = 2 },
 		},
 		sound = { breaks = "default_tool_breaks" },
 		groups = { pickaxe = 1, flammable = 2 }
 	})
+
+	level = level + 1
+	uses = uses * 2
 end
