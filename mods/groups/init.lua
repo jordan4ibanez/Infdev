76552b05694e2f57927b52f72ec1027ae3469c2f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-19 15:18:11 -0500

    Add ease of use function

--- mods/groups/init.lua
+++ mods/groups/init.lua
@@ -16,3 +16,13 @@ infdev.groups = {
 	liquid_flow = "liquid_flow",
 	water = "water"
 }
+
+---Turns an infdev.group into "group:whatever"
+---@param input_group string
+---@return string
+function infdev.group_string(input_group)
+	if (infdev.groups[input_group] == nil) then
+		error("Group " .. input_group .. " does not exist.")
+	end
+	return "group:" .. input_group
+end
