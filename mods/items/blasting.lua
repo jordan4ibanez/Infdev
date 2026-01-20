b8571a898147612c44240518b03e463e2260ab0c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-19 19:44:33 -0500

    Add some basic safety checking

--- mods/items/blasting.lua
+++ mods/items/blasting.lua
@@ -1,1 +1,14 @@
 -- Blast furnace upgrade recipes.
+
+infdev = infdev or {}
+
+local blast_recipes = {}
+
+---Register a tool upgrade in the blast furnace.
+---@param input string
+---@param output string
+function infdev.register_blast(input, output)
+	if (blast_recipes[input] == output) then
+		error("Error: Tried to overwrite blast recipe " .. input .. " to " .. output)
+	end
+end
