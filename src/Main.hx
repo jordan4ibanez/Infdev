063a337516282d99e384dbb300533126f5d3f893
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-05-30 02:51:22 -0400

    Doing some extreme hacking here

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,23 @@
 import luanti_types.Core;
+import Reflect;
+
+abstract class Entity {
+	@:native("on_activate")
+	function onActivate(self:Entity) {
+		trace("hello from entity!");
+	}
+}
+
+// class Mob extends Entity {
+// 	override function onActivate(self:Entity) {
+// 		trace("I am overloaded");
+// 	}
+// }
 
 class Main {
 	public static function main() {
-		Core.log(error, "testing");
+		// Core.log(error, "testing");
+
+		// Core.registerEntity("haxe_luanti:test", Mob);
 	};
 }
