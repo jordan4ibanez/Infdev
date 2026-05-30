ef6a1b5c419c13a11fe38765068ec7722a71d6ee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-05-30 13:35:10 -0400

    Add some duct tape for minimal ui interaction

--- luanti_types/Core.hx
+++ luanti_types/Core.hx
@@ -7,6 +7,15 @@ import luanti_types.LogLevel;
 extern class Core {
 	static function log(level:LogLevel, text:String):Void;
 
+	// fixme: this is incorrect.
 	@:native("register_entity")
 	static function registerEntity(name:String, prototype:Dynamic):Void;
+
+	// fixme: this is incorrect.
+	@:native("request_shutdown")
+	static function requestShutdown(?message:String, ?reconnect:Bool, ?delay:Float):Void;
+
+	// fixme: this is incorrect.
+	@:native("register_on_joinplayer")
+	static function registerOnJoinPlayer(delegate:() -> Void):Void;
 }
