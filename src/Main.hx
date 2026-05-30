06436c62b06640d8335712fa1e40c745e0481e63
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-05-30 13:42:48 -0400

    Implement a vec3

--- src/Main.hx
+++ src/Main.hx
@@ -3,6 +3,15 @@ import luanti_types.Core;
 import Reflect;
 import Type;
 
+class Vec3 {
+	var x:Float;
+	var y:Float;
+	var z:Float;
+
+	public function new() {
+		x = Math.random();
+		y = Math.random();
+		z = Math.random();
 	}
 }
 
