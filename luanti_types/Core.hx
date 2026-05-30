4be7e6f53502e69d23495d6aad1b61429bc41182
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-05-30 02:29:57 -0400

    I like this

--- luanti_types/Core.hx
+++ luanti_types/Core.hx
@@ -6,4 +6,7 @@ import luanti_types.LogLevel;
 @:native("core")
 extern class Core {
 	static function log(level:LogLevel, text:String):Void;
+
+	@:native("register_entity")
+	static function registerEntity(name:String, prototype:Dynamic):Void;
 }
