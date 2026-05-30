292fc0da1e44234464827cdd0403e586df9d267d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-05-30 13:41:32 -0400

    Clear

--- src/Main.hx
+++ src/Main.hx
@@ -3,23 +3,12 @@ import luanti_types.Core;
 import Reflect;
 import Type;
 
-abstract class Entity {
-	@:native("on_activate")
-	function onActivate(self:Entity) {
-		trace("hello from entity!");
 	}
 }
 
-// class Mob extends Entity {
-// 	override function onActivate(self:Entity) {
-// 		trace("I am overloaded");
-// 	}
-// }
 
 class Main {
 	public static function main() {
-		// Core.log(error, "testing");
 
-		// Core.registerEntity("haxe_luanti:test", Mob);
 	};
 }
