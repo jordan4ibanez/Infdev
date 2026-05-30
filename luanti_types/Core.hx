58642356f2b3ae48c3d8fdcba08344e7f6844eb8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-05-30 18:04:51 -0400

    spaces

--- luanti_types/Core.hx
+++ luanti_types/Core.hx
@@ -5,17 +5,17 @@ import luanti_types.LogLevel;
 
 @:native("core")
 extern class Core {
-	static function log(level:LogLevel, text:String):Void;
+	static function log(level: LogLevel, text: String): Void;
 
 	// fixme: this is incorrect.
 	@:native("register_entity")
-	static function registerEntity(name:String, prototype:Dynamic):Void;
+	static function registerEntity(name: String, prototype: Dynamic): Void;
 
 	// fixme: this is incorrect.
 	@:native("request_shutdown")
-	static function requestShutdown(?message:String, ?reconnect:Bool, ?delay:Float):Void;
+	static function requestShutdown(?message: String, ?reconnect: Bool, ?delay: Float): Void;
 
 	// fixme: this is incorrect.
 	@:native("register_on_joinplayer")
-	static function registerOnJoinPlayer(delegate:() -> Void):Void;
+	static function registerOnJoinPlayer(delegate: () -> Void): Void;
 }
