6eeecfbf5cc19d61e422e76a6ab0727c65740ed1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-05-31 00:40:54 -0400

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -42,55 +42,54 @@ abstract class Entity {
 		var currentClass = classType;
 
 		while (currentClass != null) {
+			// ? Static.
 
-		// ? Static.
+			var staticFields = Reflect.fields(classType);
 
-		var staticFields = Reflect.fields(classType);
-
-		for (field in staticFields) {
-			trace("static", field);
-			switch (field) {
-				case "new":
-					{
-						// ? Note: This is manually hardwiring in the constructor into on_activate.
-						// Manually inject the new method into Luanti style.
-						var constructorFunc: Dynamic = Reflect.field(input, "new");
-						if (constructorFunc == null) {
-							throw "Logic error. New missing from Entity derived class.";
+			for (field in staticFields) {
+				trace("static", field);
+				switch (field) {
+					case "new":
+						{
+							// ? Note: This is manually hardwiring in the constructor into on_activate.
+							// Manually inject the new method into Luanti style.
+							var constructorFunc: Dynamic = Reflect.field(input, "new");
+							if (constructorFunc == null) {
+								throw "Logic error. New missing from Entity derived class.";
+							}
+							luantiTable.on_activate = constructorFunc;
+							// ? End hardwire.
 						}
-						luantiTable.on_activate = constructorFunc;
-						// ? End hardwire.
-					}
-				default:
-					{
-						// Don't overwrite child overrides. (Probably not needed for static. [But this is very complex so I'm not taking chances.])
-						if (Reflect.hasField(luantiTable, field)) {
-							trace("Warning: Skipping static class data", field);
-							continue;
+					default:
+						{
+							// Don't overwrite child overrides. (Probably not needed for static. [But this is very complex so I'm not taking chances.])
+							if (Reflect.hasField(luantiTable, field)) {
+								trace("Warning: Skipping static class data", field);
+								continue;
+							}
+							var dataValue = Reflect.field(input, field);
+							// todo: do stuff with it (if needed)
+							Reflect.setField(luantiTable, field, dataValue);
 						}
-						var dataValue = Reflect.field(input, field);
-						// todo: do stuff with it (if needed)
-						Reflect.setField(luantiTable, field, dataValue);
-					}
+				}
 			}
-		}
 
-		// ? Instance.
-		var prototype: Dynamic = Reflect.field(classType, "prototype");
-		var instanceFields = Reflect.fields(prototype);
-		for (field in instanceFields) {
-			trace("instance", field);
-			// Don't overwrite child overrides.
-			if (Reflect.hasField(luantiTable, field)) {
-				trace("Warning: Skipping instance class data", field);
-				continue;
+			// ? Instance.
+			var prototype: Dynamic = Reflect.field(classType, "prototype");
+			var instanceFields = Reflect.fields(prototype);
+			for (field in instanceFields) {
+				trace("instance", field);
+				// Don't overwrite child overrides.
+				if (Reflect.hasField(luantiTable, field)) {
+					trace("Warning: Skipping instance class data", field);
+					continue;
+				}
+				var instanceDataValue = Reflect.field(prototype, field);
+				// ? Only map functions. Everything else is null.
+				if (Reflect.isFunction(instanceDataValue)) {
+					Reflect.setField(luantiTable, field, instanceDataValue);
+				}
 			}
-			var instanceDataValue = Reflect.field(prototype, field);
-			// ? Only map functions. Everything else is null.
-			if (Reflect.isFunction(instanceDataValue)) {
-				Reflect.setField(luantiTable, field, instanceDataValue);
-			}
-		}
 		}
 
 		return luantiTable;
