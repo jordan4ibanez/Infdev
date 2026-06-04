b8dcdcba0a790959c050d303d5c111111a294b05
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-04 01:46:10 -0400

    Move this back

--- luanti_types/Core.hx
+++ luanti_types/Core.hx
@@ -13,6 +13,31 @@ extern class Core {
 	// This is the real function.
 	public static extern function register_entity(name: String, prototype: Dynamic): Void;
 
+	// This is the hijacked function.
+	static public inline function registerEntity(name: String, clazz: Class<Entity>): Void {
+		var rawLuantiPrototype: Dynamic = {}
+		// ? Works from the current class backwards until reached root (Entity).
+		var currentClass: Class<Dynamic> = clazz;
+		while (currentClass != null) {
+			// trace("in class: " + Type.getClassName(currentClass));
+			// Class components.
+			var prototype = Reflect.field(currentClass, "prototype");
+			for (method in Reflect.fields(prototype)) {
+				untyped {
+					if (rawLuantiPrototype[method] != null) {
+						// trace("skipping method " + method + " already has it from child class");
+						continue;
+					}
+					rawLuantiPrototype[method] = Reflect.getProperty(prototype, method);
+				}
+				// trace(method);
+			}
+			// Move up the inheritance tree.
+			currentClass = Type.getSuperClass(currentClass);
+		}
+		Core.register_entity(name, rawLuantiPrototype);
+	}
+
 	// fixme: this is incorrect.
 	@:native("request_shutdown")
 	static function requestShutdown(?message: String, ?reconnect: Bool, ?delay: Float): Void;
