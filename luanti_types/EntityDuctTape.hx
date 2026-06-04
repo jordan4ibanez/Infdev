4c296ed3352753ce163a0823013316c2a73ff8ee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-04 01:03:37 -0400

    Now DRY it out

--- luanti_types/EntityDuctTape.hx
+++ luanti_types/EntityDuctTape.hx
@@ -51,33 +51,21 @@ class EntityDuctTape {
 				newFunction = macro function(staticData: String, dtimeS: Float) {
 					trace("Generated fallback on_activate for " + $v{className});
 				};
-
-				switch (newFunction.expr) {
-					case EFunction(_, func):
-						fields.push({
-							name: "on_activate",
-							access: [APublic],
-							kind: FFun(func),
-							pos: Context.currentPos()
-						});
-					default:
-				}
 			} else {
 				newFunction = macro function(staticData: String, dtimeS: Float) {
 					trace("Generated fallback on_activate for " + $v{className});
 					super.on_activate(staticData, dtimeS); // Safely call Entity's on_activate
 				};
-
-				switch (newFunction.expr) {
-					case EFunction(_, func):
-						fields.push({
-							name: "on_activate",
-							access: [APublic, AOverride], // Mark as an override
-							kind: FFun(func),
-							pos: Context.currentPos()
-						});
-					default:
-				}
+			}
+			switch (newFunction.expr) {
+				case EFunction(_, func):
+					fields.push({
+						name: "on_activate",
+						access: access,
+						kind: FFun(func),
+						pos: Context.currentPos()
+					});
+				default:
 			}
 		}
 
