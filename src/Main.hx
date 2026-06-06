5f45c1e0b7edf54e82eaff07451611e29ac49d40
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-05 20:17:31 -0400

    Move this

--- src/Main.hx
+++ src/Main.hx
@@ -14,28 +14,7 @@ class Vec3 {
 	}
 }
 
-@:autoBuild(luanti_types.EntityDuctTape.build())
-@:build(luanti_types.EntityDuctTape.build())
-class Entity {
-	// This returns this.
-	public function new() {}
 
-	@:native("on_activate")
-	public function onActivate(staticData: String, dtimeS: Float) {
-		// trace(this.uuid);
-		Lua.print("hello world! from Entity");
-	}
-
-	@:native("on_deactivate")
-	public function onDeactivate(removal: Bool) {}
-
-	@:native("on_step")
-	public function onStep(delta: Float) {}
-
-	final public function getGUID(): String {
-		return untyped __lua__("self.object:get_guid()");
-	};
-}
 
 class Mob extends Entity {
 	override function onActivate(staticData: String, dtimeS: Float) {
--- src/entity/Entity.hx
+++ src/entity/Entity.hx
@@ -1,2 +1,24 @@
 package entity;
 
+@:autoBuild(luanti_types.EntityDuctTape.build())
+@:build(luanti_types.EntityDuctTape.build())
+class Entity {
+	// This returns this.
+	public function new() {}
+
+	@:native("on_activate")
+	public function onActivate(staticData: String, dtimeS: Float) {
+		// trace(this.uuid);
+		Lua.print("hello world! from Entity");
+	}
+
+	@:native("on_deactivate")
+	public function onDeactivate(removal: Bool) {}
+
+	@:native("on_step")
+	public function onStep(delta: Float) {}
+
+	final public function getGUID(): String {
+		return untyped __lua__("self.object:get_guid()");
+	};
+}
