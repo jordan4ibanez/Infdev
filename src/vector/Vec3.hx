bf2c5074f2bce0cb98b5af8a299734a868358116
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-07 14:14:58 -0400

    Update Vec3.hx

--- src/vector/Vec3.hx
+++ src/vector/Vec3.hx
@@ -1,9 +1,22 @@
 package vector;
 
-private class EngineVector3 {
-	var x: Float;
-	var y: Float;
-	var z: Float;
+/**
+ * This class technically doesn't exist.
+ */
+private abstract class EngineVector3 {
+	var x: Float = 0;
+	var y: Float = 0;
+	var z: Float = 0;
 }
 
-class Vec3 extends EngineVector3 {}
+class Vec3 extends EngineVector3 {
+	public function new(?x: Float, ?y: Float, ?z: Float) {
+		this.x = x ?? 0;
+		this.y = y ?? 0;
+		this.z = z ?? 0;
+	}
+
+	public function doThing() {
+		trace(x, y, z);
+	}
+}
