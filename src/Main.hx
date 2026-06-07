27ea2e106c677b055306bc324f541093ef7acf1b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-07 14:34:06 -0400

    Fix return type

--- src/Main.hx
+++ src/Main.hx
@@ -2,7 +2,7 @@ import entity.Mob;
 import luanti_types.Core;
 
 class Main {
-	public static function main() {
+	public static function main(): Void {
 		Core.registerEntity("haxe_luanti:test", Mob);
 
 		Core.registerOnJoinPlayer(() -> {
