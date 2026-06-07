d533d4433f27c5583debdb833a4ca6042e66915d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-07 00:09:05 -0400

    Awesome

--- src/entity/Entity.hx
+++ src/entity/Entity.hx
@@ -25,6 +25,26 @@ class Entity {
 	@:native("on_punch")
 	public function onPunch(puncher: Dynamic, timeFromLastPunch: Float, toolCapabilities: Dynamic, dir: Dynamic, damager: Int) {}
 
+	@:native("on_death")
+	public function onDeath(?killer: Dynamic) {}
+
+	@:native("on_rightclick")
+	public function onRightClick(clicker: Dynamic) {}
+
+	@:native("on_attach_child")
+	public function onAttachChild(child: Dynamic) {}
+
+	@:native("on_detach_child")
+	public function onDetachChild(child: Dynamic) {}
+
+	@:native("on_detach")
+	public function onDetach(parent: Dynamic) {}
+
+	@:native("get_staticdata")
+	public function getStaticData(): String {
+		return "";
+	}
+
 	final public function getGUID(): String {
 		return untyped __lua__("self.object:get_guid()");
 	};
