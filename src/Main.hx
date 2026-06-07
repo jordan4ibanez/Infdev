17173f90809b8b9ac472f0d552fbeb0bf6c98f30
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-07 14:05:19 -0400

    You served your purpose

--- src/Main.hx
+++ src/Main.hx
@@ -1,19 +1,6 @@
 import entity.Mob;
 import luanti_types.Core;
 
-class Vec3 {
-	public var x: Float;
-	public var y: Float;
-	public var z: Float;
-
-	public function new() {
-		// trace("I am a new vec3");
-		x = Math.random();
-		y = Math.random();
-		z = Math.random();
-	}
-}
-
 class Main {
 	public static function main() {
 		Core.registerEntity("haxe_luanti:test", Mob);
