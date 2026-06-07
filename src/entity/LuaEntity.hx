3c6aef708ae35b6bc1b7ede106c9658135f8ee1a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-07 00:39:37 -0400

    Trying to figure out what the fuck is going on with this oop mess

--- src/entity/LuaEntity.hx
+++ src/entity/LuaEntity.hx
@@ -1,5 +1,43 @@
 package entity;
 
+@:autoBuild(luanti_types.EntityDuctTape.build())
+@:build(luanti_types.EntityDuctTape.build())
 class LuaEntity extends ObjectRef {
-    
+	// ? Here begins programmer facing overrideable methods.
+	@:native("on_activate")
+	public function onActivate(staticData: String, dtimeS: Float) {
+		// trace(this.uuid);
+		// Lua.print("hello world! from ObjectRef");
+	}
+
+	@:native("on_deactivate")
+	public function onDeactivate(removal: Bool) {}
+
+	// todo: fix this dynamic mess.
+
+	@:native("on_step")
+	public function onStep(delta: Float, moveResult: Dynamic) {}
+
+	@:native("on_punch")
+	public function onPunch(puncher: Dynamic, timeFromLastPunch: Float, toolCapabilities: Dynamic, dir: Dynamic, damager: Int) {}
+
+	@:native("on_death")
+	public function onDeath(?killer: Dynamic) {}
+
+	@:native("on_rightclick")
+	public function onRightClick(clicker: Dynamic) {}
+
+	@:native("on_attach_child")
+	public function onAttachChild(child: Dynamic) {}
+
+	@:native("on_detach_child")
+	public function onDetachChild(child: Dynamic) {}
+
+	@:native("on_detach")
+	public function onDetach(parent: Dynamic) {}
+
+	@:native("get_staticdata")
+	public function getStaticData(): String {
+		return "";
+	}
 }
--- src/entity/ObjectRef.hx
+++ src/entity/ObjectRef.hx
@@ -2,52 +2,12 @@ package entity;
 
 import lua.Lua;
 
-@:autoBuild(luanti_types.EntityDuctTape.build())
-@:build(luanti_types.EntityDuctTape.build())
+// ? This is the base blueprint of LuaEntity and Player.
 class ObjectRef {
 	public final object: Dynamic = null;
 
 	// This returns this.
 	public function new() {}
-
-	// ? Here begins programmer facing overrideable methods.
-
-	@:native("on_activate")
-	public function onActivate(staticData: String, dtimeS: Float) {
-		// trace(this.uuid);
-		Lua.print("hello world! from ObjectRef");
-	}
-
-	@:native("on_deactivate")
-	public function onDeactivate(removal: Bool) {}
-
-	// todo: fix this dynamic mess.
-
-	@:native("on_step")
-	public function onStep(delta: Float, moveResult: Dynamic) {}
-
-	@:native("on_punch")
-	public function onPunch(puncher: Dynamic, timeFromLastPunch: Float, toolCapabilities: Dynamic, dir: Dynamic, damager: Int) {}
-
-	@:native("on_death")
-	public function onDeath(?killer: Dynamic) {}
-
-	@:native("on_rightclick")
-	public function onRightClick(clicker: Dynamic) {}
-
-	@:native("on_attach_child")
-	public function onAttachChild(child: Dynamic) {}
-
-	@:native("on_detach_child")
-	public function onDetachChild(child: Dynamic) {}
-
-	@:native("on_detach")
-	public function onDetach(parent: Dynamic) {}
-
-	@:native("get_staticdata")
-	public function getStaticData(): String {
-		return "";
-	}
 
 	// ? Begin baked in engine entity features made nicer to use.
 
