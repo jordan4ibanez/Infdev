758aace3793c6b79a5c1ec4e4aaf0050747abd5f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-07 00:10:16 -0400

    It's working woo

--- src/entity/Mob.hx
+++ src/entity/Mob.hx
@@ -3,9 +3,14 @@ package entity;
 class Mob extends Entity {
 	override function onActivate(staticData: String, dtimeS: Float) {
 		super.onActivate(staticData, dtimeS);
+		trace(staticData);
 	}
 
 	override function onStep(delta: Float) {
 		super.onStep(delta);
+	}
+
+	override function getStaticData(): String {
+		return "test 1234";
 	}
 }
