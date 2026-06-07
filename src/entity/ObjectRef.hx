de2821d9a1286ae23393912d1c2c5c91e5ff3c0a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-07 00:45:21 -0400

    Nice

--- src/entity/ObjectRef.hx
+++ src/entity/ObjectRef.hx
@@ -10,6 +10,11 @@ abstract class ObjectRef {
 	public function new() {}
 
 	// ? Begin baked in engine entity features made nicer to use.
+	//
+	// Plain lua object reference, methods:
+	//
+	//* begins: is_valid
+	//* ends: get_guid
 
 	final public function getGUID(): String {
 		return untyped __lua__("self.object:get_guid()");
