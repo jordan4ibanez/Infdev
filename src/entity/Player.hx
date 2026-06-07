fa1a6da783fa6716b379c838b29616fc32aa7ba2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-07 00:48:57 -0400

    Makes sense, easy enough!

---
+++ src/entity/Player.hx
@@ -0,0 +1,10 @@
+package entity;
+
+class Player extends ObjectRef {
+	// ? Begin baked in engine player entity features made nicer to use.
+	//
+	// Player object reference entity methods:
+	//
+	//* Begins: get_player_name
+	//* Ends: set_flags
+}
