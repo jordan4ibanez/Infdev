edd83fe68092f186df4b604ed565a661dbfbe78f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-07 23:06:40 -0400

    Create EngineVector3.hx

---
+++ src/vector/EngineVector3.hx
@@ -0,0 +1,14 @@
+package vector;
+
+/**
+ * This class technically doesn't exist.
+ */
+abstract class EngineVector3 {
+	public var x: Float = 0;
+	public var y: Float = 0;
+	public var z: Float = 0;
+
+	public static function toVec3(self: EngineVector3): Vec3 {
+		return new Vec3(self.x, self.y, self.z);
+	}
+}
