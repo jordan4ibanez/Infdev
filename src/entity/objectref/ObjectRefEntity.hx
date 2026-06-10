454eb6b279e6c7fc5f0d2bb03306af035fa80538
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-10 01:25:05 -0400

    That works

--- src/entity/objectref/ObjectRefEntity.hx
+++ src/entity/objectref/ObjectRefEntity.hx
@@ -4,6 +4,4 @@ package entity.objectref;
  * A Luanti C++ engine ServerActiveObject reference.
  * This is the form of it to cast into when you know it is an entity.
  */
-abstract class ObjectRefEntity extends ObjectRefBase {
-	private final function new() {}
-}
+abstract class ObjectRefEntity extends ObjectRefBase {}
