95a7fb7b5bcbab78d4e4e4c1b5c2c72ce8d9bdd7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-10 01:46:35 -0400

    Update ObjectRefEntity.hx

--- src/entity/objectref/ObjectRefEntity.hx
+++ src/entity/objectref/ObjectRefEntity.hx
@@ -24,5 +24,22 @@ abstract class ObjectRefEntity extends ObjectRefBase {
 
 	@:native("get_rotation")
 	public abstract function getRotation(): EngineVector3;
-	
+
+	@:native("set_yaw")
+	public abstract function setYaw(yaw: Float): Void;
+
+	@:native("get_yaw")
+	public abstract function getYaw(): Float;
+
+	@:native("set_texture_mod")
+	public abstract function setTextureMod(mod: Dynamic): Void;
+
+	@:native("get_texture_mod")
+	public abstract function getTextureMod(): Dynamic;
+
+	@:native("set_sprite")
+	public abstract function setSprite(startFrame: Dynamic, numFrames: Int, frameLength: Float, selectXByCamera: Bool): Void;
+
+	@:native("get_luaentity")
+	public abstract function getLuaEntity(): Null<Dynamic>;
 }
