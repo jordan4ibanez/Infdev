da594b02c2abaa5457e44d81101bb1b3eb9e5420
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-10 01:57:14 -0400

    Update ObjectRefPlayer.hx

--- src/entity/objectref/ObjectRefPlayer.hx
+++ src/entity/objectref/ObjectRefPlayer.hx
@@ -1,2 +1,7 @@
 package entity.objectref;
 
+/**
+ * A Luanti C++ engine ServerActiveObject reference.
+ * This is the form of it to cast into when you know it is a player.
+ */
+abstract class ObjectRefPlayer extends ObjectRefBase {}
