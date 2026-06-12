b33110f62e0a0497b94641e199f368a2dbfe8b8c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-12 00:56:56 -0400

    Clean this shithole up

--- src/entity/Mob.hx
+++ src/entity/Mob.hx
@@ -1,12 +1,11 @@
 package entity;
 
 import luantitypes.Macros;
-import lua.Lua;
 import luantitypes.Core;
 import vector.Vec3;
-import entity.objectref.ObjectRefBase;
 import entity.helpers.EntitySerialization;
 
+// This is how your class gets registered into the engine.
 @:luantiEntity("infdev:mob")
 abstract class Mob extends LuaEntity {
 	var helperVec3 = new Vec3();
@@ -20,20 +19,7 @@ abstract class Mob extends LuaEntity {
 	override function onStep(delta: Float, moveResult: Dynamic) {
 		super.onStep(delta, moveResult);
 
-		// helperVec3.doThing();
-
-		// var x = Core.getObjectsInsideRadius(this.object.getPos(), 5);
-
-		// for (obj in x) {
-		// 	Lua.print(obj);
-		// 	trace(obj.getPos(), obj.getGUID(), obj.isValid());
-		// }
-
 		this.object.rightClick(this.object);
-	}
-
-	override function onRightClick(clicker: ObjectRefBase) {
-		super.onRightClick(clicker);
 	}
 
 	override function getStaticData(): String {
