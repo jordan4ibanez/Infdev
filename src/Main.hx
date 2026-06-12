9cf81be8b70b5ebd46bfcf67136556bfc5d433e1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-11 23:57:39 -0400

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -5,7 +5,7 @@ import luantitypes.Core;
 class Main {
 	public static function main(): Void {
 		// TODO: USE __init__ WITH A MACRO TO CREATE A CUSTOM ENTITY REGISTRATION!
-		Core.registerEntity("haxe_luanti:test", Mob);
+		Core.registerEntity("infdev:test", Mob);
 
 		// Core.registerOnJoinPlayer((player, lastLogin) -> {
 		// untyped {
