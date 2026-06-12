866c90c222ab5bad3d9ca410ca2ef63604889cf4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-11 23:26:05 -0400

    WHOOPS

--- src/entity/helpers/PlayerHandling.hx
+++ src/entity/helpers/PlayerHandling.hx
@@ -21,6 +21,7 @@ final class PlayerHandling {
 			var ple = player.getLuaEntity();
 			ple.onDeactivate(false);
 			ModStorage.setString(player.getPlayerName() + PLAYER_DATA_KEY, ple.getStaticData());
+			// todo: delete this player from the map.
 		});
 
 		// Player LuaEntity destruction.
@@ -29,6 +30,7 @@ final class PlayerHandling {
 				var ple = player.getLuaEntity();
 				ple.onDeactivate(false);
 				ModStorage.setString(player.getPlayerName() + PLAYER_DATA_KEY, ple.getStaticData());
+				// todo: delete this player from the map. (overkill)
 			}
 		});
 
