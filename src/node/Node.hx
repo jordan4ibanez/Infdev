0b85c073d825aadd8c72a2c455ae22f5fb082945
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-13 00:03:53 -0400

    No logic here only weirdness, this is lisp now

--- src/node/Node.hx
+++ src/node/Node.hx
@@ -5,12 +5,15 @@ typedef NodeDef = {
 	function onStep(): Void;
 }
 
+@:autoBuild(luantitypes.NodeDuctTape.build())
+@:build(luantitypes.NodeDuctTape.build())
 abstract class Node {
 	// static function __init__() {
 	// 	var t: NodeDef = Dirt;
 	// }
 }
 
+@:registerNode("")
 class Dirt extends Node {
 	public static function onStep() {};
 }
