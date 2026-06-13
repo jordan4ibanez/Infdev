be467e981434aab0a8fad3781eb043c56c879fa2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-13 00:00:07 -0400

    Hmmm

--- src/node/Node.hx
+++ src/node/Node.hx
@@ -1,5 +1,16 @@
 package node;
 
+typedef NodeDef = {
+	@:native("on_step")
+	function onStep(): Void;
+}
+
 abstract class Node {
-    
+	// static function __init__() {
+	// 	var t: NodeDef = Dirt;
+	// }
+}
+
+class Dirt extends Node {
+	public static function onStep() {};
 }
