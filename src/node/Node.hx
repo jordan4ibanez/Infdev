e4b6ae149ce4593ceff0f4ee0473a5cc7afd5676
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-14 01:36:56 -0400

    Gotchya

--- src/node/Node.hx
+++ src/node/Node.hx
@@ -1,25 +1,16 @@
 package node;
 
-import vector.EngineVector3;
-import luantitypes.Macros;
-
 /**
  * When you extend this class, you get a specialty static class which only uses
  * OOP for auto complete.
  */
 @:autoBuild(luantitypes.NodeDuctTape.build())
-@:build(luantitypes.NodeDuctTape.build())
-abstract class Node {
-	// static function __init__() {
-	// 	var t: NodeDef = Dirt;
-	// }
-	// public static function on_construct(pos: EngineVector3) {};
-	public function testing(): Void {};
+interface Node {
+	@:native("fart")
+	public var testing: () -> Void;
 }
 
-@:registerNode("infdev:dirt")
-class Dirt extends Node {
-	override function testing() {
-		super.testing();
-	}
+@:luantiNode("infdev:dirt")
+class Dirt implements Node {
+	public var testing: () -> Void = () -> {};
 }
