a3d5c4bed1c75a1ec646abf561065c3f1c377d1d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-15 01:56:45 -0400

    Turn this off for now

--- src/definition/NodeDefinition.hx
+++ src/definition/NodeDefinition.hx
@@ -2,15 +2,17 @@ package definition;
 
 interface NodeDefinition extends ItemDefinition {}
 
-@:luantiNode("infdev:dirt")
-final class Dirt implements ItemDefinition {
-	public var description: String = "hi";
+// @:luantiNode("infdev:dirt")
+// final class Dirt implements NodeDefinition {
+// 	public var description: String = "hi";
 
-	public var shortDescription: String = "test";
+// 	public var shortDescription: String = "test";
 
-	public var groups: Dynamic<Int> = {
-		soil: 1,
-	};
+// 	public var groups: Dynamic<Int> = {
+// 		soil: 1,
+// 	};
 
-	public var testing = () -> {};
-}
+// 	public var wieldImage: String;
+
+// 	public var testing: () -> Void;
+// }
