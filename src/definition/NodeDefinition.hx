ecc63044c2342bb2877157052d0d6f8cba4fed6d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-15 18:44:28 -0400

    Turn this off

--- src/definition/NodeDefinition.hx
+++ src/definition/NodeDefinition.hx
@@ -5,13 +5,11 @@ import haxe.extern.EitherType;
 import definition.ItemImageDefinition;
 
 interface NodeDefinition extends ItemDefinition {}
-
-@:luantiNode("infdev:dirt")
-final class Dirt implements NodeDefinition {
-	public var description: String;
-	public var shortDescription: String;
-	public var groups: Dynamic<Int>;
-	public var wieldImage: EitherType<ItemImageDefinition, String> = new ItemImageDefinition("image.png", new TileAnimationDefinitionSheet2d(12, 12, 10.0));
-
-	public var testing: () -> Void;
-}
+// @:luantiNode("infdev:dirt")
+// final class Dirt implements NodeDefinition {
+// 	public var description: String;
+// 	public var shortDescription: String;
+// 	public var groups: Dynamic<Int>;
+// 	public var wieldImage: EitherType<ItemImageDefinition, String> = new ItemImageDefinition("image.png", new TileAnimationDefinitionSheet2d(12, 12, 10.0));
+// 	public var testing: () -> Void;
+// }
