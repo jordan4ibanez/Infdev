cbd8245a6391b51a3c2cb96db7926f18864673b1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-15 18:36:57 -0400

    Nice!

--- src/definition/NodeDefinition.hx
+++ src/definition/NodeDefinition.hx
@@ -1,18 +1,17 @@
 package definition;
 
+import definition.TileAnimationDefinition.TileAnimationDefinitionSheet2d;
+import haxe.extern.EitherType;
+import definition.ItemImageDefinition;
+
 interface NodeDefinition extends ItemDefinition {}
 
-// @:luantiNode("infdev:dirt")
-// final class Dirt implements NodeDefinition {
-// 	public var description: String = "hi";
+@:luantiNode("infdev:dirt")
+final class Dirt implements NodeDefinition {
+	public var description: String;
+	public var shortDescription: String;
+	public var groups: Dynamic<Int>;
+	public var wieldImage: EitherType<ItemImageDefinition, String> = new ItemImageDefinition("image.png", new TileAnimationDefinitionSheet2d(12, 12, 10.0));
 
-// 	public var shortDescription: String = "test";
-
-// 	public var groups: Dynamic<Int> = {
-// 		soil: 1,
-// 	};
-
-// 	public var wieldImage: String;
-
-// 	public var testing: () -> Void;
-// }
+	public var testing: () -> Void;
+}
