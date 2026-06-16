6410317ffaa237a03ece1deb2f871751f2d8ebb3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-15 20:41:02 -0400

    Builder pattern this too

--- src/definition/sound/SimpleSoundSpec.hx
+++ src/definition/sound/SimpleSoundSpec.hx
@@ -1,7 +1,32 @@
 package definition.sound;
 
 class SimpleSoundSpec {
-	var name: String;
-	var gain: Float;
-	var pitch: Float;
+	public var name: String = "";
+	public var gain: Float = 1.0;
+	public var pitch: Float = 1.0;
+	public var fade: Float = 0.0;
+
+	public function new() {}
+
+	// Uses builder pattern.
+
+	public function setName(name: String): SimpleSoundSpec {
+		this.name = name;
+		return this;
+	}
+
+	public function setGain(gain: Float): SimpleSoundSpec {
+		this.gain = gain;
+		return this;
+	}
+
+	public function setPitch(pitch: Float): SimpleSoundSpec {
+		this.pitch = pitch;
+		return this;
+	}
+
+	public function setFade(fade: Float): SimpleSoundSpec {
+		this.fade = fade;
+		return this;
+	}
 }
