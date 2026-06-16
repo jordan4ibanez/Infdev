61cc12735d07c851de114b4fa7f10baf356795bd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-16 02:09:29 -0400

    The base

--- src/definition/ToolCapabilities.hx
+++ src/definition/ToolCapabilities.hx
@@ -1,2 +1,13 @@
 package definition;
 
+import lua.Table;
+
+class GroupDefinition {
+	public var maxlevel = 0;
+	public var uses = 20;
+    
+}
+
+class ToolCapabilities {
+	public var groupcaps: Table<String, GroupDefinition>;
+}
