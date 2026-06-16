aa6e1a1891ed801b9e714cfb9847412ccf0a53f7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-16 19:55:51 -0400

    Swap to fluent interface style

--- src/definition/ToolCapabilities.hx
+++ src/definition/ToolCapabilities.hx
@@ -10,27 +10,34 @@ final class GroupCapabilities {
 
 final class ToolCapabilities {
 	@:native("full_punch_interval")
-	var fullPunchInterval: Float = 1.0;
+	var _fullPunchInterval: Float = 1.0;
 
 	@:native("max_drop_level")
-	var maxDropLevel: Int = 0;
+	var _maxDropLevel: Int = 0;
 
-	var groupcaps: Table<String, GroupCapabilities>;
+	@:native("groupcaps")
+	var _groupcaps: Table<String, GroupCapabilities>;
 
 	@:native("damage_groups")
-	var damageGroups: Table<String, Int>;
+	var __damageGroups: Table<String, Int>;
 
 	@:native("punch_attack_uses")
-	var punchAttackUses: Null<Int>;
+	var _punchAttackUses: Null<Int>;
 
 	public function new() {}
 
-	public function setFullPunchInterval() {}
+	// Uses builder pattern.
+
+	public function fullPunchInterval(fullPunchInterval: Float): ToolCapabilities {
+		this._fullPunchInterval = fullPunchInterval;
+		return this;
+	}
 }
 
 class Blah {
 	static function __init__() {
-		var i = new ToolCapabilities();
+		var i = new ToolCapabilities()
+			.fullPunchInterval(5.0);
 		untyped __lua__("
 		print(i);
 		");
