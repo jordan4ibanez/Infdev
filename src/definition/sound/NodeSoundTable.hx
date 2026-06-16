7cb69ba93a62933ec486620c98c1839e4a746977
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-15 20:38:19 -0400

    Builder pattern

--- src/definition/sound/NodeSoundTable.hx
+++ src/definition/sound/NodeSoundTable.hx
@@ -1,3 +1,38 @@
 package definition.sound;
 
-class NodeSoundTable {}
+class NodeSoundTable {
+	public var breaks: SimpleSoundSpec;
+	public var eat: SimpleSoundSpec;
+	@:native("punch_use")
+	public var punchUse: SimpleSoundSpec;
+	@:native("punch_use_air")
+	public var punchUseAir: SimpleSoundSpec;
+
+	static function __init__() {
+		var x = new NodeSoundTable();
+	}
+
+	public function new() {}
+
+	// Uses builder pattern.
+
+	public function setBreaks(breaks: SimpleSoundSpec): NodeSoundTable {
+		this.breaks = breaks;
+		return this;
+	}
+
+	public function setEat(eat: SimpleSoundSpec): NodeSoundTable {
+		this.eat = eat;
+		return this;
+	}
+
+	public function setPunchUse(punchUse: SimpleSoundSpec): NodeSoundTable {
+		this.punchUse = punchUse;
+		return this;
+	}
+
+	public function setPunchUseAir(punchUseAir: SimpleSoundSpec): NodeSoundTable {
+		this.punchUseAir = punchUseAir;
+		return this;
+	}
+}
