7ebeb3450d8dd2ad024fc2eac4f7d1398723ff32
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-16 20:11:25 -0400

    Move that back

--- src/definition/ToolCapabilities.hx
+++ src/definition/ToolCapabilities.hx
@@ -15,13 +15,13 @@ final class ToolCapabilities {
 	@:native("max_drop_level")
 	var maxDropLevel: Int = 0;
 
+	var groupcaps: Table<String, GroupCapabilities>;
+
 	@:native("damage_groups")
 	var damageGroups: Table<String, Int>;
 
 	@:native("punch_attack_uses")
 	var punchAttackUses: Null<Int>;
-
-	var groupcaps: Table<String, GroupCapabilities>;
 
 	public function new() {}
 
