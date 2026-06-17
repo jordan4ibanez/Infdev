65577849d5f522e6d22a263115ea68829d19ff0c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-17 00:39:07 -0400

    Update WearBarColors.hx

--- src/definition/images/WearBarColors.hx
+++ src/definition/images/WearBarColors.hx
@@ -1,5 +1,13 @@
 package definition.images;
 
+enum abstract WearBarBlend(String) to String {
+	var verticalFrames = "vertical_frames";
+	var sheet2d = "sheet_2d";
+}
+
 final class WearBarColors {
+	var blend: WearBarBlend;
+
+    
 
 }
