f40b1ec263ac06a1e773e943b2562b7f58b5b287
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-17 01:58:25 -0400

    Move it

--- src/definition/TouchInteractionMode.hx
+++
@@ -1,5 +0,0 @@
-package definition;
-
-final class TouchInteractionMode {
-    
-}
---
+++ src/definition/TouchInteractionSetting.hx
@@ -0,0 +1,11 @@
+package definition;
+
+enum abstract TileAnimationType(String) to String {
+	var verticalFrames = "vertical_frames";
+	var sheet2d = "sheet_2d";
+}
+
+
+final class TouchInteractionSetting {
+	var pointed_nothing
+}
