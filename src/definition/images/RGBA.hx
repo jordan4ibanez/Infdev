2ffb88a51afd10e977ae4962b017588b2d6fc56d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-17 00:59:53 -0400

    Ooooo colors

---
+++ src/definition/images/RGBA.hx
@@ -0,0 +1,15 @@
+package definition.images;
+
+final class RGBA {
+	var r: Int;
+	var g: Int;
+	var b: Int;
+	var a: Int;
+
+	public function new(r: Int, g: Int, b: Int, a: Int) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.a = a;
+	}
+}
