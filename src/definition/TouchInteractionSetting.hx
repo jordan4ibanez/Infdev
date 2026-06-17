eb5e65c33607a405521ea6f1ecdbbaa066d599a7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-17 02:01:20 -0400

    That looks correct

--- src/definition/TouchInteractionSetting.hx
+++ src/definition/TouchInteractionSetting.hx
@@ -1,11 +1,22 @@
 package definition;
 
-enum abstract TileAnimationType(String) to String {
-	var verticalFrames = "vertical_frames";
-	var sheet2d = "sheet_2d";
+enum abstract TouchInteractionMode(String) to String {
+	var longDigShortPlace = "long_dig_short_place";
+	var shortDigLongPlace = "short_dig_long_place";
+	var user;
 }
 
-
 final class TouchInteractionSetting {
-	var pointed_nothing
+	@:native("pointed_nothing")
+	var pointedNothing: TouchInteractionMode;
+	@:native("pointed_node")
+	var pointedNode: TouchInteractionMode;
+	@:native("pointed_object")
+	var pointedObject: TouchInteractionMode;
+
+	public function new(pointedNothing: TouchInteractionMode, pointedNode: TouchInteractionMode, pointedObject: TouchInteractionMode) {
+		this.pointedNothing = pointedNothing;
+		this.pointedNode = pointedNode;
+		this.pointedObject = pointedObject;
+	}
 }
