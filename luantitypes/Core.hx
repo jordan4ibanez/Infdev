d1260ae294f6f5375e6643269e1bcc8159b36777
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-19 19:13:37 -0400

    Update Core.hx

--- luantitypes/Core.hx
+++ luantitypes/Core.hx
@@ -150,6 +150,10 @@ extern class Core {
 		picker: Null<ObjectRefBase>,
 		pointedThing: PointedThing,
 		timeFromLastPunch: Float): Null<ItemStack>;
+
+	// todo: node thing
+	@:native("node_dig")
+	static function nodeDig(pos: EngineVector3, node: Dynamic, digger: Null<ObjectRefBase>): Bool;
 }
 
 @:noCompletion
