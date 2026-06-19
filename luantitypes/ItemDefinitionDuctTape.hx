cdd26354313f85fa109c6046fa73ee04563c09f3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-19 14:58:50 -0400

    excellent

--- luantitypes/ItemDefinitionDuctTape.hx
+++ luantitypes/ItemDefinitionDuctTape.hx
@@ -220,7 +220,7 @@ class ItemDefinitionDuctTape {
 				for (implementation in fieldChecks) {
 					final has = Lambda.find(fields, (f) -> f.name == implementation.classMethodName) != null;
 
-					// trace(check.classMethodName, has);
+					trace(implementation.classMethodName, has);
 
 					if (has) {
 						var parsed = Context.parse(implementation.code, Context.currentPos());
