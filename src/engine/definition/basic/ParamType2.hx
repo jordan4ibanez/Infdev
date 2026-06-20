87ca82f50a703a4f8f420afa0f3a860522de8997
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-20 18:03:10 -0400

    the heckening
    heck
    
    heck2
    
    not golf
    
    Update ParamType2.hx

--- src/engine/definition/basic/ParamType2.hx
+++ src/engine/definition/basic/ParamType2.hx
@@ -1,5 +1,18 @@
 package engine.definition.basic;
 
 enum abstract ParamType2(String) to String {
-	
+	var ParamType2FlowingLiquid = "flowingliquid";
+	var ParamType2WallMounted = "wallmounted";
+	var ParamType2FaceDir = "facedir";
+	var ParamType2FourDir = "4dir";
+	var ParamType2Leveled = "leveled";
+	var ParamType2DegRotate = "degrotate";
+	var ParamType2MeshOptions = "meshoptions";
+	var ParamType2Color = "color";
+	var ParamType2ColorFaceDir = "colorfacedir";
+	var ParamType2ColorFourDir = "color4dir";
+	var ParamType2ColorWallMounted = "colorwallmounted";
+	var ParamType2GlassLikeLiquidLevel = "glasslikeliquidlevel";
+	var ParamType2ColorDegRotate = "colordegrotate";
+	var none = "none";
 }
