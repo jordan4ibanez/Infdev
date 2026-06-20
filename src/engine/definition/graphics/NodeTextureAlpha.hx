f609399a1537f716d279fa2bf0b7061e8ac0f3b7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-20 01:47:33 -0400

    Noice

--- src/engine/definition/graphics/NodeTextureAlpha.hx
+++ src/engine/definition/graphics/NodeTextureAlpha.hx
@@ -1,2 +1,7 @@
 package engine.definition.graphics;
 
+enum abstract NodeTextureAlpha(String) to String {
+	var NodeTextureAlphaOpaque = "opaque";
+	var NodeTextureAlphaClip = "clip";
+	var NodeTextureAlphaBlend = "blend";
+}
