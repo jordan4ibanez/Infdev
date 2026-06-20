c1c3de718ca83f6f074c04ed24c345ece043071d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-20 19:58:19 -0400

    Update NodeBox.hx

--- src/engine/definition/NodeBox.hx
+++ src/engine/definition/NodeBox.hx
@@ -69,6 +69,11 @@ class NodeBoxWallMounted extends NodeBox {
 		this.wallTop = [x1, y1, z1, x2, y2, z2];
 		return this;
 	}
+
+	public function setWallBottom(x1: Float, y1: Float, z1: Float, x2: Float, y2: Float, z2: Float): NodeBoxWallMounted {
+		this.wallBottom = [x1, y1, z1, x2, y2, z2];
+		return this;
+	}
 }
 
 class NodeBoxConnected extends NodeBox {
