128e787e8bfea6eaf999fe44db2cbba44c922dfc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-19 22:42:44 -0400

    Upgrade

--- luantitypes/LogLevel.hx
+++ luantitypes/LogLevel.hx
@@ -1,10 +1,10 @@
 package luantitypes;
 
 enum abstract LogLevel(String) to String {
-	var none;
-	var error;
-	var warning;
-	var action;
-	var info;
-	var verbose;
+	var LogLevelNone = "none";
+	var LogLevelError = "error";
+	var LogLevelWarning = "warning";
+	var LogLevelAction = "action";
+	var LogLevelInfo = "info";
+	var LogLevelVerbose = "verbose";
 }
