72c97040210093008b0302ede0e3b9a8cead12b2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-19 23:02:25 -0400

    Create DrawType.hx

---
+++ src/engine/definition/basic/DrawType.hx
@@ -0,0 +1,23 @@
+package engine.definition.basic;
+
+enum abstract LiquidType(String) to String {
+	var normal = "normal";
+	var airlike = "airlike";
+	var liquid = "liquid";
+	var flowingliquid = "flowingliquid";
+	var glasslike = "glasslike";
+	var glasslike_framed = "glasslike_framed";
+	var glasslike_framed_optional = "glasslike_framed_optional ";
+	var allfaces = "allfaces";
+	var allfaces_optional = "allfaces_optional";
+	var torchlike = "torchlike";
+	var signlike = "signlike";
+
+	var plantlike = "plantlike";
+	var firelike = "firelike";
+	var fencelike = "fencelike";
+	var raillike = "raillike";
+	var nodebox = "nodebox";
+	var mesh = "mesh";
+	var plantlike_rooted = "plantlike_rooted";
+}
