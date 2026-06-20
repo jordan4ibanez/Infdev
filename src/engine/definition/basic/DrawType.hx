72aee3abd052d9a875255d43cdc1fd4c00efaf20
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-19 23:42:22 -0400

    Update DrawType.hx

--- src/engine/definition/basic/DrawType.hx
+++ src/engine/definition/basic/DrawType.hx
@@ -2,21 +2,21 @@ package engine.definition.basic;
 
 enum abstract DrawType(String) to String {
 	var DrawTypeNormal = "normal";
-	var DrawTypeAirlike = "airlike";
+	var DrawTypeAirLike = "airlike";
 	var DrawTypeLiquid = "liquid";
-	var DrawTypeFlowingliquid = "flowingliquid";
-	var DrawTypeGlasslike = "glasslike";
-	var DrawTypeGlasslike_framed = "glasslike_framed";
-	var DrawTypeGlasslike_framed_optional = "glasslike_framed_optional ";
-	var DrawTypeAllfaces = "allfaces";
-	var DrawTypeAllfaces_optional = "allfaces_optional";
-	var DrawTypeTorchlike = "torchlike";
-	var DrawTypeSignlike = "signlike";
-	var DrawTypePlantlike = "plantlike";
-	var DrawTypeFirelike = "firelike";
-	var DrawTypeFencelike = "fencelike";
-	var DrawTypeRaillike = "raillike";
-	var DrawTypeNodebox = "nodebox";
+	var DrawTypeFlowingLiquid = "flowingliquid";
+	var DrawTypeGlassLike = "glasslike";
+	var DrawTypeGlassLikeFramed = "glasslike_framed";
+	var DrawTypeGlassLikeFramedOptional = "glasslike_framed_optional ";
+	var DrawTypeAllFaces = "allfaces";
+	var DrawTypeAllFacesOptional = "allfaces_optional";
+	var DrawTypeTorchLike = "torchlike";
+	var DrawTypeSignLike = "signlike";
+	var DrawTypePlantLike = "plantlike";
+	var DrawTypeFireLike = "firelike";
+	var DrawTypeFenceLike = "fencelike";
+	var DrawTypeRailLike = "raillike";
+	var DrawTypeNodeBox = "nodebox";
 	var DrawTypeMesh = "mesh";
-	var DrawTypePlantlike_rooted = "plantlike_rooted";
+	var DrawTypePlantLikeRooted = "plantlike_rooted";
 }
