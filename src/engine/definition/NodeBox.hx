6407484040377c1bce30a973bd90c405e295d5ac
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-20 21:29:43 -0400

    disconnectedLeft

--- src/engine/definition/NodeBox.hx
+++ src/engine/definition/NodeBox.hx
@@ -105,7 +105,7 @@ class NodeBoxConnected extends NodeBox {
 	@:native("disconnected_front")
 	private var disconnectedFront: BoxArray; // done
 	@:native("disconnected_left")
-	private var disconnectedLeft: BoxArray;
+	private var disconnectedLeft: BoxArray; // done
 	@:native("disconnected_back")
 	private var disconnectedBack: BoxArray;
 	@:native("disconnected_right")
@@ -199,11 +199,11 @@ class NodeBoxConnected extends NodeBox {
 		return this;
 	}
 
-	public function setDisconnected(x1: Float, y1: Float, z1: Float, x2: Float, y2: Float, z2: Float): NodeBoxConnected {
-		if (this. == null) {
-			this. = []
+	public function setDisconnectedLeft(x1: Float, y1: Float, z1: Float, x2: Float, y2: Float, z2: Float): NodeBoxConnected {
+		if (this.disconnectedLeft == null) {
+			this.disconnectedLeft = []
 		}
-		this..push([x1, y1, z1, x2, y2, z2]);
+		this.disconnectedLeft.push([x1, y1, z1, x2, y2, z2]);
 		return this;
 	}
 
