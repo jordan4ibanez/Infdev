b75bf895191381513b986eb8738bc0bda11f73b0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-20 22:15:35 -0400

    format

--- src/engine/definition/sound/NodeSoundTable.hx
+++ src/engine/definition/sound/NodeSoundTable.hx
@@ -1,43 +1,48 @@
 package engine.definition.sound;
 
 class NodeSoundTable {
-	private var footstep: SimpleSoundSpec;//done
+	private var footstep: SimpleSoundSpec; // done
 
-	private var dig: SimpleSoundSpec;//done
+	private var dig: SimpleSoundSpec; // done
 
-	private var dug: SimpleSoundSpec;//done
+	private var dug: SimpleSoundSpec; // done
 
-	private var place: SimpleSoundSpec;//done
+	private var place: SimpleSoundSpec; // done
 
 	@:native("place_failed")
-	private var placeFailed: SimpleSoundSpec;//done
+	private var placeFailed: SimpleSoundSpec; // done
 
-	private var fall: SimpleSoundSpec;//done
+	private var fall: SimpleSoundSpec; // done
 
 	public function new() {}
 
 	public function setFootstep(sound: SimpleSoundSpec): NodeSoundTable {
 		this.footstep = sound;
-		return this;		
+		return this;
 	}
+
 	public function setDig(sound: SimpleSoundSpec): NodeSoundTable {
 		this.dig = sound;
-		return this;		
+		return this;
 	}
+
 	public function setDug(sound: SimpleSoundSpec): NodeSoundTable {
 		this.dug = sound;
-		return this;		
+		return this;
 	}
+
 	public function setPlace(sound: SimpleSoundSpec): NodeSoundTable {
 		this.place = sound;
-		return this;		
+		return this;
 	}
+
 	public function setPlaceFailed(sound: SimpleSoundSpec): NodeSoundTable {
 		this.placeFailed = sound;
-		return this;		
+		return this;
 	}
+
 	public function setFall(sound: SimpleSoundSpec): NodeSoundTable {
 		this.fall = sound;
-		return this;		
+		return this;
 	}
 }
