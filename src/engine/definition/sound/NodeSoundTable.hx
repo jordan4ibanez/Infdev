3a1a49fffc94aa6add9484b0d592847af016da20
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-20 22:11:25 -0400

    Update NodeSoundTable.hx

--- src/engine/definition/sound/NodeSoundTable.hx
+++ src/engine/definition/sound/NodeSoundTable.hx
@@ -1,3 +1,16 @@
 package engine.definition.sound;
 
-class NodeSoundTable {}
+class NodeSoundTable {
+	private var footstep: SimpleSoundSpec;
+
+	private var dig: SimpleSoundSpec;
+
+	private var dug: SimpleSoundSpec;
+
+	private var place: SimpleSoundSpec;
+
+	@:native("place_failed")
+	private var placeFailed: SimpleSoundSpec;
+
+	private var fall: SimpleSoundSpec;
+}
