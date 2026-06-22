ea1f2f6af0a4d2973bc18f6ef59539c4141f0dbc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-22 12:53:10 -0400

    Update ConnectSides.hx

--- src/engine/definition/basic/ConnectSides.hx
+++ src/engine/definition/basic/ConnectSides.hx
@@ -1,10 +1,10 @@
 package engine.definition.basic;
 
 enum abstract ConnectSides(String) to String {
-	var top = "";
-	var bottom = "";
-	var front = "";
-	var left = "";
-	var back = "";
-	var right = "";
+	var top = "top";
+	var bottom = "bottom";
+	var front = "front";
+	var left = "left";
+	var back = "back";
+	var right = "right";
 }
