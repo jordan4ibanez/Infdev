0a904755058c4f814d0ef83cf7cda378e92edcfc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-21 22:19:09 -0400

    Update NodeDropTable.hx

--- src/engine/definition/basic/NodeDropTable.hx
+++ src/engine/definition/basic/NodeDropTable.hx
@@ -1,6 +1,18 @@
 package engine.definition.basic;
 
 // Design goal: functional oop and make it look like lisp
+
+class NodeDrop {
+	var rarity: Int = 1;
+
+	public function new() {}
+
+	public function setRarity(rarity: Int): NodeDrop {
+		this.rarity = rarity;
+		return this;
+	}
+}
+
 class NodeDropTable {
 	@:native("max_items")
 	private var maxItems: Int = 1;
