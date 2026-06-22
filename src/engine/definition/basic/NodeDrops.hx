bbcb47897b9ea561d00f90e379ce73eee1a754c7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-21 21:30:33 -0400

    Stage 1

--- src/engine/definition/basic/NodeDrops.hx
+++ src/engine/definition/basic/NodeDrops.hx
@@ -1,7 +1,14 @@
 package engine.definition.basic;
 
 // Design goal: functional oop and make it look like lisp
-
 class NodeDrops {
+	@:native("max_items")
+	private var maxItems: Int = 1;
 
+	public function new() {}
+
+	public function setMaxItems(maxItems: Int): NodeDrops {
+		this.maxItems = maxItems;
+		return this;
+	}
 }
