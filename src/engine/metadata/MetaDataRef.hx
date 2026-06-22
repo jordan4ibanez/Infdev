d2a53d7ba1a26504c1ff8992fad099cd35ab606a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-22 19:13:09 -0400

    reverse keyword order

--- src/engine/metadata/MetaDataRef.hx
+++ src/engine/metadata/MetaDataRef.hx
@@ -5,38 +5,38 @@ import lua.Table;
 
 abstract class MetaDataRef {
 	@:native("contains")
-	abstract public function contains(key: String): Bool;
+	public abstract function contains(key: String): Bool;
 
 	@:native("get")
-	abstract public function get<T>(key: String): Null<Dynamic<T>>;
+	public abstract function get<T>(key: String): Null<Dynamic<T>>;
 
 	@:native("set_string")
-	abstract public function setString(key: String, value: String): Void;
+	public abstract function setString(key: String, value: String): Void;
 
 	@:native("get_string")
-	abstract public function getString(key: String): String;
+	public abstract function getString(key: String): String;
 
 	@:native("set_int")
-	abstract public function setInt(key: String, int: Int): Void;
+	public abstract function setInt(key: String, int: Int): Void;
 
 	@:native("get_int")
-	abstract public function getInt(key: String): Int;
+	public abstract function getInt(key: String): Int;
 
 	@:native("set_float")
-	abstract public function setFloat(key: String, float: Float): Void;
+	public abstract function setFloat(key: String, float: Float): Void;
 
 	@:native("get_float")
-	abstract public function getFloat(key: String): Float;
+	public abstract function getFloat(key: String): Float;
 
 	@:native("get_keys")
-	abstract public function getKeys(): LuaArray<String>;
+	public abstract function getKeys(): LuaArray<String>;
 
 	@:native("to_table")
-	abstract public function toTable(): Table<Dynamic, Dynamic>;
+	public abstract function toTable(): Table<Dynamic, Dynamic>;
 
 	@:native("from_table")
-	abstract public function fromTable(table: Dynamic): Bool;
+	public abstract function fromTable(table: Dynamic): Bool;
 
 	@:native("equals")
-	abstract public function equals(other: MetaDataRef): Bool;
+	public abstract function equals(other: MetaDataRef): Bool;
 }
