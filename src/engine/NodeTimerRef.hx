284a981d54411f558e4400d06cdcd55ad50edf7f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-22 19:12:05 -0400

    Update NodeTimerRef.hx

--- src/engine/NodeTimerRef.hx
+++ src/engine/NodeTimerRef.hx
@@ -1,5 +1,18 @@
 package engine;
 
 abstract class NodeTimerRef {
-    // todo: this thing
+	abstract public function set(timeout: Float, elapsed: Float): Void;
+
+	abstract public function start(timeout: Float): Void;
+
+	abstract public function stop(): Void;
+
+	@:native("get_timeout")
+	abstract public function getTimeout(): Float;
+
+	@:native("get_elapsed")
+	abstract public function getElapsed(): Float;
+
+	@:native("is_started")
+	abstract public function isStarted(): Bool;
 }
