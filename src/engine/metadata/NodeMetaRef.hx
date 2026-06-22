7228b15abb3f524f0c3dbf1523d83f0eacacda34
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-22 18:34:51 -0400

    Amazing

--- src/engine/metadata/NodeMetaRef.hx
+++ src/engine/metadata/NodeMetaRef.hx
@@ -1,3 +1,11 @@
 package engine.metadata;
 
-abstract class NodeMetaRef extends MetaDataRef {}
+abstract class NodeMetaRef extends MetaDataRef {
+	@:native("get_inventory")
+	abstract public function getInventory(): InvRef;
+
+	@:native("mark_as_private")
+	abstract public function markAsPrivate(name: String): Void;
+
+	// todo: rest version with a lua table as an inline.
+}
