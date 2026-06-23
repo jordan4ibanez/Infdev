601262c3ac9de4b5fbf5ade1914a10394a53b554
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-22 23:39:13 -0400

    Mhm mhm

--- src/game/Items.hx
+++ src/game/Items.hx
@@ -1,5 +1,10 @@
 package game;
 
+import luantitypes.Core.Global;
+import lua.Lua;
+import engine.definition.ToolCapabilities;
+import engine.definition.graphics.WearBarColors;
+import engine.vector.Vec3;
 import engine.ItemStack;
 import engine.entity.objectref.ObjectRefBase;
 import engine.NodeTable;
@@ -9,6 +14,18 @@ import engine.definition.ItemDefinition;
 final class Hand extends ItemDefinition {
 	public function new() {
 		super();
+
+		wieldScale = new Vec3(1, 1, 1);
+
+		this.itemColor = "white";
+
+		this.toolCapabilities = new ToolCapabilities()
+			.setFullPunchInterval(1.0)
+			.addGroupCap("oddly_breakable_by_hand", new GroupCapabilities()
+				.setTimesFromArray([3.0, 2.0, 3.0])
+				.setUses(0));
+
+		// Lua.print(Global.dump(this.toolCapabilities));
 	}
 }
 
