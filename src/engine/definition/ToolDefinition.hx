561e37ee4d9b6456a06574bcc4c77f1c14957505
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-23 01:16:50 -0400

    Update ToolDefinition.hx

--- src/engine/definition/ToolDefinition.hx
+++ src/engine/definition/ToolDefinition.hx
@@ -1,7 +1,23 @@
 package engine.definition;
 
 /**
- * This one is specifically for registering a tool.
+ * When you extend this class, you get a specialty class which is extremely interesting.
+ * 
+ * The extended class is wrapped in a static class.
+ * 
+ * Defined vars are all copied to the static class. (They are virtual final)
+ * 
+ * Defined methods are copied to the static class and wrapped in static methods.
+ * 
+ * ! Warning: Do not call an override API method unless you define it. It doesn't exist.
+ * 
+ * Feel free to edit your custom vars during runtime.
+ * 
+ * Never call another override function unless 
+ * 
+ * Also another note: This one is for Toold. 
+ * Use the ItemDefinition class for items.
+ * Use the NodeDefinition class for nodes.
  */
 @:luantiDefinitionRoot
 class ToolDefinition extends ItemDefinition {
