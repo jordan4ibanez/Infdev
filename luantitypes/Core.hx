bc58b63c18c0770c24d70def28f85fdda4637334
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-22 23:14:32 -0400

    So get the override item

--- luantitypes/Core.hx
+++ luantitypes/Core.hx
@@ -54,6 +54,9 @@ extern class Core {
 	@:native("register_craftitem")
 	static function registerCraftItem(name: String, def: Dynamic): Void;
 
+	@:native("override_item")
+	static function overrideItem(name: String, redef: Dynamic, ?delFields: Dynamic): Void;
+
 	// fixme: this is incorrect.
 	@:native("request_shutdown")
 	static function requestShutdown(?message: String, ?reconnect: Bool, ?delay: Float): Void;
