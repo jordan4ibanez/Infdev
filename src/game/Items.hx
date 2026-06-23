466398d603b19c8efd4ef6bcd5c6179d5c45f283
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-22 22:58:22 -0400

    Hmmmmmm

--- src/game/Items.hx
+++ src/game/Items.hx
@@ -1,10 +1,22 @@
 package game;
 
+import engine.ItemStack;
+import engine.entity.objectref.ObjectRefBase;
+import engine.NodeTable;
 import engine.definition.ItemDefinition;
+
+
 
 @:register("infdev:stick")
 final class Stick extends ItemDefinition {
 	public function new() {
 		super();
+
+		toolCapabilities.addGroupCap()
+	}
+
+	override function afterUse(itemstack: ItemStack, user: Null<ObjectRefBase>, node: NodeTable, digparams: Dynamic): Null<ItemStack> {
+		return super.afterUse(itemstack, user, node, digparams);
+		trace("hmmm");
 	}
 }
