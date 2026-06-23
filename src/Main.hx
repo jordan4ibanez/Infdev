f5212afd5d44e7ef5e1224c373ae015bed0beb31
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-23 17:55:32 -0400

    Turn all this off

--- src/Main.hx
+++ src/Main.hx
@@ -4,15 +4,15 @@ import luantitypes.Core;
 
 class Main {
 	public static function main(): Void {
-		Core.registerOnJoinPlayer((player, lastLogin) -> {
-			// untyped {
-			// 	trace(Global.dump(player.getMeta()));
-			// }
+		// Core.registerOnJoinPlayer((player, lastLogin) -> {
+		// untyped {
+		// 	trace(Global.dump(player.getMeta()));
+		// }
 
-			// Core.requestShutdown();
+		// Core.requestShutdown();
 
-			// untyped __lua__("core.set_node(player:get_pos(), {name='infdev:dirt'})");
-		});
+		// untyped __lua__("core.set_node(player:get_pos(), {name='infdev:dirt'})");
+		// });
 
 		// Core.registerGlobalStep((delta) -> {
 		// 	for (player in Core.getConnectedPlayers()) {
