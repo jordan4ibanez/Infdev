b72dbd6932eca9c045002205f464ee847cd3b9ff
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-22 20:53:06 -0400

    Update InvRef.hx

--- src/engine/InvRef.hx
+++ src/engine/InvRef.hx
@@ -3,3 +3,6 @@ package engine;
 abstract class InvRef {
 	@:native("is_empty")
 	public abstract function isEmpty(listName: String): Boolean;
+
+	@:native("get_size")
+	public abstract function getSize(listName: String): Int;
