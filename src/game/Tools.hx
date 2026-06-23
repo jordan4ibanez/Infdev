f565a678c8aced52ddc7a201751c4280f1cf10dc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-23 01:43:24 -0400

    Update Tools.hx

--- src/game/Tools.hx
+++ src/game/Tools.hx
@@ -1,2 +1,11 @@
 package game;
 
+import engine.NodeTable;
+import engine.entity.objectref.ObjectRefBase;
+import engine.ItemStack;
+import luantitypes.Core.Global;
+import lua.Lua;
+import engine.definition.ToolCapabilities;
+import engine.vector.Vec3;
+import engine.definition.ToolDefinition;
+
