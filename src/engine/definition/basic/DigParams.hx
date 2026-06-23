cfa6cc0efec9908588e957f637c428d8842db9bc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-23 01:46:47 -0400

    I love inline function

--- src/engine/definition/basic/DigParams.hx
+++ src/engine/definition/basic/DigParams.hx
@@ -2,4 +2,23 @@ package engine.definition.basic;
 
 // This has no definition in the docs.
 // { time : 1, diggable : true, wear : 0 }
-abstract class DigParams {}
+@:final
+abstract class DigParams {
+	private var time: Int;
+	private var diggable: Bool;
+	private var wear: Int;
+
+	private function new() {}
+
+	public inline function getTime(): Int {
+		return this.time;
+	}
+
+	public inline function isDiggable(): Bool {
+		return this.diggable;
+	}
+
+	public inline function getWear(): Int {
+		return this.wear;
+	}
+}
