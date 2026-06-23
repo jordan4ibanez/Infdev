dcd824f5111796975f770691afe8733a72acb4e8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-23 01:14:23 -0400

    Update ToolDefinition.hx

--- src/engine/definition/ToolDefinition.hx
+++ src/engine/definition/ToolDefinition.hx
@@ -1,5 +1,8 @@
 package engine.definition;
 
+/**
+ * This one is specifically for registering a tool.
+ */
 @:luantiDefinitionRoot
 class ToolDefinition extends ItemDefinition {
 	public function new() {
