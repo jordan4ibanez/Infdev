b1ba3ee03275ff5c3a57d09cd7d614081b2b71b2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-23 22:01:26 -0400

    A neat note about a neat feature

--- terraingenerator/TerrainGenerator.hx
+++ terraingenerator/TerrainGenerator.hx
@@ -1,5 +1,10 @@
 package terraingenerator;
 
+/**
+ * This is the terrain generator.
+ * It runs multi threaded in the game engine.
+ * I think that's pretty neat.
+ */
 class TerrainGenerator {
 	static function main() {}
 }
