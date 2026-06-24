a4f1ccd7ff6df5b8004ea01c9dd21826526a45a9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-23 23:50:47 -0400

    Update ItemStack.hx

--- src/engine/ItemStack.hx
+++ src/engine/ItemStack.hx
@@ -3,21 +3,27 @@ package engine;
 // todo: getters
 // todo: rework this entire thing this is just a bootstrap
 // https://github.com/luanti-org/luanti/blob/master/doc/lua_api.md#itemstack
-extern class ItemStack {
+@:final
+abstract class ItemStack {
+	static function create(itemName: String): ItemStack {
+		return untyped __lua__("ItemStack(itemName)");
+	}
+
 	@:native("set_count")
-	public function setCount(count: Int): Void;
+	public abstract function setCount(count: Int): Void;
 
 	@:native("get_count")
-	public function getCount(): Int;
+	public abstract function getCount(): Int;
 
 	@:native("get_wear")
-	public function getWear(): Int;
+	public abstract function getWear(): Int;
 
 	@:native("add_wear")
-	public function addWear(wear: Int): Void;
+	public abstract function addWear(wear: Int): Void;
 
 	@:native("set_wear")
-	public function setWear(wear: Int): Void;
+	public abstract function setWear(wear: Int): Void;
 
-	public function setMetadata(metadata: String): Void;
+	// @:native("")
+	// public abstract function setMetadata(metadata: String): Void;
 }
