88ec863225102f22e8f250ea8dfde94ca4789f9c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-24 00:52:20 -0400

    Add some noice node groups yeeee

---
+++ src/game/groups/NodeGroup.hx
@@ -0,0 +1,9 @@
+package src.game.groups;
+
+enum abstract NodeGroup(String) to String {
+	var NodeGroupDirt;
+	var NodeGroupSand;
+	var NodeGroupStone;
+	var NodeGroupBrick;
+	var NodeGroupGlass;
+}
