e477551aaace2d5f78103d9807361dcbd0250929
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-24 00:21:56 -0400

    Fix the bootstrap of the patcher

--- LuantiCompilerFix.hx
+++ LuantiCompilerFix.hx
@@ -7,25 +7,29 @@ using StringTools;
 class LuantiCompilerFix {
 	public static function patch(fileName: String) {
 		Context.onAfterGenerate(() -> {
+			// This can lock up the Haxe language server so just completely ignore this thing failing to patch it if the file doesn't exist.
 			final path = "mods/infdev/" + fileName;
+			try {
+				var content: String = File.getContent(path);
 
-			var content: String = File.getContent(path);
+				var lines = content.split("\n");
 
-			var lines = content.split("\n");
+				for (i => line in lines) {
+					// Hard code package.loaded.luv to short circuit into _hx_luv define.
+					if (line.contains("package.loaded.luv")) {
+						lines[i] = '-- Short circuit to automatic define.\nif false then';
+					}
 
-			for (i => line in lines) {
-				// Hard code package.loaded.luv to short circuit into _hx_luv define.
-				if (line.contains("package.loaded.luv")) {
-					lines[i] = '-- Short circuit to automatic define.\nif false then';
+					// Check if this thing is gonna blow up Luanti safe mode.
+					// if (line.contains("require") && !line.startsWith("--")) {
+					// 	lines[i] = '--${line} (Disabled for Luanti.)';
+					// }
 				}
 
-				// Check if this thing is gonna blow up Luanti safe mode.
-				// if (line.contains("require") && !line.startsWith("--")) {
-				// 	lines[i] = '--${line} (Disabled for Luanti.)';
-				// }
+				File.saveContent(path, lines.join("\n"));
+			} catch (doNotCare) {
+				trace(path + " doesn't exist. Failed to patch.");
 			}
-
-			File.saveContent(path, lines.join("\n"));
 		});
 	}
 }
