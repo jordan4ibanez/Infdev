2510185b0fb2e7df035a30ae82177d15f49fb4a3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-25 14:03:23 -0400

    Update ItemDefinitionDuctTape.hx

--- src/engine/compilercode/ItemDefinitionDuctTape.hx
+++ src/engine/compilercode/ItemDefinitionDuctTape.hx
@@ -3,9 +3,6 @@ package src.engine.compilercode;
 #if macro
 import haxe.macro.Context;
 import haxe.macro.Expr;
-import haxe.macro.Type;
-import haxe.macro.ExprTools;
-import haxe.macro.Expr.FieldType;
 
 typedef MethodMatcherThing = {
 	public var classMethodName: String;
