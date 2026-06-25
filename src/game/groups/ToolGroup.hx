a452132de6aaa319b568b3a364280a9250905bf2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-25 01:52:16 -0400

    Create ToolGroup.hx

---
+++ src/game/groups/ToolGroup.hx
@@ -0,0 +1,11 @@
+package src.game.groups;
+
+enum abstract ToolGroup(String) to String {
+	var ToolGroupPickaxe;
+	var ToolGroupShovel;
+	var ToolGroupAxe;
+	var ToolGroupSword;
+	var ToolGroupShears;
+	// No one's gonna read this code anyways.
+	var ToolGroupGun;
+}
