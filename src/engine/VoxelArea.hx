029ea62682496f8536468b75185bc18e152dfb26
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-26 19:06:41 -0400

    Update VoxelArea.hx

--- src/engine/VoxelArea.hx
+++ src/engine/VoxelArea.hx
@@ -3,7 +3,7 @@ package src.engine;
 import src.engine.vector.EngineVector3;
 
 @:final
-abstract class VoxelArea {
+abstract extern class VoxelArea {
 	static inline function create(minPos: EngineVector3, maxPos: EngineVector3): VoxelArea {
 		return untyped __lua__("VoxelArea(minPos, maxPos)");
 	}
