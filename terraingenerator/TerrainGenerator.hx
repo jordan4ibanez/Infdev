1d5c9bc6c7064eaa410642178602f425406414e4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-25 23:46:23 -0400

    Let's get OOP with this one

--- terraingenerator/TerrainGenerator.hx
+++ terraingenerator/TerrainGenerator.hx
@@ -8,24 +8,29 @@ import src.engine.Core;
  * I think that's pretty neat.
  */
 class TerrainGenerator {
-	final chunk_size = Core.get_mapgen_chunksize();
-	final c_dirt = Core.get_content_id("infdev:dirt");
-	final c_stone = Core.get_content_id("infdev:stone");
-	final c_air = Core.get_content_id("air");
-	final c_grass = Core.get_content_id("infdev:grass");
-	final c_water_source = Core.get_content_id("infdev:water_source");
-	final c_sand = Core.get_content_id("infdev:sand");
-	final c_bedrock = Core.get_content_id("infdev:bedrock");
-	final c_sandstone = Core.get_content_id("infdev:sandstone");
-	static function clamp(input: Float, low: Float, high: Float): Float {
+	final chunk_size = Core.getMapgenChunkSize();
+	final c_dirt = Core.getContentID("infdev:dirt");
+	final c_stone = Core.getContentID("infdev:stone");
+	final c_air = Core.getContentID("air");
+	final c_grass = Core.getContentID("infdev:grass");
+	final c_water_source = Core.getContentID("infdev:water_source");
+	final c_sand = Core.getContentID("infdev:sand");
+	final c_bedrock = Core.getContentID("infdev:bedrock");
+	final c_sandstone = Core.getContentID("infdev:sandstone");
+
+	public function new() {
+		Core.log(LogLevelNone, "test");
+	}
+
+	static function main() {
+		new TerrainGenerator();
+	}
+
+	function clamp(input: Float, low: Float, high: Float): Float {
 		if (low > high) {
 			low = high;
 			high = low;
 		}
 		return Math.max(low, Math.min(high, input));
-	}
-
-	static function main() {
-		Core.log(LogLevelNone, "test");
 	}
 }
