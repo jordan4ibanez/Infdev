f93ea97d7747a851698db668ce2394cd2dd03a07
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-26 14:54:46 -0400

    Update VoxelManip.hx

--- src/engine/VoxelManip.hx
+++ src/engine/VoxelManip.hx
@@ -1,4 +1,15 @@
 package src.engine;
 
+import src.engine.vector.EngineVector3;
+
+@:multiReturn
+extern class MinMaxPos {
+	public var min: EngineVector3;
+	public var max: EngineVector3;
+}
+
 @:final
-abstract extern class VoxelManip {}
+abstract extern class VoxelManip {
+	@:native("read_from_map")
+	public function readFromMap(): MinMaxPos;
+}
