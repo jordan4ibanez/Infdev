43259bb856dc507d66358ca1de3ffb6301080b01
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-26 19:10:48 -0400

    Update VoxelArea.hx

--- src/engine/VoxelArea.hx
+++ src/engine/VoxelArea.hx
@@ -2,9 +2,25 @@ package src.engine;
 
 import src.engine.vector.EngineVector3;
 
+// ? This class is in camel case for some reason??
 @:final
 abstract extern class VoxelArea {
 	static inline function create(minPos: EngineVector3, maxPos: EngineVector3): VoxelArea {
 		return untyped __lua__("VoxelArea(minPos, maxPos)");
 	}
+
+	public function getExtent(): EngineVector3;
+
+	public function getVolume(): Int;
+
+	/**
+	 * This is 1 indexed.
+	 */
+	public function index(x: Int, y: Int, z: Int): Int;
+
+	/**
+	 * This is 1 indexed.
+	 */
+	@:native("indexp")
+	public function indexP(pos: EngineVector3): Int;
 }
