2d817e3356e42639102e11f443e1018393ce9552
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-26 19:06:02 -0400

    Hello abstract static factory extern virtual thing

--- src/engine/VoxelArea.hx
+++ src/engine/VoxelArea.hx
@@ -1,4 +1,10 @@
 package src.engine;
 
+import src.engine.vector.EngineVector3;
+
 @:final
-abstract extern class VoxelArea {}
+abstract class VoxelArea {
+	static inline function create(minPos: EngineVector3, maxPos: EngineVector3): VoxelArea {
+		return untyped __lua__("VoxelArea(minPos, maxPos)");
+	}
+}
