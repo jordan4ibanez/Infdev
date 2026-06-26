d53484c72c6411b5865298db7b4424862e723720
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-25 23:41:13 -0400

    Bring this back

--- terraingenerator/TerrainGenerator.hx
+++ terraingenerator/TerrainGenerator.hx
@@ -8,16 +8,15 @@ import src.engine.Core;
  * I think that's pretty neat.
  */
 class TerrainGenerator {
-	
-	// final chunk_size = Core.get_mapgen_chunksize();
-	// final c_dirt = Core.get_content_id("infdev:dirt");
-	// final c_stone = Core.get_content_id("infdev:stone");
-	// final c_air = Core.get_content_id("air");
-	// final c_grass = Core.get_content_id("infdev:grass");
-	// final c_water_source = Core.get_content_id("infdev:water_source");
-	// final c_sand = Core.get_content_id("infdev:sand");
-	// final c_bedrock = Core.get_content_id("infdev:bedrock");
-	// final c_sandstone = Core.get_content_id("infdev:sandstone");
+	final chunk_size = Core.get_mapgen_chunksize();
+	final c_dirt = Core.get_content_id("infdev:dirt");
+	final c_stone = Core.get_content_id("infdev:stone");
+	final c_air = Core.get_content_id("air");
+	final c_grass = Core.get_content_id("infdev:grass");
+	final c_water_source = Core.get_content_id("infdev:water_source");
+	final c_sand = Core.get_content_id("infdev:sand");
+	final c_bedrock = Core.get_content_id("infdev:bedrock");
+	final c_sandstone = Core.get_content_id("infdev:sandstone");
 	static function clamp(input: Float, low: Float, high: Float): Float {
 		if (low > high) {
 			low = high;
