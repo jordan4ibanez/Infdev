9d18577a51207d233c465295e5edc0ff1a113728
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-26 01:18:26 -0400

    And this is the instance method

--- terraingenerator/TerrainGenerator.hx
+++ terraingenerator/TerrainGenerator.hx
@@ -29,6 +29,10 @@ class TerrainGenerator {
 		return Math.max(low, Math.min(high, input));
 	}
 
+	function generateThread(voxelManip: Dynamic, minPos: EngineVector3, maxPos: EngineVector3, blockSeed: Int): Void {
+		trace("Good day, I am a map generator thread being called.");
+	}
+
 	// ? Everything below this is infrastructure to get the singleton map generator to load up.
 	static function classGenerateThreadWrapper(voxelManip: Dynamic, minPos: EngineVector3, maxPos: EngineVector3, blockSeed: Int): Void {
 		instance.generateThread(voxelManip, minPos, maxPos, blockSeed);
