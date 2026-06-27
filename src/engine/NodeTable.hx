6b698411a6c7b1c8f9095c6ff10ce682398c281e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-26 22:40:46 -0400

    whatever

--- src/engine/NodeTable.hx
+++ src/engine/NodeTable.hx
@@ -5,27 +5,34 @@ class NodeTable {
 	public var name: String;
 	public var param1: Int = 0;
 	public var param2: Int = 0;
-	// public function new(name: String) {
-	// 	this.name = name;
-	// }
-	// public inline function getName(): String {
-	// 	return this.name;
-	// }
-	// public inline function setName(newName: String) {
-	// 	this.name = newName;
-	// }
-	// public inline function getParam1(): Int {
-	// 	return this.param1;
-	// }
-	// public inline function setParam1(param1: Int): Void {
-	// 	this.param1 = param1;
-	// }
-	// public inline function getParam2(): Int {
-	// 	return this.param2;
-	// }
-	// public inline function setParam2(param2: Int): Void {
-	// 	this.param2 = param2;
-	// }
+
+	public function new(name: String) {
+		this.name = name;
+	}
+
+	public inline function getName(): String {
+		return this.name;
+	}
+
+	public inline function setName(newName: String) {
+		this.name = newName;
+	}
+
+	public inline function getParam1(): Int {
+		return this.param1;
+	}
+
+	public inline function setParam1(param1: Int): Void {
+		this.param1 = param1;
+	}
+
+	public inline function getParam2(): Int {
+		return this.param2;
+	}
+
+	public inline function setParam2(param2: Int): Void {
+		this.param2 = param2;
+	}
 }
 // class Blah {
 // 	static function __init__() {
