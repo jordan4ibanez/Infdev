2ccd0ae1665417be896578c75b3b5f5488a03bed
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-26 22:09:03 -0400

    Update ValueNoiseMap.hx

--- src/engine/ValueNoiseMap.hx
+++ src/engine/ValueNoiseMap.hx
@@ -16,4 +16,9 @@ abstract extern class ValueNoiseMap {
 
 	@:native("get_3d_map_flat")
 	public extern function get3DMapFlat(pos: EngineVector3): LuaArray<Float>;
+
+	@:native("calc_2d_map")
+	public extern function calc2DMap(pos: EngineVector3): Void;
+
+    
 }
