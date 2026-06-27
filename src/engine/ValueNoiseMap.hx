b021f66be57a7315b1c7b6812e5712fd10f89725
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-26 22:06:51 -0400

    TIME TO COOK

--- src/engine/ValueNoiseMap.hx
+++ src/engine/ValueNoiseMap.hx
@@ -1,4 +1,12 @@
 package src.engine;
 
+import src.engine.compilercode.LuaArray;
+import src.engine.vector.EngineVector3;
+
 @:final
-abstract extern class ValueNoiseMap {}
+abstract extern class ValueNoiseMap {
+	@:native("get_2d_map")
+	public extern function get2DMap(pos: EngineVector3): LuaArray<LuaArray<Float>>;
+
+    
+}
