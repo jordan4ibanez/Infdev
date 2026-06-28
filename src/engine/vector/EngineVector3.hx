7174b60e16e9151c7f8e309b9661baa83d26957a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-27 22:15:27 -0400

    ev3 extends ev2 cause who gives a heck

--- src/engine/vector/EngineVector3.hx
+++ src/engine/vector/EngineVector3.hx
@@ -3,9 +3,7 @@ package src.engine.vector;
 /**
  * This class technically doesn't exist.
  */
-abstract class EngineVector3 {
-	public var x: Float = 0;
-	public var y: Float = 0;
+abstract class EngineVector3 extends EngineVector2 {
 	public var z: Float = 0;
 
 	public static function toVec3(self: EngineVector3): Vec3 {
