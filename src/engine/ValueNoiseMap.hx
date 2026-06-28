8f6b41f93e5f06cab2c41c87d66463c6b5c37492
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-27 21:04:56 -0400

    Was missing a buffer input

--- src/engine/ValueNoiseMap.hx
+++ src/engine/ValueNoiseMap.hx
@@ -12,10 +12,10 @@ abstract extern class ValueNoiseMap {
 	public extern function get3DMap(pos: EngineVector3): LuaArray<LuaArray<LuaArray<Float>>>;
 
 	@:native("get_2d_map_flat")
-	public extern function get2DMapFlat(pos: EngineVector3): LuaArray<Float>;
+	public extern function get2DMapFlat(pos: EngineVector3, ?buffer: LuaArray<Float>): LuaArray<Float>;
 
 	@:native("get_3d_map_flat")
-	public extern function get3DMapFlat(pos: EngineVector3): LuaArray<Float>;
+	public extern function get3DMapFlat(pos: EngineVector3, ?buffer: LuaArray<Float>): LuaArray<Float>;
 
 	@:native("calc_2d_map")
 	public extern function calc2DMap(pos: EngineVector3): Void;
