21826edbe3a885ae8db315dc7c49b4203dce788e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-27 22:17:36 -0400

    GLORP

--- terraingenerator/TerrainGenerator.hx
+++ terraingenerator/TerrainGenerator.hx
@@ -136,6 +136,7 @@ class TerrainGenerator {
 		// __overworld_terrain_blend_noise_map_2d:get_2d_map_flat({ x = minp.x, y = minp.z },
 		// 	overworld_terrain_blend_noise)
 
+		final overWorldTerrainBlendNoise: LuaArray<Float> = Core.getValueNoiseMap(overWorldTerrainBlendParameters, constantArea2D).get2DMapFlat(new Vec2(minPos.x, minPos.z));
 
 		// local overworld_terrain_noise_big          = {}
 		// local __overworld_terrain_noise_map_2d_big = core.get_value_noise_map(overworld_terrain_noise_parameters_big,
