8fb8da6fdb683603b7397fa7bff73d43e0d06c3c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-29 16:47:55 -0400

    Update Dirt.hx

--- src/game/node/Dirt.hx
+++ src/game/node/Dirt.hx
@@ -1,5 +1,8 @@
 package src.game.node;
 
+import src.engine.definition.NodeDefinition;
+import src.game.groups.NodeGroup;
+
 @:register("infdev:dirt")
 final class Dirt extends NodeDefinition {
 	public function new() {
