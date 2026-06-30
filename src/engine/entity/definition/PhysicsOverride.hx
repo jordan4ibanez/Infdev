e9b7d8fa5013b0dbe20c5603a75a07c4cd455dbc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-30 19:49:08 -0400

    Update PhysicsOverride.hx

--- src/engine/entity/definition/PhysicsOverride.hx
+++ src/engine/entity/definition/PhysicsOverride.hx
@@ -1,34 +1,34 @@
 package src.engine.entity.definition;
 
 final class PhysicsOverride {
-	public var speed: Float;//
+	public var speed: Float;
 	@:native("speed_walk")
-	public var speedWalk: Float;//
+	public var speedWalk: Float;
 	@:native("speed_climb")
-	public var speedClimb: Float;//
+	public var speedClimb: Float;
 	@:native("speed_crouch")
-	public var speedCrouch: Float;//
+	public var speedCrouch: Float;
 	@:native("speed_fast")
-	public var speedFast: Float;//
-	public var jump: Float;//
-	public var gravity: Float;//
+	public var speedFast: Float;
+	public var jump: Float;
+	public var gravity: Float;
 	@:native("liquid_fluidity")
-	public var liquidFluidity: Float;//
+	public var liquidFluidity: Float;
 	@:native("liquid_fluidity_smooth")
-	public var liquidFluiditySmooth: Float;//
+	public var liquidFluiditySmooth: Float;
 	@:native("liquid_sink")
-	public var liquidSink: Float;//
+	public var liquidSink: Float;
 	@:native("acceleration_default")
-	public var accelerationDefault: Float;//
+	public var accelerationDefault: Float;
 	@:native("acceleration_air")
-	public var accelerationAir: Float;//
+	public var accelerationAir: Float;
 	@:native("acceleration_fast")
-	public var accelerationFast: Float;//
-	public var sneak: Bool;//
+	public var accelerationFast: Float;
+	public var sneak: Bool;
 	@:native("sneak_glitch")
-	public var sneakGlitch: Bool;//
+	public var sneakGlitch: Bool;
 	@:native("new_move")
-	public var newMove: Bool;//
+	public var newMove: Bool;
 
 	public function new() {}
 
