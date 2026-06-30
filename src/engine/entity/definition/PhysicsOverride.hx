d05a3eb440b2dd883f2661468c2bf6ef51132668
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-06-30 12:32:25 -0400

    Update PhysicsOverride.hx

--- src/engine/entity/definition/PhysicsOverride.hx
+++ src/engine/entity/definition/PhysicsOverride.hx
@@ -2,19 +2,31 @@ package src.engine.entity.definition;
 
 final class PhysicsOverride {
     public var speed: Float;
+    @:native("")
     public var speed_walk;
+    @:native("")
     public var speed_climb;
+    @:native("")
     public var speed_crouch;
+    @:native("")
     public var speed_fast;
     public var jump;
     public var gravity;
+    @:native("")
     public var liquid_fluidity;
+    @:native("")
     public var liquid_fluidity_smooth;
+    @:native("")
     public var liquid_sink;
+    @:native("")
     public var acceleration_default;
+    @:native("")
     public var acceleration_air;
+    @:native("")
     public var acceleration_fast;
     public var sneak;
+    @:native("")
     public var sneak_glitch;
+    @:native("")
     public var new_move;
 }
