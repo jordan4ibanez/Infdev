aab63e39834787e9b28bd0fd2b25f441699705c0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-01 22:38:26 -0400

    Update ObjectProperties.hx

--- src/engine/entity/definition/ObjectProperties.hx
+++ src/engine/entity/definition/ObjectProperties.hx
@@ -3,4 +3,10 @@ package src.engine.entity.definition;
 final class ObjectProperties {
 	@:native("hp_max")
 	var hpMax: Int;
+
+	@:native("breath_max")
+	var breathMax: Int;
+
+	@:native("zoom_fov")
+	var zoomFOV: Float;
 }
