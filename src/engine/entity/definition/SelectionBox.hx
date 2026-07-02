f9758acdc8156364b83200e3197fb00d701fa0d7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-01 23:33:21 -0400

    WOO!

--- src/engine/entity/definition/SelectionBox.hx
+++ src/engine/entity/definition/SelectionBox.hx
@@ -1,11 +1,12 @@
 package src.engine.entity.definition;
 
+import haxe.extern.EitherType;
 import lua.Table;
-import src.engine.compilercode.LuaArray;
 
 @:forward
-abstract SelectionBox(Table<Int, Float>) from Table<Int, Float> to Table<Int, Float> {
+abstract SelectionBox(Table<Int, EitherType<Float, Bool>>) from Table<Int, EitherType<Float, Bool>> to Table<Int, EitherType<Float, Bool>> {
 	public inline function new(x1: Float, y1: Float, z1: Float, x2: Float, y2: Float, z2: Float, ?rotates: Bool) {
 		this = Table.create([x1, y1, z1, x2, y2, z2]);
+		this.rotates = rotates;
 	}
 }
