e925183226eb250690327f58a2a12cb75086106a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-02 12:37:06 -0400

    Update EntityVisual.hx

--- src/engine/entity/definition/EntityVisual.hx
+++ src/engine/entity/definition/EntityVisual.hx
@@ -1,5 +1,11 @@
 package src.engine.entity.definition;
 
 enum abstract EntityVisual(String) to String {
-	var EntityVisualNone = "none";
+	var EntityVisual = "cube";
+	var EntityVisual = "sprite";
+	var EntityVisual = "upright_sprite";
+	var EntityVisual = "mesh";
+	var EntityVisual = "wielditem";
+	var EntityVisual = "item";
+	var EntityVisual = "node";
 }
