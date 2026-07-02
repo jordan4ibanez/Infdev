e1277c705a5397fd21d3ccf49b514d7fdae5dc81
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-01 22:49:23 -0400

    Update ObjectProperties.hx

--- src/engine/entity/definition/ObjectProperties.hx
+++ src/engine/entity/definition/ObjectProperties.hx
@@ -4,7 +4,10 @@ class ObjectProperties {
 	@:native("hp_max")
 	public var hpMax: Int;
 
-    var physical: Bool;
+	var physical: Bool;
+
+	@:native("collide_with_objects")
+	var collideWithObjects: Bool;
 }
 
 class ObjectPropertiesPlayer extends ObjectProperties {
@@ -14,10 +17,8 @@ class ObjectPropertiesPlayer extends ObjectProperties {
 	@:native("zoom_fov")
 	public var zoomFOV: Float;
 
-    @:native("eye_height")
-    public var eyeHeight: Float;
-
-
+	@:native("eye_height")
+	public var eyeHeight: Float;
 }
 
 class ObjectPropertiesEntity extends ObjectProperties {}
