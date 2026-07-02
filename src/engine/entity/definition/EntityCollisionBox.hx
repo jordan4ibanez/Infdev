f1a389ab09f6b0ea5c9524c56bea20b3c9785860
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-01 23:34:48 -0400

    Create EntityCollisionBox.hx

---
+++ src/engine/entity/definition/EntityCollisionBox.hx
@@ -0,0 +1,10 @@
+package src.engine.entity.definition;
+
+import lua.Table;
+
+@:forward
+abstract EntityCollisionBox(Table<Int, Float>) from Table<Int, Float> to Table<Int, Float> {
+	public inline function new(x1: Float, y1: Float, z1: Float, x2: Float, y2: Float, z2: Float) {
+		this = Table.create([x1, y1, z1, x2, y2, z2]);
+	}
+}
