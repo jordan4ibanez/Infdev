872ed5bff74e08f0f1e49fa06fcab373d83985f4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-01 23:33:41 -0400

    Make it more specific

--- src/engine/entity/definition/EntitySelectionBox.hx
+++ src/engine/entity/definition/EntitySelectionBox.hx
@@ -4,7 +4,7 @@ import haxe.extern.EitherType;
 import lua.Table;
 
 @:forward
-abstract SelectionBox(Table<Int, EitherType<Float, Bool>>) from Table<Int, EitherType<Float, Bool>> to Table<Int, EitherType<Float, Bool>> {
+abstract EntitySelectionBox(Table<Int, EitherType<Float, Bool>>) from Table<Int, EitherType<Float, Bool>> to Table<Int, EitherType<Float, Bool>> {
 	public inline function new(x1: Float, y1: Float, z1: Float, x2: Float, y2: Float, z2: Float, ?rotates: Bool) {
 		this = Table.create([x1, y1, z1, x2, y2, z2]);
 		this.rotates = rotates;
