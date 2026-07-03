fa93cee5022f9d099c88080a8a35dd0b115cd17f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-03 16:17:02 -0400

    Update StepUpMode.hx

--- src/engine/entity/definition/StepUpMode.hx
+++ src/engine/entity/definition/StepUpMode.hx
@@ -1,7 +1,7 @@
 package src.engine.entity.definition;
 
 enum abstract StepUpMode(String) to String {
-	var StepUpModelegacy = "legacy";
-	var StepUpModefloaty = "floaty";
-	var StepUpModerigid = "rigid";
+	var StepUpModeLegacy = "legacy";
+	var StepUpModeFloaty = "floaty";
+	var StepUpModeRigid = "rigid";
 }
