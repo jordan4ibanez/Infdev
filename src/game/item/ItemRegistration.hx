ed1515d072cbec4616e0154fa9ee3be6aed549e8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-03 17:29:30 -0400

    Amazing, it's lisp

--- src/game/item/ItemRegistration.hx
+++ src/game/item/ItemRegistration.hx
@@ -1,10 +1,16 @@
 package src.game.item;
 
 import src.engine.definition.ItemDefinition;
+import src.engine.definition.basic.ItemPointabilitiesTable;
 
 @:register("infdev:stick")
 final class Stick extends ItemDefinition {
 	public function new() {
 		super();
+
+		this.pointabilities = new ItemPointabilitiesTable()
+			.setNodes(new ItemPointableMap()
+				.set("infdev:dirt", ItemPointableTrue)
+				.set("infdev:sand", ItemPointableBlocking));
 	}
 }
