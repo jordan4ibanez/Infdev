fc0b29b103d02bd9807732f4b6b56916eb01876e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-04 17:59:12 -0400

    Create PlayerControl.hx

---
+++ src/engine/entity/definition/PlayerControl.hx
@@ -0,0 +1,16 @@
+package src.engine.entity.definition;
+
+abstract class PlayerControl {
+	public var up: Bool;
+	public var down: Bool;
+	public var left: Bool;
+	public var right: Bool;
+	public var jump: Bool;
+	public var aux1: Bool;
+	public var sneak: Bool;
+	public var dig: Bool;
+	public var place: Bool;
+	public var LMB: Bool;
+	public var RMB: Bool;
+	public var zoom: Bool;
+}
