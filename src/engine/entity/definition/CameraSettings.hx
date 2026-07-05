f97bb1ff29ebc30e0f2e0c98d856d51c2bb910db
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-04 21:36:14 -0400

    There's no example in the api docs so here's my best guess

--- src/engine/entity/definition/CameraSettings.hx
+++ src/engine/entity/definition/CameraSettings.hx
@@ -1,2 +1,16 @@
 package src.engine.entity.definition;
 
+enum abstract CameraMode(String) to String {
+	var CameraModeAny = "any";
+	var CameraModeFirst = "first";
+	var CameraModeThird = "third";
+	var CameraModeThirdFront = "third_front";
+}
+
+class CameraSettings.hx {
+	public var mode: CameraMode;
+
+	public function new(cameraMode: CameraMode) {
+		this.mode = cameraMode;
+	}
+}
