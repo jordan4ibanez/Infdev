66bed6bb6e3958558175e789bd33ef6426018ec8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-04 22:42:20 -0400

    Update PlayerFlags.hx

--- src/engine/entity/definition/PlayerFlags.hx
+++ src/engine/entity/definition/PlayerFlags.hx
@@ -1,5 +1,8 @@
 package src.engine.entity.definition;
 
 class PlayerFlags {
-    
+    public var breathing: Bool;
+    public var drowning: Bool;
+    @:native("node_damage")
+    public var nodeDamage: Bool;
 }
