48539be6265e51f6f5d5b1139bd473fa256c80c8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-04 22:43:35 -0400

    tada

--- src/engine/entity/definition/PlayerFlags.hx
+++ src/engine/entity/definition/PlayerFlags.hx
@@ -1,8 +1,25 @@
 package src.engine.entity.definition;
 
 class PlayerFlags {
-    public var breathing: Bool;
-    public var drowning: Bool;
-    @:native("node_damage")
-    public var nodeDamage: Bool;
+	public var breathing: Bool;
+	public var drowning: Bool;
+	@:native("node_damage")
+	public var nodeDamage: Bool;
+
+	public function new() {}
+
+	public inline function setBreathing(breathing: Bool): PlayerFlags {
+		this.breathing = breathing;
+		return this;
+	}
+
+	public inline function setDrowning(drowning: Bool): PlayerFlags {
+		this.drowning = drowning;
+		return this;
+	}
+
+	public inline function set(nodeDamage: Bool): PlayerFlags {
+		this.nodeDamage = nodeDamage;
+		return this;
+	}
 }
