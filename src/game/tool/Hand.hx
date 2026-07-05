67c2fffa3355f0d62b7f62ff710be2698fb5ea39
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-04 23:46:36 -0400

    hand

--- src/game/tool/Hand.hx
+++ src/game/tool/Hand.hx
@@ -1,2 +1,21 @@
 package src.game.tool;
 
+@:override("")
+final class Hand extends ToolDefinition {
+	public function new() {
+		super();
+		wieldScale = new Vec3(1, 1, 1);
+		this.itemColor = "white";
+		this.toolCapabilities = new ToolCapabilities()
+			.setFullPunchInterval(1.0)
+			.addGroupCap(NodeGroupHandDiggable, new GroupCapabilities()
+				.setTimesFromMap([
+					1 => 1.0,
+					2 => 2.0,
+					3 => 3.0,
+					BEDROCK => Math.POSITIVE_INFINITY
+				])
+				.setMaxLevel(0)
+				.setUses(0));
+	}
+}
--- src/game/tool/ToolRegistration.hx
+++ src/game/tool/ToolRegistration.hx
@@ -9,25 +9,6 @@ import src.engine.entity.objectref.ObjectRefBase;
 import src.engine.vector.Vec3;
 import src.game.groups.NodeGroup.BEDROCK;
 
-@:override("")
-final class Hand extends ToolDefinition {
-	public function new() {
-		super();
-		wieldScale = new Vec3(1, 1, 1);
-		this.itemColor = "white";
-		this.toolCapabilities = new ToolCapabilities()
-			.setFullPunchInterval(1.0)
-			.addGroupCap(NodeGroupHandDiggable, new GroupCapabilities()
-				.setTimesFromMap([
-					1 => 1.0,
-					2 => 2.0,
-					3 => 3.0,
-					BEDROCK => Math.POSITIVE_INFINITY
-				])
-				.setMaxLevel(0)
-				.setUses(0));
-	}
-}
 
 @:register("infdev:oracle_pickaxe")
 final class OraclePickaxe extends ToolDefinition {
