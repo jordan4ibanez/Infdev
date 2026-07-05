9c7caf47a9d551f198fa8cf33121bf439ae64545
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-05 18:40:17 -0400

    time for the BRICK AHHH

--- src/game/node/stone/Brick.hx
+++ src/game/node/stone/Brick.hx
@@ -1,2 +1,17 @@
 package src.game.node.stone;
 
+import src.engine.definition.NodeDefinition;
+import src.game.groups.NodeGroup;
+
+@:register("infdev:brick")
+final class Brick extends NodeDefinition {
+	public function new() {
+		super();
+
+		this.nodeGroups = [
+			NodeGroupStone => 2
+		];
+
+		this.tiles = ["default_brick.png"];
+	}
+}
