d26030ea52bff9fb3a893d0d827ec54599d9caa4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-06 18:20:39 -0400

    modlib was used to translate that mesh hand

--- src/Main.hx
+++ src/Main.hx
@@ -5,5 +5,24 @@ import lua.Lua;
 class Main {
 	public static function main(): Void {
 		Lua.print("Hello Infdev.");
+
+		untyped __lua__('
+		
+		local path = core.get_modpath("infdev") .. "/models"
+
+		local out_path = core.get_worldpath()
+
+		local filename = "mcl_meshhand.b3d"
+		
+		-- First read the B3D
+		local in_file = assert(io.open(path .. "/" .. filename, "rb"))
+		local model = assert(modlib.b3d.read(in_file))
+		in_file:close()
+		-- Then write the glTF
+		local out_file = io.open(out_path .. "/" .. filename .. ".gltf", "wb")
+		model:write_gltf(out_file)
+		out_file:close()		
+		
+		');
 	};
 }
