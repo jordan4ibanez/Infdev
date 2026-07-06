5dcab554f58d093a19e202a1f9b23180525ea564
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-06 16:23:52 -0400

    copy this

--- src/game/node/dirt/DirtSound.hx
+++ src/game/node/dirt/DirtSound.hx
@@ -1,2 +1,21 @@
 package src.game.node.dirt;
 
+import src.engine.definition.sound.NodeSoundTable;
+import src.engine.definition.sound.SimpleSoundSpecTable;
+
+@:final
+abstract class DirtSound {
+	public static function get(): NodeSoundTable {
+		return new NodeSoundTable()
+			.setDig(new SimpleSoundSpecTable("stone_mine")
+				.setPitch(0.9))
+			.setDug(new SimpleSoundSpecTable("stone_mine")
+				.setPitch(0.75))
+			.setFootstep(new SimpleSoundSpecTable("stone_step")
+				.setPitch(0.8))
+			.setPlace(new SimpleSoundSpecTable("stone_mine")
+				.setPitch(0.65))
+			.setPlaceFailed(new SimpleSoundSpecTable("stone_mine")
+				.setPitch(0.4));
+	}
+}
