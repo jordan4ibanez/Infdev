c37603447b0d42a900125093d6d432102450d050
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-05 22:30:01 -0400

    A global max level for things

---
+++ src/engine/definition/MaxLevel.hx
@@ -0,0 +1,7 @@
+package src.engine.definition;
+
+// Why 30? I have no idea.
+// Max level for everything. Tools, nodes, items, etc.
+// Bedrock ignores this.
+// @:noCompletion
+inline final MAX_LEVEL = 30;
