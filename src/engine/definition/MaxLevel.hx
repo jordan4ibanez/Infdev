f1a8a690659910970b7ba5671d9905ae7c4cbbf9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-05 22:34:30 -0400

    Now silence this

--- src/engine/definition/MaxLevel.hx
+++ src/engine/definition/MaxLevel.hx
@@ -3,5 +3,5 @@ package src.engine.definition;
 // Why 30? I have no idea.
 // Max level for everything. Tools, nodes, items, etc.
 // Bedrock ignores this.
-// @:noCompletion
+@:noCompletion
 inline final MAX_LEVEL = 30;
