8a1af49e5aa9b9e1d82b0cf277d4e64557caf33a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-06 16:52:50 -0400

    this.this.this.this.this.this.this.

--- src/game/node/WaterFlow.hx
+++ src/game/node/WaterFlow.hx
@@ -11,11 +11,11 @@ final class WaterFlow extends NodeDefinition {
 	public function new() {
 		super();
 
-		description = "Flowing Water";
-		drawType = DrawTypeFlowingLiquid;
-		waving = WavingTypeLiquids;
-		tiles = ["default_water.png"];
-		specialTiles = [
+		this.description = "Flowing Water";
+		this.drawType = DrawTypeFlowingLiquid;
+		this.waving = WavingTypeLiquids;
+		this.tiles = ["default_water.png"];
+		this.specialTiles = [
 			new TileDefinition("default_water_flowing_animated.png")
 				.setBackfaceCulling(false)
 				.setAnimation(new TileAnimationDefinitionVerticalFrames(16, 16, 0.5)),
@@ -23,22 +23,22 @@ final class WaterFlow extends NodeDefinition {
 				.setBackfaceCulling(true)
 				.setAnimation(new TileAnimationDefinitionVerticalFrames(16, 16, 0.5))
 		];
-		useTextureAlpha = NodeTextureAlphaBlend;
-		paramtype1 = ParamType1Light;
-		paramtype2 = ParamType2FlowingLiquid;
-		walkable = false;
-		pointable = false;
-		diggable = false;
-		buildableTo = true;
-		isGroundContent = false;
-		drop = "";
-		drowning = 1;
-		liquidType = LiquidTypeFlowing;
-		liquidAlternativeFlowing = "infdev:water_flow";
-		liquidAlternativeSource = "infdev:water_source";
-		liquidViscosity = 1;
-		postEffectColor = new RGBA(30, 60, 90, 103);
-		nodeGroups = [
+		this.useTextureAlpha = NodeTextureAlphaBlend;
+		this.paramtype1 = ParamType1Light;
+		this.paramtype2 = ParamType2FlowingLiquid;
+		this.walkable = false;
+		this.pointable = false;
+		this.diggable = false;
+		this.buildableTo = true;
+		this.isGroundContent = false;
+		this.drop = "";
+		this.drowning = 1;
+		this.liquidType = LiquidTypeFlowing;
+		this.liquidAlternativeFlowing = "infdev:water_flow";
+		this.liquidAlternativeSource = "infdev:water_source";
+		this.liquidViscosity = 1;
+		this.postEffectColor = new RGBA(30, 60, 90, 103);
+		this.nodeGroups = [
 			NodeGroupLiquidFlow => 1,
 			NodeGroupWater => 1
 		];
