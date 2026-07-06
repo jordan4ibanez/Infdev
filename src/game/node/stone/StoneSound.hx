01e60d59490c9dbf7a625a5c99e1387c2678880e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-06 00:57:08 -0400

    REALLY stretching that one sound effect lmaooo

--- src/game/node/stone/StoneSound.hx
+++ src/game/node/stone/StoneSound.hx
@@ -1,5 +1,21 @@
 package src.game.node.stone;
 
-final class StoneSound {
-    
+import src.engine.definition.sound.NodeSoundTable;
+import src.engine.definition.sound.SimpleSoundSpecTable;
+
+@:final
+abstract class StoneSound {
+	public static function get(): NodeSoundTable {
+		return new NodeSoundTable()
+			.setDig(new SimpleSoundSpecTable("stone_mine")
+				.setPitch(0.9))
+			.setDug(new SimpleSoundSpecTable("stone_mine")
+				.setPitch(0.75))
+			.setFootstep(new SimpleSoundSpecTable("stone_mine")
+				.setPitch(1.1))
+			.setPlace(new SimpleSoundSpecTable("stone_mine")
+				.setPitch(0.65))
+			.setPlaceFailed(new SimpleSoundSpecTable("stone_mine")
+				.setPitch(0.4));
+	}
 }
