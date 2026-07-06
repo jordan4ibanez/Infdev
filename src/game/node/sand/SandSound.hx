6b9210d437386c68cf26a7ee97d531fb6e884ce9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-06 16:57:33 -0400

    sand

--- src/game/node/sand/SandSound.hx
+++ src/game/node/sand/SandSound.hx
@@ -1,6 +1,5 @@
 package src.game.node.sand;
 
-
 import src.engine.definition.sound.NodeSoundTable;
 import src.engine.definition.sound.SimpleSoundSpecTable;
 
@@ -8,16 +7,16 @@ import src.engine.definition.sound.SimpleSoundSpecTable;
 abstract class SandSound {
 	public static function get(): NodeSoundTable {
 		return new NodeSoundTable()
-			.setDig(new SimpleSoundSpecTable("dirt_mine")
+			.setDig(new SimpleSoundSpecTable("sand_mine")
 				.setPitch(0.9))
-			.setDug(new SimpleSoundSpecTable("dirt_mine")
+			.setDug(new SimpleSoundSpecTable("sand_mine")
 				.setPitch(0.75))
-			.setFootstep(new SimpleSoundSpecTable("dirt_step")
+			.setFootstep(new SimpleSoundSpecTable("sand_step")
 				.setPitch(0.8)
 				.setGain(0.5))
-			.setPlace(new SimpleSoundSpecTable("dirt_mine")
+			.setPlace(new SimpleSoundSpecTable("sand_mine")
 				.setPitch(0.65))
-			.setPlaceFailed(new SimpleSoundSpecTable("dirt_mine")
+			.setPlaceFailed(new SimpleSoundSpecTable("sand_mine")
 				.setPitch(0.4));
 	}
 }
