98c6a2bcfe7b349d2c5c030b316cdba7c9b54ddb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-06 16:52:41 -0400

    import

--- src/game/node/WaterFlow.hx
+++ src/game/node/WaterFlow.hx
@@ -1,11 +1,9 @@
 package src.game.node;
 
+import src.engine.definition.NodeDefinition;
+import src.engine.definition.basic.TileDefinition;
 import src.engine.definition.graphics.RGBA;
 import src.engine.definition.graphics.TileAnimationDefinition.TileAnimationDefinitionVerticalFrames;
-import src.engine.definition.basic.NodeDropTable;
-import src.engine.definition.ToolCapabilities.GroupCapabilities;
-import src.engine.definition.basic.TileDefinition;
-import src.engine.definition.NodeDefinition;
 import src.game.groups.NodeGroup;
 
 @:register("infdev:water_flow")
