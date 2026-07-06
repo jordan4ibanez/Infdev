061c64763cbbe3a301c504b274941b9d2fa63f2e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-05 23:19:55 -0400

    delete it

--- src/game/tool/Hand.hx
+++ src/game/tool/Hand.hx
@@ -7,24 +7,6 @@ import src.game.groups.NodeGroup;
 
 @:override("")
 final class Hand extends ToolDefinition {
-	function infillMap(min: Int, max: Int, baseTime: Float, increment: Float, ?customValues: LuaMap<Int, Float>): LuaMap<Int, Float> {
-		var output = new LuaMap();
-
-		final minRange = min;
-		final maxRange = max + 1;
-
-		var currentTime = baseTime;
-
-		for (i in minRange...maxRange) {
-			output[i] = currentTime;
-			currentTime += increment;
-		}
-
-		// Custom values overwrite.
-
-		return output;
-	}
-
 	public function new() {
 		super();
 		wieldScale = new Vec3(1, 1, 1);
