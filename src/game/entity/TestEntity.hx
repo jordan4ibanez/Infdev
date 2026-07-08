31b3a7b3eca04e8c9f1fe0dc28002f93ee12339a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-08 02:42:27 -0400

    That's fucking nuts

--- src/game/entity/TestEntity.hx
+++ src/game/entity/TestEntity.hx
@@ -10,8 +10,30 @@ class TestEntity extends LuaEntity {
 
 		this.object.setProperties(new ObjectProperties()
 			.setVisual(EntityVisualMesh)
-			.setMesh("character.glb"));
+			.setMesh("character.glb")
+			.setTextures(["character.png"]));
 
-		this.object.playAnimation("walk", {speed: 20});
+		this.object.playAnimation("walk", {
+			speed: 1,
+			priority: 1
+		});
+
+		this.object.playAnimation("mine", {
+			priority: 2
+		});
+
+		// so this would be like -90 to 90 + 90 / 180
+		// I think this animation may need to be inverted
+		this.object.playAnimation("look_pitch", {
+			speed: 0,
+			min_frame: 0.7,
+			max_frame: 0.7,
+		});
+
+		this.object.playAnimation("look_yaw", {
+			speed: 0,
+			min_frame: 0.35,
+			max_frame: 0.35,
+		});
 	}
 }
