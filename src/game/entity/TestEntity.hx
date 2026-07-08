48894c45d5473d92e2a20a0c4d17c9f8cff06d02
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-08 02:46:02 -0400

    That looks awesome

--- src/game/entity/TestEntity.hx
+++ src/game/entity/TestEntity.hx
@@ -19,7 +19,8 @@ class TestEntity extends LuaEntity {
 		});
 
 		this.object.playAnimation("mine", {
-			priority: 2
+			priority: 2,
+            speed: 3
 		});
 
 		// so this would be like -90 to 90 + 90 / 180
