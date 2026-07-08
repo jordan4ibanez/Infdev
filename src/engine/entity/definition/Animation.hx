f27e6cbf689473e1ddce87758c75c866843bce8c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-08 00:58:15 -0400

    Update Animation.hx

--- src/engine/entity/definition/Animation.hx
+++ src/engine/entity/definition/Animation.hx
@@ -1,2 +1,27 @@
 package src.engine.entity.definition;
 
+typedef Animation = {
+	@:native("min_frame")
+	@:optional
+	var minFrame: Float;
+
+	@:native("max_frame")
+	@:optional
+	var maxFrame: Float;
+
+	@:native("start_frame")
+	@:optional
+	var startFrame: Float;
+
+	@:optional
+	var speed: Float;
+
+	@:optional
+	var loop: Bool;
+
+	@:optional
+	var blend: Float;
+
+	@:optional
+	var priority: Int;
+}
