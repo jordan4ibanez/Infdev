99437d7f71a77c115ad98d5541468f68ce25726a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-10 01:26:58 -0400

    It's a human!

--- src/game/entity/Human.hx
+++ src/game/entity/Human.hx
@@ -3,37 +3,20 @@ package src.game.entity;
 import src.engine.entity.LuaEntity;
 import src.engine.entity.ObjectProperties;
 
-@:luantiEntity("infdev:test")
-class TestEntity extends LuaEntity {
+@:luantiEntity("infdev:human")
+class Human extends LuaEntity {
 	override function onActivate(staticData: String, dtimeS: Float) {
 		super.onActivate(staticData, dtimeS);
 
 		this.object.setProperties(new ObjectProperties()
 			.setVisual(EntityVisualMesh)
 			.setMesh("character.glb")
-			.setTextures(["character.png"]));
+			.setTextures(["character.png"])
+			.setMakesFootstepSound(true)
+			.setAutomaticFaceMovementDir(1.0));
 
-		this.object.playAnimation("walk", {
+		this.object.playAnimation("human", {
 			speed: 1,
-			priority: 1
-		});
-
-		this.object.playAnimation("mine", {
-			priority: 2,
-		});
-
-		// so this would be like -90 to 90 + 90 / 180
-		// I think this animation may need to be inverted
-		this.object.playAnimation("look_pitch", {
-			speed: 0,
-			min_frame: 0.7,
-			max_frame: 0.7,
-		});
-
-		this.object.playAnimation("look_yaw", {
-			speed: 0,
-			min_frame: 0.35,
-			max_frame: 0.35,
 		});
 	}
 }
