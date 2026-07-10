b7c9b2556f7e058db4855e49334bbda78dd4eebd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-10 02:27:08 -0400

    I'm pretty sure that's how a human acts

--- src/game/entity/Human.hx
+++ src/game/entity/Human.hx
@@ -1,10 +1,14 @@
 package src.game.entity;
 
+import lua.Math;
 import src.engine.entity.LuaEntity;
 import src.engine.entity.ObjectProperties;
+import src.engine.vector.Vec3;
 
 @:luantiEntity("infdev:human")
 class Human extends LuaEntity {
+	var velocityVector = new Vec3();
+
 	override function onActivate(staticData: String, dtimeS: Float) {
 		super.onActivate(staticData, dtimeS);
 
@@ -13,10 +17,26 @@ class Human extends LuaEntity {
 			.setMesh("character.glb")
 			.setTextures(["character.png"])
 			.setMakesFootstepSound(true)
-			.setAutomaticFaceMovementDir(1.0));
+			.setPhysical(true));
+
+		this.object.setAcceleration(new Vec3(0, -10, 0));
 
 		this.object.playAnimation("human", {
 			speed: 1,
 		});
+	}
+
+	override function onStep(delta: Float, moveResult: Dynamic) {
+		super.onStep(delta, moveResult);
+
+		velocityVector.x = Math.random() * Math.random(-1, 1);
+		velocityVector.z = Math.random() * Math.random(-1, 1);
+		this.object.addVelocity(velocityVector);
+
+		var vel = this.object.getVelocity();
+
+		var yaw = untyped __lua__("core.dir_to_yaw(vel)");
+
+		this.object.setYaw(yaw);
 	}
 }
