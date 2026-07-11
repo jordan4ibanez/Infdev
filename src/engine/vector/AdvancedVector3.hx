183715b8c7d12cd84377b89da36fc1a13bbc4d93
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-11 19:06:49 -0400

    Update AdvancedVector3.hx

--- src/engine/vector/AdvancedVector3.hx
+++ src/engine/vector/AdvancedVector3.hx
@@ -2,8 +2,11 @@ package src.engine.vector;
 
 import lua.Table;
 
+// This is a completely virtual class.
 abstract AdvancedVector3(Table<String, Float>) {
-	public inline function new() {
-		this = Table.create();
+	public inline function new(x: Float, y: Float, z: Float) {
+		this = (untyped __lua__("vector.new({0}, {1}, {2})", x, y, z));
+	}
+}
 	}
 }
