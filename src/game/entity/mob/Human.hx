8eae0e0388d6942d8ea91ad2dc65fc28dc427834
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-10 23:51:07 -0400

    That is some serious power

--- src/game/entity/mob/Human.hx
+++ src/game/entity/mob/Human.hx
@@ -6,7 +6,8 @@ import src.engine.vector.Vec3;
 
 @:luantiEntity("infdev:human")
 class Human extends Mob {
-	var velocityVector = new Vec3();
+	var velocityVector: Vec3 = new Vec3();
+	var turnTimer: Float = 0;
 
 	override function onActivate(staticData: String, dtimeS: Float) {
 		super.onActivate(staticData, dtimeS);
@@ -37,5 +38,10 @@ class Human extends Mob {
 		var yaw = untyped __lua__("core.dir_to_yaw(vel)");
 
 		this.object.setYaw(yaw);
+	}
+
+	override function setHP(hp: Int) {
+		trace('The HP of human ${this.object.getGUID()} is now $hp');
+		this.hp = hp;
 	}
 }
