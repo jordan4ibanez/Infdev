a3157c74d082af6f73fb348d940a285b29691da4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-12 18:57:14 -0400

    Update Vector.hx

--- src/engine/vector/Vector.hx
+++ src/engine/vector/Vector.hx
@@ -117,6 +117,7 @@ extern class Vector {
 	public static inline function inArea(pos: Vec3, min: Vec3, max: Vec3): Bool {
 		return untyped __lua__("vector.in_area({0}, {1}, {2})", pos, min, max);
 	}
+
 	// ? For the following functions x can be either a vector or a number:
 	public static inline function add(v: Vec3, x: EitherType<Vec3, Float>): Vec3 {
 		return untyped __lua__("vector.add({0}, {1})", v, x);
