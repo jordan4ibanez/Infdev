660c36d54fb0efe8363b6883e26585e53871edb6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-12 19:13:35 -0400

    time to oopify

--- src/engine/vector/Vec3.hx
+++ src/engine/vector/Vec3.hx
@@ -77,6 +77,86 @@ abstract Vec3({
 	public inline function copy(): Vec3 {
 		return untyped __lua__("vector.copy({0})", this);
 	}
+
+
+	public static inline function direction(p1: Vec3, p2: Vec3): Vec3 {
+		return untyped __lua__("vector.direction({0}, {1})", p1, p2);
+	}
+
+	public static inline function distance(p1: Vec3, p2: Vec3): Float {
+		return untyped __lua__("vector.distance({0}, {1})", p1, p2);
+	}
+
+	public static inline function length(v: Vec3): Float {
+		return untyped __lua__("vector.length({0})", v);
+	}
+
+	public static inline function normalize(v: Vec3): Vec3 {
+		return untyped __lua__("vector.normalize({0})", v);
+	}
+
+	public static inline function floor(v: Vec3): Vec3 {
+		return untyped __lua__("vector.floor({0})", v);
+	}
+
+	public static inline function ceil(v: Vec3): Vec3 {
+		return untyped __lua__("vector.ceil({0})", v);
+	}
+
+	public static inline function round(v: Vec3): Vec3 {
+		return untyped __lua__("vector.round({0})", v);
+	}
+
+	public static inline function sign(v: Vec3, tolerance: Float): Vec3 {
+		return untyped __lua__("vector.sign({0}, {1})", v, tolerance);
+	}
+
+	public static inline function abs(v: Vec3): Vec3 {
+		return untyped __lua__("vector.abs({0})", v);
+	}
+
+	public static inline function apply(v: Vec3, func: (Float) -> Void, whatever: Rest<Dynamic>): Vec3 {
+		return untyped __lua__("vector.apply({0}, {1}, table.unpack({2}))", v, func, whatever);
+	}
+
+	// I literally have no idea what vector.combine does.
+	public static inline function equals(v1: Vec3, v2: Vec3): Bool {
+		return untyped __lua__("vector.equals({0}, {1})", v1, v2);
+	}
+
+	public static inline function dot(v1: Vec3, v2: Vec3): Float {
+		return untyped __lua__("vector.dot({0}, {1})", v1, v2);
+	}
+
+	public static inline function check(v: Vec3): Bool {
+		return untyped __lua__("vector.check({0})", v);
+	}
+
+	public static inline function inArea(pos: Vec3, min: Vec3, max: Vec3): Bool {
+		return untyped __lua__("vector.in_area({0}, {1}, {2})", pos, min, max);
+	}
+
+	// ?
+	// ?
+	// ? For the following functions x can be either a vector or a number:
+	// ?
+	// ?
+	public static inline function add(v: Vec3, x: EitherType<Vec3, Float>): Vec3 {
+		return untyped __lua__("vector.add({0}, {1})", v, x);
+	}
+
+	public static inline function subtract(v: Vec3, x: EitherType<Vec3, Float>): Vec3 {
+		return untyped __lua__("vector.subtract({0}, {1})", v, x);
+	}
+
+	// But multiply and divide using a float is deprectated??
+	public static inline function multiply(v: Vec3, x: Vec3): Vec3 {
+		return untyped __lua__("vector.multiply({0}, {1})", v, x);
+	}
+
+	public static inline function divide(v: Vec3, x: Vec3): Vec3 {
+		return untyped __lua__("vector.divide({0}, {1})", v, x);
+	}
 }
 
 class TestIt {
