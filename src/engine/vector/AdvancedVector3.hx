3b0b2f59194741e8b6e4f66006bfc6517c11931d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-11 20:52:07 -0400

    Update AdvancedVector3.hx

--- src/engine/vector/AdvancedVector3.hx
+++ src/engine/vector/AdvancedVector3.hx
@@ -54,6 +54,10 @@ abstract AdvancedVector3({
 	public static inline function rotateAroundAxis(v: AdvancedVector3, r: AdvancedVector3, a: Float): AdvancedVector3 {
 		return untyped __lua__("vector.rotate_around_axis({0}, {1}, {2})", min, max, a);
 	}
+
+	public static inline function dirToRotation(direction: AdvancedVector3, ?up: AdvancedVector3): AdvancedVector3 {
+		return untyped __lua__("vector.dir_to_rotation({0}, {1})", direction, up);
+	}
 }
 
 class TestIt {
