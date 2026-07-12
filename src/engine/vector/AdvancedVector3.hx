26a8f7718783ef2cdab653aa83e8b94773d043a7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-11 21:12:50 -0400

    everything is broken hooray!

--- src/engine/vector/AdvancedVector3.hx
+++ src/engine/vector/AdvancedVector3.hx
@@ -13,7 +13,7 @@ abstract AdvancedVector3({
 	}
 
 	// ? Functions
-	public inline function sort(v1: AdvancedVector3, v2: AdvancedVector3): {
+	public inline function sort(v2: AdvancedVector3): {
 		var first: AdvancedVector3;
 		var second: AdvancedVector3;
 	} {
@@ -24,28 +24,28 @@ abstract AdvancedVector3({
 		};
 	}
 
-	public inline function angle(v1: AdvancedVector3, v2: AdvancedVector3): Float {
+	public inline function angle(v2: AdvancedVector3): Float {
 		return untyped __lua__("vector.angle({0}, {1})", v1, v2);
 	}
 
-	public inline function cross(v1: AdvancedVector3, v2: AdvancedVector3): Float {
+	public inline function cross(v2: AdvancedVector3): Float {
 		return untyped __lua__("vector.cross({0}, {1})", v1, v2);
 	}
 
-	public inline function offset(v: AdvancedVector3, x: Float, y: Float, z: Float): AdvancedVector3 {
+	public inline function offset(x: Float, y: Float, z: Float): AdvancedVector3 {
 		return untyped __lua__("vector.offset({0}, {1}, {2}, {3})", v1, x, y, z);
 	}
 
-	// ? Operators (just kinda works)
+	// ? Operators (just kinda work)
 	//
 	//
 	//
 	// ? Rotation-related functions
-	public inline function rotate(v: AdvancedVector3, r: AdvancedVector3): AdvancedVector3 {
+	public inline function rotate(r: AdvancedVector3): AdvancedVector3 {
 		return untyped __lua__("vector.rotate({0}, {1})", min, max);
 	}
 
-	public inline function rotateAroundAxis(v: AdvancedVector3, r: AdvancedVector3, a: Float): AdvancedVector3 {
+	public inline function rotateAroundAxis(r: AdvancedVector3, a: Float): AdvancedVector3 {
 		return untyped __lua__("vector.rotate_around_axis({0}, {1}, {2})", min, max, a);
 	}
 }
