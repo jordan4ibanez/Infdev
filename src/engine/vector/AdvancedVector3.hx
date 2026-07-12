c4ce2007d895ec2eddc0438ccbab74aa232eae08
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-11 21:09:27 -0400

    And you go in there

--- src/engine/vector/AdvancedVector3.hx
+++ src/engine/vector/AdvancedVector3.hx
@@ -1,7 +1,5 @@
 package src.engine.vector;
 
-import src.engine.compilercode.LuaArray;
-
 // This is a completely virtual class.
 abstract AdvancedVector3({
 	var x: Float;
@@ -10,68 +8,5 @@ abstract AdvancedVector3({
 }) {
 	public inline function new(x: Float, y: Float, z: Float) {
 		this = untyped __lua__("vector.new({0}, {1}, {2})", x, y, z);
-	}
-}
-
-// This is a completely virtual class.
-
-@:native("vector")
-extern class Vector {
-	// ? Functions
-	public static inline function sort(v1: AdvancedVector3, v2: AdvancedVector3): {
-		var first: AdvancedVector3;
-		var second: AdvancedVector3;
-	} {
-		var temp: LuaArray<AdvancedVector3> = untyped __lua__("{vector.sort({0}, {1})}", v1, v2);
-		return {
-			first: temp[0],
-			second: temp[1]
-		};
-	}
-
-	public static inline function angle(v1: AdvancedVector3, v2: AdvancedVector3): Float {
-		return untyped __lua__("vector.angle({0}, {1})", v1, v2);
-	}
-
-	public static inline function cross(v1: AdvancedVector3, v2: AdvancedVector3): Float {
-		return untyped __lua__("vector.cross({0}, {1})", v1, v2);
-	}
-
-	public static inline function offset(v: AdvancedVector3, x: Float, y: Float, z: Float): AdvancedVector3 {
-		return untyped __lua__("vector.offset({0}, {1}, {2}, {3})", v1, x, y, z);
-	}
-
-	public static inline function randomInArea(min: AdvancedVector3, max: AdvancedVector3): Float {
-		return untyped __lua__("vector.random_in_area({0}, {1})", min, max);
-	}
-
-	// ? Operators (just kinda works)
-	//
-	//
-	//
-	// ? Rotation-related functions
-	public static inline function rotate(v: AdvancedVector3, r: AdvancedVector3): AdvancedVector3 {
-		return untyped __lua__("vector.rotate({0}, {1})", min, max);
-	}
-
-	public static inline function rotateAroundAxis(v: AdvancedVector3, r: AdvancedVector3, a: Float): AdvancedVector3 {
-		return untyped __lua__("vector.rotate_around_axis({0}, {1}, {2})", min, max, a);
-	}
-
-	public static inline function dirToRotation(direction: AdvancedVector3, ?up: AdvancedVector3): AdvancedVector3 {
-		return untyped __lua__("vector.dir_to_rotation({0}, {1})", direction, up);
-	}
-}
-
-class TestIt {
-	static function __init__() {
-		var v1 = new AdvancedVector3(1, 1, 1);
-		var v2 = new AdvancedVector3(100, 199, 123123);
-
-		var sort = Vector.randomInArea(v1, v2);
-
-		untyped print(sort);
-
-		Core.requestShutdown("heck");
 	}
 }
