33b8fb737c8923e0e8990a69f89061030cb94f73
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-12 19:40:49 -0400

    Update ValueNoiseMap.hx

--- src/engine/ValueNoiseMap.hx
+++ src/engine/ValueNoiseMap.hx
@@ -1,8 +1,8 @@
 package src.engine;
 
-import src.engine.vector.Vec3;
 import src.engine.compilercode.LuaArray;
-import src.engine.vector.EngineVector2;
+import src.engine.vector.Vec2;
+import src.engine.vector.Vec3;
 
 @:final
 abstract extern class ValueNoiseMap {
@@ -13,13 +13,13 @@ abstract extern class ValueNoiseMap {
 	public function get3DMap(pos: Vec3): LuaArray<LuaArray<LuaArray<Float>>>;
 
 	@:native("get_2d_map_flat")
-	public function get2DMapFlat(pos: EngineVector2, ?buffer: LuaArray<Float>): LuaArray<Float>;
+	public function get2DMapFlat(pos: Vec2, ?buffer: LuaArray<Float>): LuaArray<Float>;
 
 	@:native("get_3d_map_flat")
 	public function get3DMapFlat(pos: Vec3, ?buffer: LuaArray<Float>): LuaArray<Float>;
 
 	@:native("calc_2d_map")
-	public function calc2DMap(pos: EngineVector2): Void;
+	public function calc2DMap(pos: Vec2): Void;
 
 	@:native("calc_3d_map")
 	public function calc3DMap(pos: Vec3): Void;
