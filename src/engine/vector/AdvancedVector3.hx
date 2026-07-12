1fc54b4ae7dd8e67ebd4a3c4e2a633be2ebf0c42
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-11 20:24:39 -0400

    Whoops meta classes

--- src/engine/vector/AdvancedVector3.hx
+++ src/engine/vector/AdvancedVector3.hx
@@ -23,12 +23,16 @@ abstract AdvancedVector3({
 		};
 	}
 
-	public static function angle(v1: AdvancedVector3, v2: AdvancedVector3): Float {
+	public static inline function angle(v1: AdvancedVector3, v2: AdvancedVector3): Float {
 		return untyped __lua__("vector.angle({0}, {1})", v1, v2);
 	}
 
-	public static function cross(v1: AdvancedVector3, v2: AdvancedVector3): Float {
+	public static inline function cross(v1: AdvancedVector3, v2: AdvancedVector3): Float {
 		return untyped __lua__("vector.cross({0}, {1})", v1, v2);
+	}
+
+	public static inline function offset(v: AdvancedVector3, x: Float, y: Float, z: Float): AdvancedVector3 {
+		return untyped __lua__("vector.offset({0}, {1}, {2}, {3})", v1, x, y, z);
 	}
 }
 
