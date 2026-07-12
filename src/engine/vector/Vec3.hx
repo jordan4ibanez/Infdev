efd7fd1a0bed7bc44531c45384d07b331010aad9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-11 21:40:19 -0400

    No longer need this

--- src/engine/vector/Vec3.hx
+++
@@ -1,4 +0,0 @@
-package src.engine.vector;
-
-// ? note: this is using functions converted to methods from: https://github.com/luanti-org/luanti/blob/master/builtin/common/vector.lua
-class Vec3 extends EngineVector3 {}
