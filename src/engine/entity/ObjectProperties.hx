86635bb18a1c4d8a3c48a8b22c234b29972423d9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-12 22:07:01 -0400

    Yep I am done with this being a class

--- src/engine/entity/ObjectProperties.hx
+++ src/engine/entity/ObjectProperties.hx
@@ -9,9 +9,8 @@ import src.engine.entity.definition.EntitySelectionBox;
 import src.engine.entity.definition.EntityVisual;
 import src.engine.entity.definition.StepUpMode;
 import src.engine.vector.Vec2;
-import src.engine.vector.Vec3;
 
-class ObjectProperties {
+typedef ObjectProperties = {
 	@:native("hp_max")
 	public var hpMax: Int;
 
@@ -118,196 +117,157 @@ class ObjectProperties {
 
 	@:native("step_up_mode")
 	public var stepUpMode: StepUpMode;
-
-	public inline function setHPMax(hpMax: Int): ObjectProperties {
-		this.hpMax = hpMax;
-		return this;
-	}
-
-	public inline function setBreathMax(breathMax: Int): ObjectProperties {
-		this.breathMax = breathMax;
-		return this;
-	}
-
-	public inline function setZoomFOV(zoomFOV: Float): ObjectProperties {
-		this.zoomFOV = zoomFOV;
-		return this;
-	}
-
-	public inline function setEyeHeight(eyeHeight: Float): ObjectProperties {
-		this.eyeHeight = eyeHeight;
-		return this;
-	}
-
-	public inline function setPhysical(physical: Bool): ObjectProperties {
-		this.physical = physical;
-		return this;
-	}
-
-	public inline function setCollideWithObjects(collideWithObjects: Bool): ObjectProperties {
-		this.collideWithObjects = collideWithObjects;
-		return this;
-	}
-
-	public inline function setCollisionBox(collisionBox: EntityCollisionBox): ObjectProperties {
-		this.collisionBox = collisionBox;
-		return this;
-	}
-
-	public inline function setSelectionBox(selectionBox: EntitySelectionBox): ObjectProperties {
-		this.selectionBox = selectionBox;
-		return this;
-	}
-
-	public inline function setPointable(pointable: EntityPointable): ObjectProperties {
-		this.pointable = pointable;
-		return this;
-	}
-
-	public inline function setVisual(visual: EntityVisual): ObjectProperties {
-		this.visual = visual;
-		return this;
-	}
-
-	public inline function setWieldItem(wieldItem: String): ObjectProperties {
-		this.wieldItem = wieldItem;
-		return this;
-	}
-
-	public inline function setVisualSize(visualSize: EitherType<Vec2, Vec3>): ObjectProperties {
-		this.visualSize = visualSize;
-		return this;
-	}
-
-	public inline function setMesh(mesh: String): ObjectProperties {
-		this.mesh = mesh;
-		return this;
-	}
-
-	public inline function setTextures(textures: LuaArray<String>): ObjectProperties {
-		this.textures = textures;
-		return this;
-	}
-
-	public inline function setColors(colors: LuaArray<ColorSpec>): ObjectProperties {
-		this.colors = colors;
-		return this;
-	}
-
-	public inline function setNode(node: NodeTable): ObjectProperties {
-		this.node = node;
-		return this;
-	}
-
-	public inline function setUseTextureAlpha(useTextureAlpha: Bool): ObjectProperties {
-		this.useTextureAlpha = useTextureAlpha;
-		return this;
-	}
-
-	public inline function setSpriteDiv(spriteDiv: Vec2): ObjectProperties {
-		this.spriteDiv = spriteDiv;
-		return this;
-	}
-
-	public inline function setInitialSpriteBasePos(initialSpriteBasePos: Vec2): ObjectProperties {
-		this.initialSpriteBasePos = initialSpriteBasePos;
-		return this;
-	}
-
-	public inline function setIsVisible(isVisible: Bool): ObjectProperties {
-		this.isVisible = isVisible;
-		return this;
-	}
-
-	public inline function setMakesFootstepSound(makesFootstepSound: Bool): ObjectProperties {
-		this.makesFootstepSound = makesFootstepSound;
-		return this;
-	}
-
-	public inline function setAutomaticRotate(automaticRotate: Float): ObjectProperties {
-		this.automaticRotate = automaticRotate;
-		return this;
-	}
-
-	public inline function setStepHeight(stepHeight: Float): ObjectProperties {
-		this.stepHeight = stepHeight;
-		return this;
-	}
-
-	public inline function setAutomaticFaceMovementDir(automaticFaceMovementDir: EitherType<Float, Bool>): ObjectProperties {
-		this.automaticFaceMovementDir = automaticFaceMovementDir;
-		return this;
-	}
-
-	public inline function setAutomaticFaceMovementMaxRotationPerSec(automaticFaceMovementMaxRotationPerSec: Float): ObjectProperties {
-		this.automaticFaceMovementMaxRotationPerSec = automaticFaceMovementMaxRotationPerSec;
-		return this;
-	}
-
-	public inline function setBackfaceCulling(backfaceCulling: Bool): ObjectProperties {
-		this.backfaceCulling = backfaceCulling;
-		return this;
-	}
-
-	public inline function setGlow(glow: Int): ObjectProperties {
-		this.glow = glow;
-		return this;
-	}
-
-	public inline function setNametag(nametag: String): ObjectProperties {
-		this.nametag = nametag;
-		return this;
-	}
-
-	public inline function setNametagColor(nametagColor: ColorSpec): ObjectProperties {
-		this.nametagColor = nametagColor;
-		return this;
-	}
-
-	public inline function setNametagBackgroundColor(nametagBackgroundColor: EitherType<ColorSpec, Bool>): ObjectProperties {
-		this.nametagBackgroundColor = nametagBackgroundColor;
-		return this;
-	}
-
-	public inline function setNametagFontSize(nametagFontSize: EitherType<Int, Bool>): ObjectProperties {
-		this.nametagFontSize = nametagFontSize;
-		return this;
-	}
-
-	public inline function setNametagScaleZ(nametagScaleZ: Bool): ObjectProperties {
-		this.nametagScaleZ = nametagScaleZ;
-		return this;
-	}
-
-	public inline function setInfoText(infoText: String): ObjectProperties {
-		this.infoText = infoText;
-		return this;
-	}
-
-	public inline function setStaticSave(staticSave: Bool): ObjectProperties {
-		this.staticSave = staticSave;
-		return this;
-	}
-
-	public inline function setDamageTextureModifier(damageTextureModifier: String): ObjectProperties {
-		this.damageTextureModifier = damageTextureModifier;
-		return this;
-	}
-
-	public inline function setShaded(shaded: Bool): ObjectProperties {
-		this.shaded = shaded;
-		return this;
-	}
-
-	public inline function setShowOnMinimap(showOnMinimap: Bool): ObjectProperties {
-		this.showOnMinimap = showOnMinimap;
-		return this;
-	}
-
-	public inline function setStepUpMode(stepUpMode: StepUpMode): ObjectProperties {
-		this.stepUpMode = stepUpMode;
-		return this;
-	}
-
-	public function new() {}
+	// public inline function setHPMax(hpMax: Int): ObjectProperties {
+	// 	this.hpMax = hpMax;
+	// 	return this;
+	// }
+	// public inline function setBreathMax(breathMax: Int): ObjectProperties {
+	// 	this.breathMax = breathMax;
+	// 	return this;
+	// }
+	// public inline function setZoomFOV(zoomFOV: Float): ObjectProperties {
+	// 	this.zoomFOV = zoomFOV;
+	// 	return this;
+	// }
+	// public inline function setEyeHeight(eyeHeight: Float): ObjectProperties {
+	// 	this.eyeHeight = eyeHeight;
+	// 	return this;
+	// }
+	// public inline function setPhysical(physical: Bool): ObjectProperties {
+	// 	this.physical = physical;
+	// 	return this;
+	// }
+	// public inline function setCollideWithObjects(collideWithObjects: Bool): ObjectProperties {
+	// 	this.collideWithObjects = collideWithObjects;
+	// 	return this;
+	// }
+	// public inline function setCollisionBox(collisionBox: EntityCollisionBox): ObjectProperties {
+	// 	this.collisionBox = collisionBox;
+	// 	return this;
+	// }
+	// public inline function setSelectionBox(selectionBox: EntitySelectionBox): ObjectProperties {
+	// 	this.selectionBox = selectionBox;
+	// 	return this;
+	// }
+	// public inline function setPointable(pointable: EntityPointable): ObjectProperties {
+	// 	this.pointable = pointable;
+	// 	return this;
+	// }
+	// public inline function setVisual(visual: EntityVisual): ObjectProperties {
+	// 	this.visual = visual;
+	// 	return this;
+	// }
+	// public inline function setWieldItem(wieldItem: String): ObjectProperties {
+	// 	this.wieldItem = wieldItem;
+	// 	return this;
+	// }
+	// public inline function setVisualSize(visualSize: EitherType<Vec2, Vec3>): ObjectProperties {
+	// 	this.visualSize = visualSize;
+	// 	return this;
+	// }
+	// public inline function setMesh(mesh: String): ObjectProperties {
+	// 	this.mesh = mesh;
+	// 	return this;
+	// }
+	// public inline function setTextures(textures: LuaArray<String>): ObjectProperties {
+	// 	this.textures = textures;
+	// 	return this;
+	// }
+	// public inline function setColors(colors: LuaArray<ColorSpec>): ObjectProperties {
+	// 	this.colors = colors;
+	// 	return this;
+	// }
+	// public inline function setNode(node: NodeTable): ObjectProperties {
+	// 	this.node = node;
+	// 	return this;
+	// }
+	// public inline function setUseTextureAlpha(useTextureAlpha: Bool): ObjectProperties {
+	// 	this.useTextureAlpha = useTextureAlpha;
+	// 	return this;
+	// }
+	// public inline function setSpriteDiv(spriteDiv: Vec2): ObjectProperties {
+	// 	this.spriteDiv = spriteDiv;
+	// 	return this;
+	// }
+	// public inline function setInitialSpriteBasePos(initialSpriteBasePos: Vec2): ObjectProperties {
+	// 	this.initialSpriteBasePos = initialSpriteBasePos;
+	// 	return this;
+	// }
+	// public inline function setIsVisible(isVisible: Bool): ObjectProperties {
+	// 	this.isVisible = isVisible;
+	// 	return this;
+	// }
+	// public inline function setMakesFootstepSound(makesFootstepSound: Bool): ObjectProperties {
+	// 	this.makesFootstepSound = makesFootstepSound;
+	// 	return this;
+	// }
+	// public inline function setAutomaticRotate(automaticRotate: Float): ObjectProperties {
+	// 	this.automaticRotate = automaticRotate;
+	// 	return this;
+	// }
+	// public inline function setStepHeight(stepHeight: Float): ObjectProperties {
+	// 	this.stepHeight = stepHeight;
+	// 	return this;
+	// }
+	// public inline function setAutomaticFaceMovementDir(automaticFaceMovementDir: EitherType<Float, Bool>): ObjectProperties {
+	// 	this.automaticFaceMovementDir = automaticFaceMovementDir;
+	// 	return this;
+	// }
+	// public inline function setAutomaticFaceMovementMaxRotationPerSec(automaticFaceMovementMaxRotationPerSec: Float): ObjectProperties {
+	// 	this.automaticFaceMovementMaxRotationPerSec = automaticFaceMovementMaxRotationPerSec;
+	// 	return this;
+	// }
+	// public inline function setBackfaceCulling(backfaceCulling: Bool): ObjectProperties {
+	// 	this.backfaceCulling = backfaceCulling;
+	// 	return this;
+	// }
+	// public inline function setGlow(glow: Int): ObjectProperties {
+	// 	this.glow = glow;
+	// 	return this;
+	// }
+	// public inline function setNametag(nametag: String): ObjectProperties {
+	// 	this.nametag = nametag;
+	// 	return this;
+	// }
+	// public inline function setNametagColor(nametagColor: ColorSpec): ObjectProperties {
+	// 	this.nametagColor = nametagColor;
+	// 	return this;
+	// }
+	// public inline function setNametagBackgroundColor(nametagBackgroundColor: EitherType<ColorSpec, Bool>): ObjectProperties {
+	// 	this.nametagBackgroundColor = nametagBackgroundColor;
+	// 	return this;
+	// }
+	// public inline function setNametagFontSize(nametagFontSize: EitherType<Int, Bool>): ObjectProperties {
+	// 	this.nametagFontSize = nametagFontSize;
+	// 	return this;
+	// }
+	// public inline function setNametagScaleZ(nametagScaleZ: Bool): ObjectProperties {
+	// 	this.nametagScaleZ = nametagScaleZ;
+	// 	return this;
+	// }
+	// public inline function setInfoText(infoText: String): ObjectProperties {
+	// 	this.infoText = infoText;
+	// 	return this;
+	// }
+	// public inline function setStaticSave(staticSave: Bool): ObjectProperties {
+	// 	this.staticSave = staticSave;
+	// 	return this;
+	// }
+	// public inline function setDamageTextureModifier(damageTextureModifier: String): ObjectProperties {
+	// 	this.damageTextureModifier = damageTextureModifier;
+	// 	return this;
+	// }
+	// public inline function setShaded(shaded: Bool): ObjectProperties {
+	// 	this.shaded = shaded;
+	// 	return this;
+	// }
+	// public inline function setShowOnMinimap(showOnMinimap: Bool): ObjectProperties {
+	// 	this.showOnMinimap = showOnMinimap;
+	// 	return this;
+	// }
+	// public inline function setStepUpMode(stepUpMode: StepUpMode): ObjectProperties {
+	// 	this.stepUpMode = stepUpMode;
+	// 	return this;
+	// }
+	// public function new() {}
 }
