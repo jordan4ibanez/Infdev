929f2eccf38ba107ed7f44c0f694a800c9baf870
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-12 21:38:21 -0400

    Squish

--- src/engine/vector/Vec2.hx
+++ src/engine/vector/Vec2.hx
@@ -13,21 +13,18 @@ abstract Vec2({
 	public inline function new(?x: Float, ?y: Float) {
 		var x = x ?? 0;
 		var y = y ?? 0;
-
 		this = untyped __lua__("vector2.new({0}, {1})", x, y);
 	}
 
 	public inline function set(otherVec: Vec2): Vec2 {
 		this.x = otherVec.x;
 		this.y = otherVec.y;
-
 		return cast this;
 	}
 
 	public inline function multiplyScalar(scalar: Float): Vec2 {
 		this.x *= scalar;
 		this.y *= scalar;
-
 		return cast this;
 	}
 
