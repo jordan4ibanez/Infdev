236651feec59d11c4cce6092d51032bfa18f8fbf
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-12 20:22:44 -0400

    Update Vec2.hx

--- src/engine/vector/Vec2.hx
+++ src/engine/vector/Vec2.hx
@@ -15,7 +15,7 @@ abstract Vec2({
 		var x = x ?? 0;
 		var y = y ?? 0;
 
-		this = untyped __lua__("vector.new({0}, {1})", x, y);
+		this = untyped __lua__("vector2.new({0}, {1})", x, y);
 	}
 
 	public inline function set(otherVec: Vec2): Vec2 {
@@ -39,7 +39,7 @@ abstract Vec2({
 	// !
 	// ? Functions
 	public inline function sort(v2: Vec2): {first: Vec2, second: Vec2} {
-		var temp: LuaArray<Vec2> = untyped __lua__("{vector.sort({0}, {1})}", this, v2);
+		var temp: LuaArray<Vec2> = untyped __lua__("{vector2.sort({0}, {1})}", this, v2);
 		return {
 			first: temp[0],
 			second: temp[1]
@@ -47,15 +47,15 @@ abstract Vec2({
 	}
 
 	public inline function angle(v2: Vec2): Float {
-		return untyped __lua__("vector.angle({0}, {1})", this, v2);
+		return untyped __lua__("vector2.angle({0}, {1})", this, v2);
 	}
 
 	public inline function cross(v2: Vec2): Float {
-		return untyped __lua__("vector.cross({0}, {1})", this, v2);
+		return untyped __lua__("vector2.cross({0}, {1})", this, v2);
 	}
 
 	public inline function offset(x: Float, y: Float, z: Float): Vec2 {
-		return untyped __lua__("vector.offset({0}, {1}, {2}, {3})", this, x, y, z);
+		return untyped __lua__("vector2.offset({0}, {1}, {2}, {3})", this, x, y, z);
 	}
 
 	// ? Operators (just kinda work)
@@ -64,73 +64,73 @@ abstract Vec2({
 	//
 	// ? Rotation-related functions
 	public inline function rotate(r: Vec2): Vec2 {
-		return untyped __lua__("vector.rotate({0}, {1})", this, r);
+		return untyped __lua__("vector2.rotate({0}, {1})", this, r);
 	}
 
 	public inline function rotateAroundAxis(v2: Vec2, a: Float): Vec2 {
-		return untyped __lua__("vector.rotate_around_axis({0}, {1}, {2})", this, v2, a);
+		return untyped __lua__("vector2.rotate_around_axis({0}, {1}, {2})", this, v2, a);
 	}
 
 	// ? Common to all vector types
 	public inline function copy(): Vec2 {
-		return untyped __lua__("vector.copy({0})", this);
+		return untyped __lua__("vector2.copy({0})", this);
 	}
 
 	public inline function direction(p2: Vec2): Vec2 {
-		return untyped __lua__("vector.direction({0}, {1})", this, p2);
+		return untyped __lua__("vector2.direction({0}, {1})", this, p2);
 	}
 
 	public inline function distance(p2: Vec2): Float {
-		return untyped __lua__("vector.distance({0}, {1})", this, p2);
+		return untyped __lua__("vector2.distance({0}, {1})", this, p2);
 	}
 
 	public inline function length(): Float {
-		return untyped __lua__("vector.length({0})", this);
+		return untyped __lua__("vector2.length({0})", this);
 	}
 
 	public inline function normalize(): Vec2 {
-		return untyped __lua__("vector.normalize({0})", this);
+		return untyped __lua__("vector2.normalize({0})", this);
 	}
 
 	public inline function floor(): Vec2 {
-		return untyped __lua__("vector.floor({0})", this);
+		return untyped __lua__("vector2.floor({0})", this);
 	}
 
 	public inline function ceil(): Vec2 {
-		return untyped __lua__("vector.ceil({0})", this);
+		return untyped __lua__("vector2.ceil({0})", this);
 	}
 
 	public inline function round(): Vec2 {
-		return untyped __lua__("vector.round({0})", this);
+		return untyped __lua__("vector2.round({0})", this);
 	}
 
 	public inline function sign(tolerance: Float): Vec2 {
-		return untyped __lua__("vector.sign({0}, {1})", this, tolerance);
+		return untyped __lua__("vector2.sign({0}, {1})", this, tolerance);
 	}
 
 	public inline function abs(): Vec2 {
-		return untyped __lua__("vector.abs({0})", this);
+		return untyped __lua__("vector2.abs({0})", this);
 	}
 
 	public inline function apply(func: (Float) -> Void, whatever: Rest<Dynamic>): Vec2 {
-		return untyped __lua__("vector.apply({0}, {1}, table.unpack({2}))", this, func, whatever);
+		return untyped __lua__("vector2.apply({0}, {1}, table.unpack({2}))", this, func, whatever);
 	}
 
-	// I literally have no idea what vector.combine does.
+	// I literally have no idea what vector2.combine does.
 	public inline function equals(v2: Vec2): Bool {
-		return untyped __lua__("vector.equals({0}, {1})", this, v2);
+		return untyped __lua__("vector2.equals({0}, {1})", this, v2);
 	}
 
 	public inline function dot(v2: Vec2): Float {
-		return untyped __lua__("vector.dot({0}, {1})", this, v2);
+		return untyped __lua__("vector2.dot({0}, {1})", this, v2);
 	}
 
 	public inline function check(): Bool {
-		return untyped __lua__("vector.check({0})", this);
+		return untyped __lua__("vector2.check({0})", this);
 	}
 
 	public inline function inArea(min: Vec2, max: Vec2): Bool {
-		return untyped __lua__("vector.in_area({0}, {1}, {2})", this, min, max);
+		return untyped __lua__("vector2.in_area({0}, {1}, {2})", this, min, max);
 	}
 
 	// ?
@@ -139,19 +139,19 @@ abstract Vec2({
 	// ?
 	// ?
 	public inline function add(x: EitherType<Vec2, Float>): Vec2 {
-		return untyped __lua__("vector.add({0}, {1})", this, x);
+		return untyped __lua__("vector2.add({0}, {1})", this, x);
 	}
 
 	public inline function subtract(x: EitherType<Vec2, Float>): Vec2 {
-		return untyped __lua__("vector.subtract({0}, {1})", this, x);
+		return untyped __lua__("vector2.subtract({0}, {1})", this, x);
 	}
 
 	public inline function multiply(x: EitherType<Vec2, Float>): Vec2 {
-		return untyped __lua__("vector.multiply({0}, {1})", this, x);
+		return untyped __lua__("vector2.multiply({0}, {1})", this, x);
 	}
 
 	public inline function divide(x: EitherType<Vec2, Float>): Vec2 {
-		return untyped __lua__("vector.divide({0}, {1})", this, x);
+		return untyped __lua__("vector2.divide({0}, {1})", this, x);
 	}
 }
 // class TestIt {
