e1f1882260c59375d439cc34686b52ccd89bb035
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-12 22:06:18 -0400

    Mobs don't collide with other objects it's so annoying

--- src/game/entity/mob/Mob.hx
+++ src/game/entity/mob/Mob.hx
@@ -2,6 +2,7 @@ package src.game.entity.mob;
 
 import src.engine.compilercode.Macros;
 import src.engine.entity.LuaEntity;
+import src.engine.entity.ObjectProperties;
 import src.engine.entity.helpers.EntitySerialization;
 
 private class InternalEntityData {
@@ -22,6 +23,8 @@ abstract class Mob extends LuaEntity {
 	override function onActivate(staticData: String, dtimeS: Float) {
 		super.onActivate(staticData, dtimeS);
 		EntitySerialization.safeDeserialize(staticData, this, Macros.getCompileTimeClass());
+		this.object.setProperties(new ObjectProperties()
+			.setCollideWithObjects(false));
 	}
 
 	override function getStaticData(): String {
