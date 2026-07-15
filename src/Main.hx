cc93dbb76961a6fc9414e74825c2c9226f1534d7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-14 22:16:53 -0400

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -5,5 +5,20 @@ import lua.Lua;
 class Main {
 	public static function main(): Void {
 		Lua.print("Hello Infdev.");
+
+		untyped __lua__('
+core.register_chatcommand("floor", {
+    func = function(name)
+        local player = core.get_player_by_name(name)
+	local pos = player:get_pos()
+	print(pos)
+	for x = -100,100 do
+		for z = -100,100 do
+			core.set_node(vector.new(pos.x + x,pos.y,pos.z + z), {name = "infdev:cobblestone"})
+		end
+	end        
+    end,
+})
+		');
 	};
 }
