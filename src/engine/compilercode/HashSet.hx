4746aa5e8b49d1ffda8971b889861e6001e828f5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-17 19:16:10 -0400

    Create HashSet.hx

---
+++ src/engine/compilercode/HashSet.hx
@@ -0,0 +1,42 @@
+package src.engine.compilercode;
+
+// I tried to AI generate this but the AI exploded trying to figure it out.
+
+abstract HashSet<T:{}>(Map<T, Bool>) {
+	public inline function new() {
+		this = new Map();
+	}
+
+	public inline function contains(thing: T): Bool {
+		return this.exists(thing);
+	}
+
+	public inline function insert(thing: T): Void {
+		this.set(thing, true);
+	}
+
+	public inline function length(): Int {
+		return Lambda.count(this);
+	}
+
+	public inline function remove(thing: T): Bool {
+		return this.remove(thing);
+	}
+}
+
+class Blah {
+	static function __init__() {
+		trace("BEGIN TEST OF HASHSET");
+
+		var x = new HashSet<String>();
+
+		x.insert("test");
+
+		trace(x.contains("test"));
+		trace(x.length());
+
+		x.remove("test");
+
+		trace(x.contains("test"));
+	}
+}
