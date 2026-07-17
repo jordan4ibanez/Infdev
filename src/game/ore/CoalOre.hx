166ca60d9a56b34b748f52cb65fdabdb9610d1ac
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-17 18:08:48 -0400

    Local man is angry about texture

--- src/game/ore/CoalOre.hx
+++ src/game/ore/CoalOre.hx
@@ -1,10 +1,24 @@
 package src.game.ore;
 
 import src.engine.definition.OreDefinition;
+import src.game.groups.NodeGroup;
+import src.game.node.stone.StoneSound;
 
 @:register("infdev:coal_ore")
 final class CoalOre extends OreDefinition {
 	public function new() {
 		super();
+
+		this.nodeGroups = [
+			NodeGroupStone => 1
+		];
+
+		// Why is this called mineral and not ore?????
+		this.tiles = ["default_stone.png^default_mineral_coal.png"];
+
+		this.nodeSounds = StoneSound.get();
+
+		// todo: make this an actual item
+		this.drop = "infdev:coal";
 	}
 }
