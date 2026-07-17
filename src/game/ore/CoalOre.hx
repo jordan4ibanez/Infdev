2a17fcde7e04abcf5695ee0e4f89582a47820716
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-17 17:51:57 -0400

    Create CoalOre.hx

---
+++ src/game/ore/CoalOre.hx
@@ -0,0 +1,10 @@
+package src.game.ore;
+
+import src.engine.definition.OreDefinition;
+
+@:register("infdev:coal_ore")
+final class CoalOre extends OreDefinition {
+	public function new() {
+		super();
+	}
+}
