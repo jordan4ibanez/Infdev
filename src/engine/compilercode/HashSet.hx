e2a948cabcf55d5137819971bf51c3875254cf44
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-17 19:32:52 -0400

    Make this variadiac cause I fuckin can

--- src/engine/compilercode/HashSet.hx
+++ src/engine/compilercode/HashSet.hx
@@ -1,7 +1,8 @@
 package src.engine.compilercode;
 
-// I tried to AI generate this but the AI exploded trying to figure it out.
+import haxe.Rest;
 
+// I tried to AI generate this but the AI exploded trying to figure it out.
 abstract HashSet<T:{}>(Map<T, Bool>) {
 	public inline function new() {
 		this = new Map();
@@ -11,8 +12,11 @@ abstract HashSet<T:{}>(Map<T, Bool>) {
 		return this.exists(thing);
 	}
 
-	public inline function insert(thing: T): Void {
+	public inline function insert(thing: T, otherThings: Rest<T>): Void {
 		this.set(thing, true);
+		for (t in otherThings) {
+			this.set(t, true);
+		}
 	}
 
 	public inline function length(): Int {
@@ -23,20 +27,25 @@ abstract HashSet<T:{}>(Map<T, Bool>) {
 		return count;
 	}
 
-	public inline function remove(thing: T): Bool {
-		return this.remove(thing);
+	public inline function remove(thing: T, otherThings: Rest<T>): Void {
+		this.remove(thing);
+		for (t in otherThings) {
+			this.remove(t);
+		}
 	}
 }
-
 // class Blah {
 // 	static function __init__() {
 // 		trace("BEGIN TEST OF HASHSET");
 // 		var x = new HashSet<String>();
 // 		x.insert("test");
+// 		x.insert("other stuff", "cool", "blah");
 // 		trace(x.contains("test"));
 // 		trace(x.length());
 // 		x.remove("test");
 // 		trace(x.length());
 // 		trace(x.contains("test"));
+// 		x.remove("other stuff", "cool", "doesn't exist");
+// 		trace(x.length());
 // 	}
 // }
