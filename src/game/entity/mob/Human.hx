4fb39684deadeaa6ee36ca31aee1c21c06875dc7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-16 21:54:48 -0400

    And just decorate it

--- src/game/entity/mob/Human.hx
+++ src/game/entity/mob/Human.hx
@@ -7,7 +7,7 @@ import src.engine.vector.Vec2;
 import src.engine.vector.Vec3;
 import src.game.entity.Player.PlayerAnimation;
 
-@:build(src.engine.compilercode.EnumMacro.decorateValidator())
+@:build(src.engine.compilercode.EnumMacro.decorate())
 private enum abstract MobState(String) to String {
 	var MobStateIdle;
 	var MobStateWalk;
