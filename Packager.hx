b056b4148e631d16bb8d57db7a803964978fbe30
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-17 21:50:40 -0400

    Amazing

--- Packager.hx
+++ Packager.hx
@@ -1,7 +1,13 @@
+import sys.FileSystem;
+
 class Packager {
 	public static function main() {
 		// This is not portable but I do not care.
 		// I was originally going to do something fancy but who really cares? It's running in an ubuntu container on microsoft infrastructure.
+
+		if (FileSystem.exists("Infdev.zip")) {
+			FileSystem.deleteFile("Infdev.zip");
+		}
 
 		// I literally did this so I don't have to use quotations.
 		final ____components = '
@@ -16,7 +22,7 @@ class Packager {
 		// Too much engineering went into this.
 		for (component in StringTools.trim(____components).split("\n")) {
 			component = StringTools.trim(component);
-			trace(component);
+			// trace(component);
 			command += ' "$component"';
 		}
 
