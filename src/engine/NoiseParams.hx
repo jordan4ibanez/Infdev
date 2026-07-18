d11fad79199cabd5ca265b6cd1ad99985a788d45
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-18 16:52:26 -0400

    Yeah how about no

--- src/engine/NoiseParams.hx
+++ src/engine/NoiseParams.hx
@@ -3,7 +3,7 @@ package src.engine;
 import src.engine.vector.Vec3;
 
 // This is basically just a read only setter.
-final class NoiseParams {
+typedef NoiseParams = {
 	var offset: Float;
 	var scale: Float;
 	var spread: Vec3;
@@ -12,46 +12,4 @@ final class NoiseParams {
 	var persistence: Float;
 	var lacunarity: Float;
 	var flags: String;
-
-	public function new() {}
-
-	public function setOffset(offset: Float): NoiseParams {
-		this.offset = offset;
-		return this;
-	}
-
-	public function setScale(scale: Float): NoiseParams {
-		this.scale = scale;
-		return this;
-	}
-
-	public function setSpread(spread: Vec3): NoiseParams {
-		this.spread = spread;
-		return this;
-	}
-
-	public function setSeed(seed: Int): NoiseParams {
-		this.seed = seed;
-		return this;
-	}
-
-	public function setOctaves(octaves: Int): NoiseParams {
-		this.octaves = octaves;
-		return this;
-	}
-
-	public function setPersistence(persistence: Float): NoiseParams {
-		this.persistence = persistence;
-		return this;
-	}
-
-	public function setLacunarity(lacunarity: Float): NoiseParams {
-		this.lacunarity = lacunarity;
-		return this;
-	}
-
-	public function setFlags(flags: String): NoiseParams {
-		this.flags = flags;
-		return this;
-	}
 }
