3f7f4235432052dc8b6b1f319e0e7cee9588e273
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-18 14:44:10 -0400

    what a strange delta

--- src/engine/definition/OreDefinition.hx
+++ src/engine/definition/OreDefinition.hx
@@ -1,29 +1,7 @@
 package src.engine.definition;
 
-/**
- * When you extend this class, you get a specialty class which is extremely interesting.
- * 
- * The extended class is wrapped in a static class.
- * 
- * Defined vars are all copied to the static class. (They are virtual final)
- * 
- * Defined methods are copied to the static class and wrapped in static methods.
- * 
- * ! Warning: Do not call an override API method unless you define it. It doesn't exist.
- * 
- * Feel free to edit your custom vars during runtime.
- * 
- * Never call another override function unless 
- * 
- * Also another note: This one is for ores. 
- * Use the ItemDefinition class for items.
- * Use the ToolDefinition class for tools.
- * Use the NodeDefinition class for nodes.
- */
-@:luantiDefinitionRoot
-class OreDefinition extends NodeDefinition {
-
-    /*
+typedef OreSpawn =  {
+        /*
 {
     name = "",
     -- If set, core.registered_ores[that_name] will return this definition.
@@ -131,6 +109,32 @@ class OreDefinition extends NodeDefinition {
     -- integer [u16]
 }
     */
+}
+
+/**
+ * When you extend this class, you get a specialty class which is extremely interesting.
+ * 
+ * The extended class is wrapped in a static class.
+ * 
+ * Defined vars are all copied to the static class. (They are virtual final)
+ * 
+ * Defined methods are copied to the static class and wrapped in static methods.
+ * 
+ * ! Warning: Do not call an override API method unless you define it. It doesn't exist.
+ * 
+ * Feel free to edit your custom vars during runtime.
+ * 
+ * Never call another override function unless 
+ * 
+ * Also another note: This one is for ores. 
+ * Use the ItemDefinition class for items.
+ * Use the ToolDefinition class for tools.
+ * Use the NodeDefinition class for nodes.
+ */
+@:luantiDefinitionRoot
+class OreDefinition extends NodeDefinition {
+
+
 	public function new() {
 		super();
 	}
