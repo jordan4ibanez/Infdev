0010aa1ee9a6b2605e28e2581a637c217539942a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-20 13:33:02 -0400

    Create GoldOre.hx

---
+++ src/game/ore/GoldOre.hx
@@ -0,0 +1,33 @@
+package src.game.ore;
+
+import src.engine.definition.OreDefinition;
+import src.game.groups.NodeGroup;
+import src.game.node.stone.StoneSound;
+
+@:register("infdev:gold_ore")
+final class GoldOre extends OreDefinition {
+	public function new() {
+		super();
+
+		this.nodeGroups = [
+			NodeGroupStone => 2
+		];
+
+		this.tiles = ["default_stone.png^default_mineral_gold.png"];
+
+		this.nodeSounds = StoneSound.get();
+
+		// todo: make this an actual item
+		this.drop = "infdev:iron";
+
+		this.oreSpawns = [{
+			ore_type: OreTypeScatter,
+			wherein: "infdev:stone",
+			clust_scarcity: 14,
+			clust_num_ores: 5,
+			clust_size: 3,
+			y_max: -128,
+			y_min: -1023,
+		}];
+	}
+}
