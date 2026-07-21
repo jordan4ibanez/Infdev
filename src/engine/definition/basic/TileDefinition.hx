48b58779b19c04ec7baeb78f5320d3377db5b11a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-21 18:04:47 -0400

    bad
    Let's more native this

--- src/engine/definition/basic/TileDefinition.hx
+++ src/engine/definition/basic/TileDefinition.hx
@@ -10,65 +10,60 @@ enum abstract TileDefinitionAlignStyle(String) to String {
 	var TileDefinitionAlignStyleUser = "user";
 }
 
-class TileDefinition {
+typedef TileDefinition = {
 	var name: String;
 
-	@:native("tileable_vertical")
-	var tileableVertical: Bool;
+	@:optional
+	var tileable_vertical: Bool;
 
-	@:native("tileable_horizontal")
-	var tileableHorizontal: Bool;
+	@:optional
+	var tileable_horizontal: Bool;
 
-	@:native("backface_culling")
-	var backfaceCulling: Bool;
+	@:optional
+	var backface_culling: Bool;
 
+	@:optional
 	var animation: TileAnimationDefinition;
 
-	@:native("align_style")
-	var alignStyle: TileDefinitionAlignStyle;
+	@:optional
+	var align_style: TileDefinitionAlignStyle;
 
+	@:optional
 	var scale: Int;
 
+	@:optional
 	var color: ColorSpec;
-
-	public function new(name: String) {
-		this.name = name;
-	}
-
-	public function setTileableVertical(setting: Bool): TileDefinition {
-		this.tileableVertical = setting;
-		return this;
-	}
-
-	public function setTileableHorizontal(setting: Bool): TileDefinition {
-		this.tileableHorizontal = setting;
-		return this;
-	}
-
-	public function setBackfaceCulling(backfaceCulling: Bool): TileDefinition {
-		this.backfaceCulling = backfaceCulling;
-		return this;
-	}
-
-	public function setAnimation(animation: TileAnimationDefinition): TileDefinition {
-		this.animation = animation;
-		return this;
-	}
-
-	public function setAlignStyle(alignStyle: TileDefinitionAlignStyle): TileDefinition {
-		this.alignStyle = alignStyle;
-		return this;
-	}
-
-	public function setScale(scale: Int): TileDefinition {
-		this.scale = scale;
-		return this;
-	}
-
-	public function setColor(color: ColorSpec): TileDefinition {
-		this.color = color;
-		return this;
-	}
+	// public function new(name: String) {
+	// 	this.name = name;
+	// }
+	// public function setTileableVertical(setting: Bool): TileDefinition {
+	// 	this.tileable_vertical = setting;
+	// 	return this;
+	// }
+	// public function setTileableHorizontal(setting: Bool): TileDefinition {
+	// 	this.tileable_horizontal = setting;
+	// 	return this;
+	// }
+	// public function setBackfaceCulling(backfaceCulling: Bool): TileDefinition {
+	// 	this.backface_culling = backfaceCulling;
+	// 	return this;
+	// }
+	// public function setAnimation(animation: TileAnimationDefinition): TileDefinition {
+	// 	this.animation = animation;
+	// 	return this;
+	// }
+	// public function setAlignStyle(alignStyle: TileDefinitionAlignStyle): TileDefinition {
+	// 	this.align_style = alignStyle;
+	// 	return this;
+	// }
+	// public function setScale(scale: Int): TileDefinition {
+	// 	this.scale = scale;
+	// 	return this;
+	// }
+	// public function setColor(color: ColorSpec): TileDefinition {
+	// 	this.color = color;
+	// 	return this;
+	// }
 }
 
 typedef TileDefinitionOrString = EitherType<TileDefinition, String>;
