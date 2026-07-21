e076ef662bd0a2df285019330c431f57a4161088
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-21 18:50:51 -0400

    again

--- src/game/tool/OracleShovel.hx
+++ src/game/tool/OracleShovel.hx
@@ -1,8 +1,8 @@
 package src.game.tool;
 
 import src.engine.definition.NodeDefinition.MAX_NODE_LEVEL;
-import src.engine.definition.basic.ToolCapabilities;
 import src.engine.definition.ToolDefinition;
+import src.engine.definition.basic.ToolCapabilities;
 
 @:register("infdev:oracle_shovel")
 final class OracleShovel extends ToolDefinition {
@@ -13,7 +13,6 @@ final class OracleShovel extends ToolDefinition {
 		this.wieldImage = "oracle_shovel.png";
 
 		this.toolCapabilities = new ToolCapabilities()
-			.setFullPunchInterval(0.0)
 			.setMaxDropLevel(0)
 			.addGroupCap(NodeGroupDirt, new GroupCapabilities(0.1, 0.005, MAX_NODE_LEVEL, 1_000_000))
 			.addGroupCap(NodeGroupSoil, new GroupCapabilities(0.1, 0.005, MAX_NODE_LEVEL, 1_000_000))
