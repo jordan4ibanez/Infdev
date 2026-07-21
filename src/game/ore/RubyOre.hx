600fbc1e9a33746cb2200ba1bc325876af81b968
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-21 00:13:04 -0400

    Update RubyOre.hx

--- src/game/ore/RubyOre.hx
+++ src/game/ore/RubyOre.hx
@@ -4,29 +4,29 @@ import src.engine.definition.OreDefinition;
 import src.game.groups.NodeGroup;
 import src.game.node.stone.StoneSound;
 
-@:register("infdev:diamond_ore")
-final class DiamondOre extends OreDefinition {
+@:register("infdev:ruby_ore")
+final class RubyOre extends OreDefinition {
 	public function new() {
 		super();
 
 		this.nodeGroups = [
-			NodeGroupStone => 3
+			NodeGroupStone => 4
 		];
 
-		this.tiles = ["default_stone.png^default_mineral_diamond.png"];
+		this.tiles = ["default_stone.png^(default_mineral_diamond.png^[colorize:red:190)"];
 
 		this.nodeSounds = StoneSound.get();
 
 		// todo: make this an actual item
-		this.drop = "infdev:diamond";
+		this.drop = "infdev:ruby";
 
 		this.oreSpawns = [{
 			ore_type: OreTypeScatter,
 			wherein: "infdev:stone",
-			clust_scarcity: 15,
+			clust_scarcity: 18,
 			clust_num_ores: 7,
 			clust_size: 3,
-			y_max: -256,
+			y_max: -384,
 			y_min: -1023,
 		}];
 	}
