c9b2f722779eafbd40128035c5c97d2589c364b0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-21 00:44:56 -0400

    Create MoonstoneOre.hx

---
+++ src/game/ore/MoonstoneOre.hx
@@ -0,0 +1,33 @@
+package src.game.ore;
+
+import src.engine.definition.OreDefinition;
+import src.game.groups.NodeGroup;
+import src.game.node.stone.StoneSound;
+
+@:register("infdev:moonstone_ore")
+final class MoonstoneOre extends OreDefinition {
+	public function new() {
+		super();
+
+		this.nodeGroups = [
+			NodeGroupStone => 9
+		];
+
+		this.tiles = ["default_stone.png^(default_mineral_iron.png^[colorize:lightblue:200)"];
+
+		this.nodeSounds = StoneSound.get();
+
+		// todo: make this an actual item
+		this.drop = "infdev:moonstone";
+
+		this.oreSpawns = [{
+			ore_type: OreTypeScatter,
+			wherein: "infdev:stone",
+			clust_scarcity: 26,
+			clust_num_ores: 7,
+			clust_size: 3,
+			y_max: -768,
+			y_min: -1023,
+		}];
+	}
+}
