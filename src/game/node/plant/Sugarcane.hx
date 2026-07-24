8a5c4e878486e602663a204f4b7bc22ff2dc6c55
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-23 22:38:05 -0400

    Create Sugarcane.hx

---
+++ src/game/node/plant/Sugarcane.hx
@@ -0,0 +1,35 @@
+package src.game.node.plant;
+
+import src.engine.definition.NodeDefinition;
+import src.engine.definition.basic.NodeBox;
+import src.game.groups.NodeGroup;
+
+@:register("infdev:sugarcane")
+final class Sugarcane extends NodeDefinition {
+	public function new() {
+		super();
+
+		this.description = "Sugarcane";
+
+		this.tiles = [
+			"default_papyrus.png"
+		];
+
+		this.drawType = DrawTypePlantLike;
+
+		this.paramtype1 = ParamType1Light;
+
+		this.sunlightPropagates = true;
+
+		this.walkable = false;
+
+		this.selectionBox = new NodeBoxFixed()
+			.addBox(-6 / 16, -0.5, -6 / 16, 6 / 16, 0.5, 6 / 16);
+
+		this.nodeGroups = [
+			NodeGroupPlant => 1
+		];
+
+		this.nodeSounds = PlantSound.get();
+	}
+}
