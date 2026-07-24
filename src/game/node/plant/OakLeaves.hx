7522e6586590d3171d75cfc53cf61020c190cb12
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-23 23:38:52 -0400

    Create OakLeaves.hx

---
+++ src/game/node/plant/OakLeaves.hx
@@ -0,0 +1,29 @@
+package src.game.node.plant;
+
+import src.engine.definition.NodeDefinition;
+import src.game.groups.NodeGroup;
+
+@:register("infdev:oak_leaves")
+final class OakLeaves extends NodeDefinition {
+	public function new() {
+		super();
+
+		this.description = "Oak Leaves";
+
+		this.tiles = ["default_leaves.png"];
+
+		this.specialTiles = ["default_leaves_simple.png"];
+
+		this.waving = WavingTypeLeaves;
+
+		this.paramtype1 = ParamType1Light;
+
+		this.isGroundContent = false;
+
+		this.nodeGroups = [
+			NodeGroupPlant => 1
+		];
+
+		this.nodeSounds = PlantSound.get();
+	}
+}
