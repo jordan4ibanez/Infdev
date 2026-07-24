282dab8cda6c556a56a8142fe71c161f389bbc89
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-23 23:50:33 -0400

    Create OakPlanks.hx

---
+++ src/game/node/wood/OakPlanks.hx
@@ -0,0 +1,26 @@
+package src.game.node.wood;
+
+import src.engine.definition.NodeDefinition;
+import src.game.groups.NodeGroup;
+
+@:register("infdev:oak_planks")
+final class OakPlanks extends NodeDefinition {
+	public function new() {
+		super();
+
+		this.description = "Oak Planks";
+
+		this.tiles = [
+			"default_wood.png"
+		];
+
+		this.isGroundContent = false;
+
+		this.nodeGroups = [
+			NodeGroupWood => 1,
+			NodeGroupPlanks => 1
+		];
+
+		this.nodeSounds = WoodSound.get();
+	}
+}
