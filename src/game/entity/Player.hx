92241e9c62e9ff1c532c4fbf1d6276966a83b023
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-23 20:51:29 -0400

    ???casually solve a physics issue???

--- src/game/entity/Player.hx
+++ src/game/entity/Player.hx
@@ -200,6 +200,9 @@ final class Player {
 		this.object.setPhysicsOverride({
 			gravity: 1.25,
 			jump: 1.25,
+			acceleration_default: 0.5,
+			acceleration_fast: 0.5,
+			acceleration_air: 0.5,
 		});
 
 		this.object.setProperties({
