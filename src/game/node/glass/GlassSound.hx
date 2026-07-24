33ade170dc429fa6a5970b4c261524b3259df7ae
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-23 22:09:19 -0400

    Create GlassSound.hx

---
+++ src/game/node/glass/GlassSound.hx
@@ -0,0 +1,21 @@
+package src.game.node.glass;
+
+import src.engine.definition.sound.NodeSoundTable;
+import src.engine.definition.sound.SimpleSoundSpecTable;
+
+@:final
+abstract class GlassSound {
+	public static function get(): NodeSoundTable {
+		return new NodeSoundTable()
+			.setDig(new SimpleSoundSpecTable("glass_mine")
+				.setPitch(0.9))
+			.setDug(new SimpleSoundSpecTable("glass_mine")
+				.setPitch(0.75))
+			.setFootstep(new SimpleSoundSpecTable("glass_step")
+				.setPitch(0.8))
+			.setPlace(new SimpleSoundSpecTable("glass_mine")
+				.setPitch(0.65))
+			.setPlaceFailed(new SimpleSoundSpecTable("glass_mine")
+				.setPitch(0.4));
+	}
+}
