879c2d517a7cba097ef70ba6cebca1dc10ebfbe6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-23 22:07:51 -0400

    Update FireclayBrickBlock.hx

--- src/game/node/stone/FireclayBrickBlock.hx
+++ src/game/node/stone/FireclayBrickBlock.hx
@@ -1,2 +1,26 @@
 package src.game.node.stone;
 
+import src.engine.definition.NodeDefinition;
+import src.game.groups.NodeGroup;
+
+@:register("infdev:fireclay_brick_block")
+final class FireclayBrickBlock extends NodeDefinition {
+	public function new() {
+		super();
+
+		this.nodeGroups = [
+			NodeGroupStone => 1
+		];
+
+		this.description = "Fireclay Brick Block";
+
+		this.nodeSounds = StoneSound.get();
+
+		this.tiles = [
+			"default_brick.png^[transformFX^[colorize:#988558:200",
+			"default_brick.png^[colorize:#988558:200",
+		];
+
+		this.isGroundContent = false;
+	}
+}
