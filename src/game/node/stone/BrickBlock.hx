0ab22bfd19652466c12b10e9d7bb60b5eb1ab724
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-23 21:12:11 -0400

    Update BrickBlock.hx

--- src/game/node/stone/BrickBlock.hx
+++ src/game/node/stone/BrickBlock.hx
@@ -3,17 +3,22 @@ package src.game.node.stone;
 import src.engine.definition.NodeDefinition;
 import src.game.groups.NodeGroup;
 
-@:register("infdev:brick")
+@:register("infdev:brick_block")
 final class BrickBlock extends NodeDefinition {
 	public function new() {
 		super();
 
 		this.nodeGroups = [
-			NodeGroupStone => 2
+			NodeGroupStone => 1
 		];
 
 		this.nodeSounds = StoneSound.get();
 
-		this.tiles = ["default_brick.png"];
+		this.tiles = [
+			"default_brick.png^[transformFX",
+			"default_brick.png",
+		];
+
+		this.isGroundContent = false;
 	}
 }
