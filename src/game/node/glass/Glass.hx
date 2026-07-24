f8d0b5709364adbb82b90569b706c6493424f1bd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-23 22:10:27 -0400

    Update Glass.hx

--- src/game/node/glass/Glass.hx
+++ src/game/node/glass/Glass.hx
@@ -1,2 +1,28 @@
 package src.game.node.glass;
 
+import src.engine.definition.NodeDefinition;
+import src.game.groups.NodeGroup;
+
+@:register("infdev:glass")
+final class Glass extends NodeDefinition {
+	public function new() {
+		super();
+
+		this.nodeGroups = [
+			NodeGroupGlass => 1
+		];
+
+		this.description = "Glass";
+
+		this.nodeSounds = GlassSound.get();
+
+		this.drawType = DrawTypeGlassLikeFramedOptional;
+
+		this.tiles = [
+			"default_glass.png",
+			"default_glass_detail.png"
+		];
+
+		this.isGroundContent = false;
+	}
+}
