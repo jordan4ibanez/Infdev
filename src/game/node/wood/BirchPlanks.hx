2965a012fb4baa1601ff25e12f5da6d1cf88e9c0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-23 23:58:46 -0400

    Create BirchPlanks.hx

---
+++ src/game/node/wood/BirchPlanks.hx
@@ -0,0 +1,26 @@
+package src.game.node.wood;
+
+import src.engine.definition.NodeDefinition;
+import src.game.groups.NodeGroup;
+
+@:register("infdev:birch_planks")
+final class BirchPlanks extends NodeDefinition {
+	public function new() {
+		super();
+
+		this.description = "Birch Planks";
+
+		this.tiles = [
+			"default_aspen_wood.png"
+		];
+
+		this.isGroundContent = false;
+
+		this.nodeGroups = [
+			NodeGroupWood => 1,
+			NodeGroupPlanks => 1
+		];
+
+		this.nodeSounds = WoodSound.get();
+	}
+}
