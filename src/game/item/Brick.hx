e73dcc80a30c147a473d4c9ccd4fadfd4c6c956b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-27 01:47:01 -0400

    Create Brick.hx

---
+++ src/game/item/Brick.hx
@@ -0,0 +1,14 @@
+package src.game.item;
+
+import src.engine.definition.ItemDefinition;
+
+@:register("infdev:brick")
+final class Brick extends ItemDefinition {
+	public function new() {
+		super();
+
+        this.description = "Brick";
+
+		this.inventoryImage = "default_clay_brick.png^[colorize:#842020:100";
+	}
+}
