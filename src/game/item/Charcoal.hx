1ce8f76f89e4c354e79e7443f58f751145564068
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-27 02:20:26 -0400

    Create Charcoal.hx

---
+++ src/game/item/Charcoal.hx
@@ -0,0 +1,19 @@
+package src.game.item;
+
+import src.engine.definition.ItemDefinition;
+import src.game.groups.NodeGroup;
+
+@:register("infdev:charcoal")
+final class Charcoal extends ItemDefinition {
+	public function new() {
+		super();
+
+		this.inventoryImage = "default_coal_lump.png";
+
+		this.recipesCooking = [
+			{
+				recipe: NodeGroupTree.groupify()
+			}
+		];
+	}
+}
