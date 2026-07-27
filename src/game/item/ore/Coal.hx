007ba4314d91c63f7613efda40c4cb2a9e9fc03d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-27 18:41:49 -0400

    Update Coal.hx

--- src/game/item/ore/Coal.hx
+++ src/game/item/ore/Coal.hx
@@ -7,6 +7,8 @@ final class Coal extends ItemDefinition {
 	public function new() {
 		super();
 
+		this.description = "Coal";
+
 		this.inventoryImage = "default_coal_lump.png^[colorize:black:170";
 
 		this.recipeFuel = {
