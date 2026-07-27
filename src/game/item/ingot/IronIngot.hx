83286038fa1198555a120b523863c66f006049d9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-27 18:18:37 -0400

    Create IronIngot.hx

---
+++ src/game/item/ingot/IronIngot.hx
@@ -0,0 +1,20 @@
+package src.game.item.ingot;
+
+import src.engine.definition.ItemDefinition;
+
+@:register("infdev:iron_ingot")
+final class Iron extends ItemDefinition {
+	public function new() {
+		super();
+
+		this.description = "Iron Ingot";
+
+		this.inventoryImage = "default_steel_ingot.png";
+
+		this.recipesCooking = [
+			{
+				recipe: "infdev:iron"
+			}
+		];
+	}
+}
