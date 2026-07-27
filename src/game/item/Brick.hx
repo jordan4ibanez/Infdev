be3794ea323377dbae9ce5fffe3f409aa9480c83
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-27 01:47:32 -0400

    Update Brick.hx

--- src/game/item/Brick.hx
+++ src/game/item/Brick.hx
@@ -7,8 +7,15 @@ final class Brick extends ItemDefinition {
 	public function new() {
 		super();
 
-        this.description = "Brick";
+		this.description = "Brick";
 
 		this.inventoryImage = "default_clay_brick.png^[colorize:#842020:100";
+
+		this.recipesShapeless = [
+			{
+				recipe: ["infdev:brick_block"],
+				amount: 4
+			}
+		];
 	}
 }
