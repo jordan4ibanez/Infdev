4e6339a3f4230ac952c9fa3e20eba41f04ffb7b0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-28 17:12:41 -0400

    Now make the cactus nice too

--- src/game/node/plant/Cactus.hx
+++ src/game/node/plant/Cactus.hx
@@ -1,6 +1,10 @@
 package src.game.node.plant;
 
+import src.engine.Core;
+import src.engine.ItemStack;
 import src.engine.definition.NodeDefinition;
+import src.engine.definition.basic.PointedThing;
+import src.engine.entity.objectref.ObjectRefBase;
 import src.game.groups.NodeGroup;
 
 @:register("infdev:cactus")
@@ -16,10 +20,26 @@ final class Cactus extends NodeDefinition {
 			"default_cactus_side.png",
 		];
 
+		this.nodePlacementPrediction = "";
+
 		this.nodeGroups = [
-			NodeGroupPlant => 1
+			NodeGroupPlant => 1,
+			NodeGroupAttachedNode => AttachedNodeSettingAlwaysToBelow
 		];
 
 		this.nodeSounds = PlantSound.get();
+	}
+
+	override function onPlace(itemstack: ItemStack, placer: Null<ObjectRefBase>, pointedThing: PointedThing): Null<ItemStack> {
+		// Only allow placing on sand and not itself because farms get very annoying with that.
+		if (pointedThing.type == PointedThingTypeNode) {
+			var pos = pointedThing.above.copy();
+			pos.y -= 1;
+			var nodeUnder = Core.getNode(pos);
+			if (Core.getItemGroup(nodeUnder.name, NodeGroupSand) > 0) {
+				return Core.itemPlace(itemstack, placer, pointedThing).itemstack;
+			}
+		}
+		return null;
 	}
 }
