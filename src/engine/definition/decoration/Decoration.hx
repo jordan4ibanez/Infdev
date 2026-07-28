f06671897f14627a4ef6023fef65ab902605c9ba
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-27 20:00:25 -0400

    Create Decoration.hx

---
+++ src/engine/definition/decoration/Decoration.hx
@@ -0,0 +1,7 @@
+package src.engine.definition.decoration;
+
+enum abstract DecorationType(String) to String {
+	var DecorationTypeSimple = "simple";
+	var DecorationTypeSchematic = "schematic";
+	var DecorationTypeLSystem = "lsystem";
+}
