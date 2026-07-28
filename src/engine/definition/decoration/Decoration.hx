58576441273d4b5aa2becfa865d59a4b381bfffc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-27 20:02:59 -0400

    Update Decoration.hx

--- src/engine/definition/decoration/Decoration.hx
+++ src/engine/definition/decoration/Decoration.hx
@@ -7,26 +7,26 @@ enum abstract DecorationType(String) to String {
 }
 
 typedef DecorationDefinition = {
-    deco_type = "simple",
+    var deco_type = "simple",
     // Type. "simple", "schematic" or "lsystem" supported
 
-    place_on = "default:dirt_with_grass",
+    var place_on = "default:dirt_with_grass",
     // Node (or list of nodes) that the decoration can be placed on
 
-    sidelen = 8,
+    var sidelen = 8,
     // Size of the square (X / Z) divisions of the mapchunk being generated.
     // Determines the resolution of noise variation if used.
     // If the chunk size is not evenly divisible by sidelen, sidelen is made
     // equal to the chunk size.
     // Integer in range: [1, 32767]
 
-    fill_ratio = 0.02,
+    var fill_ratio = 0.02,
     // The value determines 'decorations per surface node'.
     // Used only if noise_params is not specified.
     // If >= 10.0 complete coverage is enabled and decoration placement uses
     // a different and much faster method.
 
-    noise_params = {
+    var noise_params = {
         offset = 0,
         scale = 0.45,
         spread = {x = 100, y = 100, z = 100},
@@ -43,34 +43,34 @@ typedef DecorationDefinition = {
     // If the noise value >= 10.0 complete coverage is enabled and
     // decoration placement uses a different and much faster method.
 
-    biomes = {"Oceanside", "Hills", "Plains"},
+    var biomes = {"Oceanside", "Hills", "Plains"},
     // List of biomes in which this decoration occurs. Occurs in all biomes
     // if this is omitted, and ignored if the Mapgen being used does not
     // support biomes.
     // Can be a list of (or a single) biome names, IDs, or definitions.
 
-    y_min = -31000,
-    y_max = 31000,
+    var y_min = -31000,
+    var y_max = 31000,
     // Lower and upper limits for decoration (inclusive).
     // These parameters refer to the Y coordinate of the 'place_on' node.
     // Integer [s16]
 
-    spawn_by = "default:water",
+    var spawn_by = "default:water",
     // Node (or list of nodes) that the decoration only spawns next to.
     // Checks the 8 neighboring nodes on the same height,
     // and also the ones at the height plus the check_offset, excluding both center nodes.
 
-    check_offset = -1,
+    var check_offset = -1,
     // Specifies the offset that spawn_by should also check
     // The default value of -1 is useful to e.g check for water next to the base node.
     // 0 disables additional checks, valid values: {-1, 0, 1}
 
-    num_spawn_by = 1,
+    var num_spawn_by = 1,
     // Amount of spawn_by nodes that must be surrounding the decoration
     // position to occur.
     // If absent or -1, decorations occur next to any nodes.
 
-    flags = "liquid_surface, force_placement, all_floors, all_ceilings",
+    var flags = "liquid_surface, force_placement, all_floors, all_ceilings",
     // Flags for all decoration types.
     // "liquid_surface": Find the highest liquid (not solid) surface under
     //   open air. Search stops and fails on the first solid node.
@@ -89,32 +89,32 @@ typedef DecorationDefinition = {
 
     ////- Simple-type parameters
 
-    decoration = "default:grass",
+    var decoration = "default:grass",
     // The node name used as the decoration.
     // If instead a list of strings, a randomly selected node from the list
     // is placed as the decoration.
 
-    height = 1,
+    var height = 1,
     // Decoration height in nodes.
     // If height_max is not 0, this is the lower limit of a randomly
     // selected height.
     // Integer in range: [1, 32767]
 
-    height_max = 0,
+    var height_max = 0,
     // Upper limit of the randomly selected height.
     // If absent, the parameter 'height' is used as a constant.
     // Integer in range: [1, 32767]
 
-    param2 = 0,
+    var param2 = 0,
     // Param2 value of decoration nodes.
     // If param2_max is not 0, this is the lower limit of a randomly
     // selected param2.
 
-    param2_max = 0,
+    var param2_max = 0,
     // Upper limit of the randomly selected param2.
     // If absent, the parameter 'param2' is used as a constant.
 
-    place_offset_y = 0,
+    var place_offset_y = 0,
     // Y offset of the decoration base node relative to the standard base
     // node position.
     // Can be positive or negative. Default is 0.
@@ -125,12 +125,12 @@ typedef DecorationDefinition = {
 
     ////- Schematic-type parameters
 
-    schematic = "foobar.mts",
+    var schematic = "foobar.mts",
     // If schematic is a string, it is the filepath relative to the current
     // working directory of the specified Luanti schematic file.
     // Could also be the ID of a previously registered schematic.
 
-    schematic = {
+    var schematic = {
         size = {x = 4, y = 6, z = 4},
         data = {
             {name = "default:cobble", param1 = 255, param2 = 0},
@@ -148,16 +148,16 @@ typedef DecorationDefinition = {
     // size and data are mandatory whereas yslice_prob is optional.
     // See 'Schematic specifier' for details.
 
-    replacements = {["oldname"] = "convert_to", ...},
+    var replacements = {["oldname"] = "convert_to", ...},
     // Map of node names to replace in the schematic after reading it.
 
-    flags = "place_center_x, place_center_y, place_center_z",
+    var flags = "place_center_x, place_center_y, place_center_z",
     // Flags for schematic decorations. See 'Schematic attributes'.
 
-    rotation = "90",
+    var rotation = "90",
     // Rotation can be "0", "90", "180", "270", or "random"
 
-    place_offset_y = 0,
+    var place_offset_y = 0,
     // If the flag 'place_center_y' is set this parameter is ignored.
     // Y offset of the schematic base node layer relative to the 'place_on'
     // node.
@@ -168,7 +168,7 @@ typedef DecorationDefinition = {
 
     ////- L-system-type parameters
 
-    treedef = {},
+    var treedef = {},
     // Same as for `core.spawn_tree`.
     // See section [L-system trees] for more details.
 }
