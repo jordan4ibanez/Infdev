ba18300d7883636fa9c5801898c0edcfe5370ce4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-28 13:32:31 -0400

    Great success!

--- src/engine/compilercode/DecorationDuctTape.hx
+++ src/engine/compilercode/DecorationDuctTape.hx
@@ -58,6 +58,16 @@ class DecorationDuctTape {
 
 		trace(className);
 
+		// ? Finally inject the class directly into the compiler compilation pool.
+
+		try {
+			Context.defineType(companionClassDefinition);
+			// ? This is important for debugging.
+			// trace('DuctTape: Successfully generated companion class: ' + localClass.pack.join(".") + "." + wrapperClassName);
+		} catch (e:Dynamic) {
+			// Prevent duplicate definition errors if the macro triggers multiple times.
+		}
+
 		return fields;
 	}
 }
