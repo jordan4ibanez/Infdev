7a60ada86f5057df5adc587ac5e3ef6826d37b3c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-28 14:44:58 -0400

    Update NoiseParams.hx

--- src/engine/NoiseParams.hx
+++ src/engine/NoiseParams.hx
@@ -6,10 +6,11 @@ typedef NoiseParams = {
 	var offset: Float;
 	var scale: Float;
 	var spread: Vec3;
-	var seed: Int;
 	var octaves: Int;
 	var persistence: Float;
 	var lacunarity: Float;
+	@:optional
+	var seed: Int;
 	@:optional
 	var flags: String;
 }
