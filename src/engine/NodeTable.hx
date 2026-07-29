a4489187c4128e8d17ae54c83c94fc8f8519aff6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-28 22:38:16 -0400

    Update NodeTable.hx

--- src/engine/NodeTable.hx
+++ src/engine/NodeTable.hx
@@ -1,38 +1,12 @@
 package src.engine;
 
 // {name="ignore", param1=0, param2=0}
-class NodeTable {
+typedef NodeTable = {
 	public var name: String;
-	public var param1: Int = 0;
-	public var param2: Int = 0;
-
-	public function new(name: String) {
-		this.name = name;
-	}
-
-	public inline function getName(): String {
-		return this.name;
-	}
-
-	public inline function setName(newName: String) {
-		this.name = newName;
-	}
-
-	public inline function getParam1(): Int {
-		return this.param1;
-	}
-
-	public inline function setParam1(param1: Int): Void {
-		this.param1 = param1;
-	}
-
-	public inline function getParam2(): Int {
-		return this.param2;
-	}
-
-	public inline function setParam2(param2: Int): Void {
-		this.param2 = param2;
-	}
+	@:optional
+	public var param1: Int;
+	@:optional
+	public var param2: Int;
 }
 // class Blah {
 // 	static function __init__() {
