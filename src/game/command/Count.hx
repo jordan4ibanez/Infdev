8cff997355f087a2325c25b7ca4dbd3a6629f0c5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-28 22:22:09 -0400

    Update Count.hx

--- src/game/command/Count.hx
+++ src/game/command/Count.hx
@@ -5,7 +5,7 @@ import src.engine.compilercode.LuaMap;
 
 @:register("count")
 final class Count implements ChatCommand {
-	public var params: String = "";
+	public var params: String;
 	public var description: String = "Count for fun!";
 	public var privs: LuaMap<String, Bool> = [
 		"server" => true
@@ -14,7 +14,18 @@ final class Count implements ChatCommand {
 	var counter = new Map<String, Int>();
 
 	public function func(name: String, param: String): CommandStatus {
-		trace(this.counter);
+		if (!counter.exists(name)) {
+			counter.set(name, 1);
+		} else {
+			var count = counter.get(name);
+			count++;
+			counter.set(name, count);
+		}
+
+		var count = counter.get(name);
+
+		trace('$name is at $count');
+
 		return new CommandStatus(true);
 	}
 }
