94b7b4e4b563e5dbd2f8d539e0f68382d7dc4f80
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-30 19:38:41 -0400

    to hex for rgba

--- src/engine/definition/graphics/RGBA.hx
+++ src/engine/definition/graphics/RGBA.hx
@@ -12,4 +12,13 @@ final class RGBA {
 		this.b = b;
 		this.a = a;
 	}
+
+	public function toHex(): String {
+		var rHex = StringTools.hex(this.r & 0xFF, 2);
+		var gHex = StringTools.hex(this.g & 0xFF, 2);
+		var bHex = StringTools.hex(this.b & 0xFF, 2);
+		var aHex = StringTools.hex(this.a & 0xFF, 2);
+
+		return '#$rHex$gHex$bHex$aHex';
+	}
 }
