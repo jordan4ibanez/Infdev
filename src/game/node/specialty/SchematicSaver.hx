488df89ca11ec283fcf7e61704323395a2064226
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-30 02:26:02 -0400

    I accidentally made endstone

--- src/game/node/specialty/SchematicSaver.hx
+++ src/game/node/specialty/SchematicSaver.hx
@@ -1,2 +1,22 @@
 package src.game.node.specialty;
 
+import src.engine.definition.NodeDefinition;
+import src.game.groups.NodeGroup;
+import src.game.node.stone.StoneSound;
+
+@:register("infdev:schematic_saver")
+final class SchematicSaver extends NodeDefinition {
+	public function new() {
+		super();
+
+		this.description = "Schematic Saver";
+
+		this.nodeGroups = [
+			NodeGroupHandDiggable => BEDROCK
+		];
+
+		this.nodeSounds = StoneSound.get();
+
+		this.tiles = ["default_cobble.png^[invert:rgb"];
+	}
+}
