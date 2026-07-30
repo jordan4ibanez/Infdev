820b835727d856aab776fe42b242ecb472b91216
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-30 02:48:18 -0400

    A basic thing

--- src/engine/gui/Formspec.hx
+++ src/engine/gui/Formspec.hx
@@ -1,11 +1,25 @@
 package src.engine.gui;
 
+import src.engine.vector.Vec2;
+
 class Formspec {
 	var data = "";
 
+	final version = 10;
+	var size: Vec2 = new Vec2(10, 10);
+	var fixedSize: Bool = false;
+
 	public function new() {}
 
+	function append(newData: String): Void {
+		this.data += newData;
+	}
+
 	public function serialize(): String {
+		append('formspec_version[${this.version}]');
+		append('size[${this.size.x},${this.size.y},${this.fixedSize}]');
+
+		untyped print(this.data);
 		return this.data;
 	}
 }
