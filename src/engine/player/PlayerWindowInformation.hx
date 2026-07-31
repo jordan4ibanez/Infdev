ff80d34851923c32ad3b6ea9ba2019fbfc6cabed
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-31 01:17:50 -0400

    Update PlayerWindowInformation.hx

--- src/engine/player/PlayerWindowInformation.hx
+++ src/engine/player/PlayerWindowInformation.hx
@@ -1,5 +1,7 @@
 package src.engine.player;
 
+import src.engine.vector.Vec2;
+
 // Will only be present if the client sent this information (requires v5.7+)
 //
 // Note that none of these things are constant, they are likely to change during a client
@@ -8,35 +10,29 @@ package src.engine.player;
 // real_gui_scaling and real_hud_scaling can be used instead of DPI.
 // OSes don't necessarily give the physical DPI, as they may allow user configuration.
 // real_*_scaling is just OS DPI / 96 but with another level of user configuration.
-{
-    // Current size of the in-game render target (pixels).
-    //
-    // This is usually the window size, but may be smaller in certain situations,
-    // such as side-by-side mode.
-    size = {
-        x = 1308, // integer
-        y = 577, // integer
-    },
+typedef PlayerWindowInformation = {
+	// Current size of the in-game render target (pixels).
+	//
+	// This is usually the window size, but may be smaller in certain situations,
+	// such as side-by-side mode.
+	var size: Vec2;
 
-    // Estimated maximum formspec size before Luanti will start shrinking the
-    // formspec to fit. For a fullscreen formspec, use the size returned by
-    // this table  and `padding[0,0]`. `bgcolor[;true]` is also recommended.
-    max_formspec_size = {
-        x = 20,
-        y = 11.25
-    },
+	// Estimated maximum formspec size before Luanti will start shrinking the
+	// formspec to fit. For a fullscreen formspec, use the size returned by
+	// this table  and `padding[0,0]`. `bgcolor[;true]` is also recommended.
+	var max_formspec_size: Vec2;
 
-    // GUI Scaling multiplier
-    // Equal to the setting `gui_scaling` multiplied by `dpi / 96`
-    real_gui_scaling = 1,
+	// GUI Scaling multiplier
+	// Equal to the setting `gui_scaling` multiplied by `dpi / 96`
+	var real_gui_scaling: Float;
 
-    // HUD Scaling multiplier
-    // Equal to the setting `hud_scaling` multiplied by `dpi / 96`
-    real_hud_scaling = 1,
+	// HUD Scaling multiplier
+	// Equal to the setting `hud_scaling` multiplied by `dpi / 96`
+	var real_hud_scaling: Float;
 
-    // Whether the touchscreen controls are enabled.
-    // Usually (but not always) `true` on Android.
-    // Requires at least version 5.9.0 on the client. For older clients, it
-    // is always set to `false`.
-    touch_controls = false,
+	// Whether the touchscreen controls are enabled.
+	// Usually (but not always) `true` on Android.
+	// Requires at least version 5.9.0 on the client. For older clients, it
+	// is always set to `false`.
+	var touch_controls: Bool;
 }
