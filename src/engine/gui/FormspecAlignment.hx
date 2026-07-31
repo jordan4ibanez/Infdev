665a4c4bc2d7d791a08d4fe93e59fbe82c987e67
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-31 00:47:01 -0400

    Create FormspecAlignment.hx

---
+++ src/engine/gui/FormspecAlignment.hx
@@ -0,0 +1,7 @@
+package src.engine.gui;
+
+enum abstract FormspecHorizontalAlignment(String) to String {
+	var FormspecHorizontalAlignmentLeft = "left";
+    var FormspecHorizontalAlignmentCenter = "center";
+    var FormspecHorizontalAlignmentRight = "right";
+}
