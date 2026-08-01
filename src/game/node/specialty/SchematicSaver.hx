09c0c77991396bf1e2119a7e732108ddbc9dd108
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-01 17:44:24 -0400

    Use generics properly

--- src/game/node/specialty/SchematicSaver.hx
+++ src/game/node/specialty/SchematicSaver.hx
@@ -60,7 +60,7 @@ final class SchematicSaver extends NodeDefinition {
 
 		// If the schematic has a name, use it, or else, declare it's unnamed.
 		var schematicName = Core.getMeta(pos).getString("schematic_name");
-		var formspecNameElement = (cast formspec.getElement("name_of_schematic") : FormspecLabel);
+		var formspecNameElement = (formspec.getElement("name_of_schematic") : FormspecLabel);
 		if (schematicName == "") {
 			formspecNameElement.setLabel(unnamedDefault);
 		} else {
