7cb352cbef37623b65192415d5d63fa43304bffe
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-01 17:27:53 -0400

    Add constructor to text area

--- src/engine/gui/FormspecTextArea.hx
+++ src/engine/gui/FormspecTextArea.hx
@@ -12,6 +12,19 @@ class FormspecTextArea extends FormspecElement {
 	var label: String = ""; // ? Done.
 	var defaultText: String = ""; // ? Done.
 
+	public function new(x: Float, y: Float, width: Float, height: Float, ?label: String, ?defaultText: String) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		if (label != null) {
+			this.label = label;
+		}
+		if (defaultText != null) {
+			this.defaultText = defaultText;
+		}
+	}
+
 	public function toFormspec(name: String): String {
 		return 'textarea[${this.x},${this.y};${this.width},${this.height};${name};${this.label};${this.defaultText}]';
 	}
