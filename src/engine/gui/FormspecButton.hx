024d825cb1f8071985fcd3153e76c1a0b2916cc9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-01 14:51:41 -0400

    magical

--- src/engine/gui/FormspecButton.hx
+++ src/engine/gui/FormspecButton.hx
@@ -4,8 +4,44 @@ import src.engine.gui.Formspec.FormspecElement;
 import src.engine.gui.Formspec.FormspecStyle;
 
 class FormspecButton extends FormspecElement {
+	var x: Float; // ? Done.
+	var y: Float; // ? Done.
+	var width: Float; // ? Done.
+	var height: Float; // ? Done.
+	var label: String; // ? Done.
+
+	public function new(x: Float, y: Float, width: Float, height: Float, label: String) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.label = label;
+	}
+
 	public function toFormspec(name: String): String {
-		throw new haxe.exceptions.NotImplementedException();
+		return 'button[<X>,<Y>;<W>,<H>;<name>;<label>]';
+	}
+
+	public function setStyle(style: FormspecButtonStyle): FormspecButton {
+		this.style = style;
+		return this;
+	}
+
+	public function setPos(x: Float, y: Float): FormspecButton {
+		this.x = x;
+		this.y = y;
+		return this;
+	}
+
+	public function setSize(width: Float, height: Float): FormspecButton {
+		this.width = width;
+		this.height = height;
+		return this;
+	}
+
+	public function setLabel(label: String): FormspecButton {
+		this.label = label;
+		return this;
 	}
 }
 
