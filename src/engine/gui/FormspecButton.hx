62071e5b6186d0463530cc253c8f15593227be3e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-01 15:11:40 -0400

    Update FormspecButton.hx

--- src/engine/gui/FormspecButton.hx
+++ src/engine/gui/FormspecButton.hx
@@ -2,6 +2,7 @@ package src.engine.gui;
 
 import src.engine.gui.Formspec.FormspecElement;
 import src.engine.gui.Formspec.FormspecStyle;
+import src.engine.vector.Vec2;
 
 class FormspecButton extends FormspecElement {
 	var x: Float; // ? Done.
@@ -47,6 +48,18 @@ class FormspecButton extends FormspecElement {
 
 // todo: use this on: button, button_exit, image_button, item_image_button
 class FormspecButtonStyle extends FormspecStyle {
+	var alpha: Bool; // boolean, whether to draw alpha in bgimg. Default true.
+	var bgcolor: String; // color, sets button tint.
+	var bgimg: String; // standard background image. Defaults to none.
+	var font: String; // Sets font type. This is a comma separated list of options. Valid options:
+	var font_size: Float; // Sets font size. Default is user//set. Can have multiple values:
+	var border: Bool; // boolean, draw border. Set to false to hide the bevelled button pane. Default true.
+	var content_offset: Vec2; // 2d vector, shifts the position of the button's content without resizing it.
+	var noclip: Bool; // boolean, set to true to allow the element to exceed formspec bounds.
+	var padding: Vec2; // rect, adds space between the edges of the button and the content. This value is relative to bgimg_middle.
+	var sound: String; // a sound to be played when triggered.
+	var textcolor: String; // color, default white.
+
 	public function toFormspec(name: String, windowScale: Float): String {
 		throw new haxe.exceptions.NotImplementedException();
 	}
