e84232d29a914343bc731c363690dc4369698051
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-31 20:38:36 -0400

    Update FormspecField.hx

--- src/engine/gui/FormspecField.hx
+++ src/engine/gui/FormspecField.hx
@@ -3,7 +3,23 @@ package src.engine.gui;
 import src.engine.gui.Formspec.FormspecElement;
 
 class FormspecField extends FormspecElement {
-	public function toFormspec(): String {
+	var x: Float;
+	var y: Float;
+	var width: Float;
+	var height: Float;
+	var label: String;
+	var defaultText: String = "";
+
+	public function new(x: Float, y: Float, width: Float, height: Float, label: String, defaultText: String) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.label = label;
+		this.defaultText = defaultText;
+	}
+
+	public function toFormspec(name: String): String {
 		throw new haxe.exceptions.NotImplementedException();
 	}
 }
