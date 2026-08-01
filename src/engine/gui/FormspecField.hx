ab34fa90196a5deca4cd1015a32a341d73bcacae
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-07-31 20:29:27 -0400

    Create FormspecField.hx

---
+++ src/engine/gui/FormspecField.hx
@@ -0,0 +1,9 @@
+package src.engine.gui;
+
+import src.engine.gui.Formspec.FormspecElement;
+
+class FormspecField extends FormspecElement {
+	public function toFormspec(): String {
+		throw new haxe.exceptions.NotImplementedException();
+	}
+}
