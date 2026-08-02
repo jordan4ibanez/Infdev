fd2f794eb05bde6d25a87a40d9aaa02b85fd0d3b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-02 18:25:31 -0400

    Update GameInfo.hx

--- src/engine/GameInfo.hx
+++ src/engine/GameInfo.hx
@@ -5,4 +5,5 @@ abstract class GameInfo {
 	// This is for things specifically unavailable during runtime.
 	public static final modName = Core.getCurrentModName();
 	public static final path = Core.getModPath(modName);
+	public static inline final schematicPath: String = "schematics/";
 }
