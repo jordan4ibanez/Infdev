3efa1ac37cf7fff6a98bedf8b28a82cc7175d8cb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-02 18:25:38 -0400

    That works very well!

--- src/game/decoration/trees/SmallOakTree.hx
+++ src/game/decoration/trees/SmallOakTree.hx
@@ -1,6 +1,7 @@
 package src.game.decoration.trees;
 
 import haxe.extern.EitherType;
+import src.engine.GameInfo;
 import src.engine.NoiseParams;
 import src.engine.compilercode.LuaArray;
 import src.engine.definition.Decoration.DecorationFlags;
@@ -9,9 +10,9 @@ import src.engine.definition.Decoration.DecorationSchematic;
 import src.engine.definition.Decoration.SchematicDefinition;
 
 final class SmallOakTree implements DecorationSchematic {
-	public var place_on: LuaArray<String>;
+	public var place_on: LuaArray<String> = ["infdev:grass"];
 	public var sidelen: Int;
-	public var fill_ratio: Float;
+	public var fill_ratio: Float = 0.5;
 	public var noise_params: NoiseParams;
 	public var biomes: LuaArray<String>;
 	public var y_min: Int;
@@ -19,8 +20,8 @@ final class SmallOakTree implements DecorationSchematic {
 	public var spawn_by: LuaArray<String>;
 	public var check_offset: Int;
 	public var num_spawn_by: Int;
-	public var flags: LuaArray<DecorationFlags>;
-	public var schematic: EitherType<String, SchematicDefinition>;
+	public var flags: LuaArray<DecorationFlags> = [SchematicFlagsPlaceCenterX, SchematicFlagsPlaceCenterZ];
+	public var schematic: EitherType<String, SchematicDefinition> = GameInfo.schematicPath + "small_oak_tree.mts";
 	public var replacements: Dynamic;
 	public var rotation: DecorationRotation;
 	public var place_offset_y: Int;
