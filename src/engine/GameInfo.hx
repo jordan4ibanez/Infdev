13797b17c3d18fdb23d385f389aace28e18f3976
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-03 00:57:42 -0400

    This is just art

--- src/engine/GameInfo.hx
+++ src/engine/GameInfo.hx
@@ -6,4 +6,15 @@ abstract class GameInfo {
 	public static final modName = Core.getCurrentModName();
 	public static final path = Core.getModPath(modName);
 	public static inline final schematicPath: String = "schematics/";
+
+	// This is a custom hack job to implement somewhat random noise values per world.
+	public static final mapSeed: Int = (() -> {
+		var output = 0;
+		var mapSeedString = Core.getMapSeedString();
+		for (i in 0...mapSeedString.length) {
+			output += (untyped tonumber(mapSeedString.charAt(i))) * i;
+		}
+		untyped print(output);
+		return output;
+	})();
 }
