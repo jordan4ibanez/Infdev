b4fa808f262edaf989027abf802c078c6debfcee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-03 16:04:19 -0400

    Get this mess into an actual class

--- src/engine/Core.hx
+++ src/engine/Core.hx
@@ -20,33 +20,8 @@ extern class Core {
 	static function log(level: LogLevel, text: String): Void;
 
 	// This is the real function.
-	public static function register_entity(name: String, prototype: Dynamic): Void;
-
-	// This is the hijacked function.
-	static public inline function registerEntity(name: String, clazz: Class<LuaEntity>): Void {
-		// todo: This is polluting the entire init function with locals this needs to be part of the entity duct tape!!!
-		var rawLuantiPrototype: Dynamic = {}
-		// ? Works from the current class backwards until reached root (Entity).
-		var currentClass: Class<Dynamic> = clazz;
-		while (currentClass != null) {
-			// trace("in class: " + Type.getClassName(currentClass));
-			// Class components.
-			var prototype = Reflect.field(currentClass, "prototype");
-			for (method in Reflect.fields(prototype)) {
-				untyped {
-					if (rawLuantiPrototype[method] != null) {
-						// trace("skipping method " + method + " already has it from child class");
-						continue;
-					}
-					rawLuantiPrototype[method] = Reflect.getProperty(prototype, method);
-				}
-				// trace(method);
-			}
-			// Move up the inheritance tree.
-			currentClass = Type.getSuperClass(currentClass);
-		}
-		Core.register_entity(name, rawLuantiPrototype);
-	}
+	@:allow(src.engine.compilercode.EntityDuctTape)
+	static function register_entity(name: String, prototype: Dynamic): Void;
 
 	@:native("register_node")
 	static function registerNode(name: String, def: Dynamic): Void;
--- src/engine/compilercode/EntityDuctTape.hx
+++ src/engine/compilercode/EntityDuctTape.hx
@@ -13,10 +13,31 @@ import haxe.macro.Context;
 import haxe.macro.Expr;
 
 class EntityDuctTape {
-
-	static function methodResolution(){
-		
+	function registerEntity(name: String, clazz: Class<LuaEntity>): Void {
+		var rawLuantiPrototype: Dynamic = {}
+		// ? Works from the current class backwards until reached root (Entity).
+		var currentClass: Class<Dynamic> = clazz;
+		while (currentClass != null) {
+			// trace("in class: " + Type.getClassName(currentClass));
+			// Class components.
+			var prototype = Reflect.field(currentClass, "prototype");
+			for (method in Reflect.fields(prototype)) {
+				untyped {
+					if (rawLuantiPrototype[method] != null) {
+						// trace("skipping method " + method + " already has it from child class");
+						continue;
+					}
+					rawLuantiPrototype[method] = Reflect.getProperty(prototype, method);
+				}
+				// trace(method);
+			}
+			// Move up the inheritance tree.
+			currentClass = Type.getSuperClass(currentClass);
+		}
+		Core.register_entity(name, rawLuantiPrototype);
 	}
+
+	static function methodResolution() {}
 
 	public static function build(): Array<Field> {
 		var fields = Context.getBuildFields();
