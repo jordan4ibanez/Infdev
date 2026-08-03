e891ca42c1163908b72d122b278e016c6e9ed0dd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-03 12:40:01 -0400

    Create OracleShears.hx

---
+++ src/game/tool/OracleShears.hx
@@ -0,0 +1,21 @@
+package src.game.tool;
+
+import src.engine.definition.NodeDefinition.MAX_NODE_LEVEL;
+import src.engine.definition.ToolDefinition;
+import src.engine.definition.basic.ToolCapabilities;
+
+@:register("infdev:oracle_shears")
+final class OracleShears extends ToolDefinition {
+	public function new() {
+		super();
+
+		this.description = "Oracle Shears";
+
+		this.inventoryImage = "infdev_oracle_shears.png";
+		this.wieldImage = "infdev_oracle_shears.png";
+
+		this.toolCapabilities = new ToolCapabilities()
+			.setMaxDropLevel(0)
+			.addGroupCap(NodeGroupStone, new GroupCapabilities(0.1, 0.005, MAX_NODE_LEVEL, 1_000_000));
+	}
+}
