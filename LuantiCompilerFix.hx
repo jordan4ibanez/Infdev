abdc00ee4532bc41a42d837bb32a22b61af02e35
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-04 02:52:50 -0400

    Bolt on a compiler level insecure environment request

--- LuantiCompilerFix.hx
+++ LuantiCompilerFix.hx
@@ -5,6 +5,8 @@ import sys.io.File;
 using StringTools;
 
 class LuantiCompilerFix {
+	static final DEBUG_MODE = false;
+
 	public static function patch(fileName: String) {
 		Context.onAfterGenerate(() -> {
 			// This can lock up the Haxe language server so just completely ignore this thing failing to patch it if the file doesn't exist.
@@ -13,10 +15,15 @@ class LuantiCompilerFix {
 				var content: String = File.getContent(path);
 				var outputLines: Array<String> = [];
 
-				for (currentLine in content.split("\n")) {
+				for (i => currentLine in content.split("\n")) {
 					// Hard code package.loaded.luv to short circuit into _hx_luv define.
 					if (currentLine.contains("package.loaded.luv")) {
 						currentLine = 'if false then';
+					}
+
+					if (DEBUG_MODE && i == 0 && fileName == "init.lua") {
+						currentLine = "local compilerDebugUnsafeEnvironment = core.request_insecure_environment();";
+						trace(currentLine);
 					}
 
 					// If this line is blank, it doesn't get added to the output.
