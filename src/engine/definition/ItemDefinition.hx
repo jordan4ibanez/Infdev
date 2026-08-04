7fface8dd4ece8eb0cd2972848a8b1e1c3b6647b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-04 00:36:07 -0400

    Update ItemDefinition.hx

--- src/engine/definition/ItemDefinition.hx
+++ src/engine/definition/ItemDefinition.hx
@@ -165,6 +165,7 @@ class ItemDefinition {
 	// Only one because I ain't trying to figure out multiple fuel recipes.
 	public var recipeFuel: CraftRecipeFuel;
 
+	@:noCompletion
 	public static function registerCraft(instance: ItemDefinition, registrationName: String): Void {
 		// ! Shaped.
 		if (instance.recipesShaped != null) {
