7a930026e9b4a932fbeb9bca4171f984ca94a84f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-04 23:26:10 -0400

    Update LargeOakTree.hx

---
+++ src/game/decoration/trees/oak/LargeOakTree.hx
@@ -0,0 +1,37 @@
+package src.game.decoration.trees.oak;
+
+import haxe.extern.EitherType;
+import src.engine.GameInfo;
+import src.engine.NoiseParams;
+import src.engine.compilercode.LuaArray;
+import src.engine.definition.Decoration.DecorationFlags;
+import src.engine.definition.Decoration.DecorationRotation;
+import src.engine.definition.Decoration.DecorationSchematic;
+import src.engine.definition.Decoration.SchematicDefinition;
+import src.engine.vector.Vec3;
+
+final class LargeOakTree implements DecorationSchematic {
+	public var place_on: LuaArray<String> = ["infdev:grass"];
+	public var sidelen: Int;
+	public var fill_ratio: Float;
+	public var noise_params: NoiseParams = {
+		offset: 0.0001,
+		scale: 0.000005,
+		spread: new Vec3(250, 250, 250),
+		octaves: 3,
+		lacunarity: 3.0,
+		seed: GameInfo.getMapSeed() + 48,
+		persistence: 1,
+	};
+	public var biomes: LuaArray<String>;
+	public var y_min: Int;
+	public var y_max: Int;
+	public var spawn_by: LuaArray<String>;
+	public var check_offset: Int;
+	public var num_spawn_by: Int;
+	public var flags: LuaArray<DecorationFlags> = [SchematicFlagsPlaceCenterX, SchematicFlagsPlaceCenterZ];
+	public var schematic: EitherType<String, SchematicDefinition> = GameInfo.schematicPath + "large_oak_tree.mts";
+	public var replacements: Dynamic;
+	public var rotation: DecorationRotation;
+	public var place_offset_y: Int = 1;
+}
