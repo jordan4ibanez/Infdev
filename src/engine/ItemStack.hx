03e537bae468ad91c958ff8458826ac577c1f051
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-05 15:09:11 -0400

    Type this properly

--- src/engine/ItemStack.hx
+++ src/engine/ItemStack.hx
@@ -1,11 +1,13 @@
 package src.engine;
 
+import haxe.extern.EitherType;
+
 // todo: getters
 // todo: rework this entire thing this is just a bootstrap
 // https://github.com/luanti-org/luanti/blob/master/doc/lua_api.md#itemstack
 @:final
 abstract class ItemStack {
-	public static inline function create(itemName: String): ItemStack {
+	public static inline function create(itemName: EitherType<String, ItemStack>): ItemStack {
 		return untyped __lua__("ItemStack(itemName)");
 	}
 
