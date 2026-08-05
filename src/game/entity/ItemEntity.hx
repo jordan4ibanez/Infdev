1e7b3ff7d029ae0c3cbf06ad9dbf9397edf8cc67
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-05 15:13:20 -0400

    Update ItemEntity.hx

--- src/game/entity/ItemEntity.hx
+++ src/game/entity/ItemEntity.hx
@@ -8,7 +8,7 @@ class ItemEntity extends LuaEntity {
 	@:native("set_item")
 	// todo: make a custom core.spawn_item so this doesn't need a native tag.
 	// todo: also this seems to be willy nilly with the string or ItemStack so fix that.
-	function setItem(itemName: String): Void {
+	public function setItem(itemName: String): Void {
 		var stack = ItemStack.create(itemName);
 		untyped print(stack.getName());
 		// untyped print(newItem.getName());
