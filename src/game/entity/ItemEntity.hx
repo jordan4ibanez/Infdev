b0bfa2e2e572be403b9f1535c4924861f9510e24
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-05 15:22:18 -0400

    There is already spaghetti code in the engine itself

--- src/game/entity/ItemEntity.hx
+++ src/game/entity/ItemEntity.hx
@@ -6,11 +6,9 @@ import src.engine.entity.LuaEntity;
 @:register(":__builtin:item")
 class ItemEntity extends LuaEntity {
 	@:native("set_item")
-	// todo: make a custom core.spawn_item so this doesn't need a native tag.
-	// todo: also this seems to be willy nilly with the string or ItemStack so fix that.
-	public function setItem(itemName: String): Void {
-		var stack = ItemStack.create(itemName);
-		untyped print(stack.getName());
+	public function setItem(item: String): Void {
+		var stack = ItemStack.create(item);
+		untyped print(stack.getName(), stack.getCount());
 		// untyped print(newItem.getName());
 	}
 }
