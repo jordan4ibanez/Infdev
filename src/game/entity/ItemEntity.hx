567e5d70ee938dc6c76cdbb71e88e2de37c3b8a0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-05 02:19:14 -0400

    This works great!

--- src/game/entity/ItemEntity.hx
+++ src/game/entity/ItemEntity.hx
@@ -1,7 +1,16 @@
 package src.game.entity;
 
+import src.engine.ItemStack;
 import src.engine.entity.LuaEntity;
 
+@:register(":__builtin:item")
 class ItemEntity extends LuaEntity {
-    
+	@:native("set_item")
+	// todo: make a custom core.spawn_item so this doesn't need a native tag.
+	// todo: also this seems to be willy nilly with the string or ItemStack so fix that.
+	function setItem(itemName: String): Void {
+		var stack = ItemStack.create(itemName);
+		untyped print(stack.getName());
+		// untyped print(newItem.getName());
+	}
 }
