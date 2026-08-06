017981ca6e7aa287f1f8c777355f7c7ce001e49c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-05 20:57:48 -0400

    It is time to get super cereal

--- src/game/entity/ItemEntity.hx
+++ src/game/entity/ItemEntity.hx
@@ -8,11 +8,19 @@ import src.engine.vector.Vec2;
 
 @:register(":__builtin:item")
 class ItemEntity extends LuaEntity {
+	var itemstring = "";
+	var moving_state = true;
+	var physical_state = true;
+	// Item expiry.
+	var age = 0;
+	// Pushing item out of solid nodes.
+	var force_out = null;
+	var force_out_start = null;
+
 	@:native("set_item")
 	public function setItem(item: String): Void {
 		var stack = ItemStack.create(item);
-		untyped print(stack.getName(), stack.getCount());
-		// untyped print(newItem.getName());
+		
 	}
 
 	override function onActivate(staticData: String, dtimeS: Float) {
