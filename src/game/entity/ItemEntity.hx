8453c3b1488dd48d63846402754e32c52e046a01
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-05 20:54:55 -0400

    HMMM

--- src/game/entity/ItemEntity.hx
+++ src/game/entity/ItemEntity.hx
@@ -3,6 +3,8 @@ package src.game.entity;
 import src.engine.ItemStack;
 import src.engine.compilercode.Macros;
 import src.engine.entity.LuaEntity;
+import src.engine.entity.definition.EntityCollisionBox;
+import src.engine.vector.Vec2;
 
 @:register(":__builtin:item")
 class ItemEntity extends LuaEntity {
@@ -16,5 +18,16 @@ class ItemEntity extends LuaEntity {
 	override function onActivate(staticData: String, dtimeS: Float) {
 		Macros.entityPatch();
 		super.onActivate(staticData, dtimeS);
+
+		this.object.setProperties({
+			hp_max: 1,
+			physical: true,
+			collide_with_objects: false,
+			collisionbox: new EntityCollisionBox(-0.3, -0.3, -0.3, 0.3, 0.3, 0.3),
+			visual: EntityVisualWieldItem,
+			visual_size: new Vec2(0.4, 0.4),
+			textures: [""],
+			is_visible: false,
+		});
 	}
 }
