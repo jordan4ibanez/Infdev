84b6024a7d67b54878d1839b7b259a18dc720eed
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-05 23:34:37 -0400

    Update EntityCollisionBox.hx

--- src/engine/entity/definition/EntityCollisionBox.hx
+++ src/engine/entity/definition/EntityCollisionBox.hx
@@ -7,4 +7,9 @@ abstract EntityCollisionBox(Table<Int, Float>) from Table<Int, Float> to Table<I
 	public inline function new(x1: Float, y1: Float, z1: Float, x2: Float, y2: Float, z2: Float) {
 		this = Table.create([x1, y1, z1, x2, y2, z2]);
 	}
+
+	@:arrayAccess
+	inline function get(index: Int): Float {
+		return untyped this[index + 1];
+	}
 }
