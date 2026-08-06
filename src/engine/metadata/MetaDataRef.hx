e92ff1a36f7c7772d7b2c2d41be19911aeb27118
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-05 22:15:11 -0400

    Oops

--- src/engine/metadata/MetaDataRef.hx
+++ src/engine/metadata/MetaDataRef.hx
@@ -3,7 +3,6 @@ package src.engine.metadata;
 import lua.Table;
 import src.engine.compilercode.LuaArray;
 
-@:final
 abstract class MetaDataRef {
 	@:native("contains")
 	public abstract function contains(key: String): Bool;
