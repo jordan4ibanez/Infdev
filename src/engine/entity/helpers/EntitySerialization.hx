029d747613aff8569658a92d5b52b6700853201c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-06 20:40:58 -0400

    I think this is a great copout

--- src/engine/entity/helpers/EntitySerialization.hx
+++ src/engine/entity/helpers/EntitySerialization.hx
@@ -5,6 +5,7 @@ import src.engine.Core;
 final class EntitySerialization {
 	/**
 	 * This was made to clone core.deserialize data into a haxe class with defaults to make it nice as possible.
+	 * If the class has userdata in it, it must be set to null or else this will crash.
 	 * @param staticData The static data.
 	 * @param cloneTo The object you want to write this data into.
 	 * @param clazz The class type of outputObject.
@@ -47,6 +48,13 @@ final class EntitySerialization {
 		}
 	}
 
+	/**
+	 * This was made to clone core.serialize data into a haxe class with defaults to make it nice as possible.
+	 * If the class has userdata in it, it must be set to null or else this will crash.
+	 * @param inputObject The object you want to turn into a string.
+	 * @param clazz The class type of inputObject.
+	 * @return String The serialized data string.
+	 */
 	public static function safeSerialize<T>(inputObject: Dynamic, clazz: Class<T>): String {
 		// trace(Type.getInstanceFields(clazz), Type.getClassName(clazz));
 
