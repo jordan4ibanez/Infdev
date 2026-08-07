2a3317d294a6d15a3c4e7c931174b42b139bf47d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-06 23:48:49 -0400

    Update PlayerInventoryFormspec.hx

--- src/game/entity/player/PlayerInventoryFormspec.hx
+++ src/game/entity/player/PlayerInventoryFormspec.hx
@@ -1,12 +1,13 @@
 package src.game.entity.player;
 
 import src.engine.entity.objectref.ObjectRefPlayer;
+import src.engine.gui.Formspec;
 
 // This will actually save what tab you're on between logins.
 final class PlayerInventoryFormspec {
 	var playerObject: ObjectRefPlayer;
 
-	static var formspec: Formspec = new Formspec();
+	static var formspec: Formspec = new Formspec("player_inventory");
 
 	public function new(playerObject: ObjectRefPlayer) {
 		this.playerObject = playerObject;
@@ -17,6 +18,6 @@ final class PlayerInventoryFormspec {
 	}
 
 	public function serialize(): String {
-		return formspec;
+		return formspec.serialize(this.playerObject);
 	}
 }
