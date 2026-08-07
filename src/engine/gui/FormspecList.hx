82be47adc779623dccf26224ace23f407606dc72
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-07 18:46:49 -0400

    Update FormspecList.hx

--- src/engine/gui/FormspecList.hx
+++ src/engine/gui/FormspecList.hx
@@ -3,10 +3,22 @@ package src.engine.gui;
 import src.engine.gui.Formspec.FormspecElement;
 
 class FormspecList extends FormspecElement {
+	var inventoryLocation: String;
+	var listName: String;
 	var x: Float; // ? Done.
 	var y: Float; // ? Done.
 	var width: Float; // ? Done.
 	var height: Float; // ? Done.
+
+	public function new(x: Float, y: Float, width: Float, height: Float) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+
+        // todo: fixme
+		// this.style = new FormspecLabelStyle();
+	}
 
 	public function toFormspec(name: String): String {
 		return 'list[<inventory location>;<list name>;${this.x},${this.y};${this.width},${this.height};<starting item index>]';
