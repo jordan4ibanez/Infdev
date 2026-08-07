aed03a2d98338706d8d8c8feae457693c76da400
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-07 01:12:19 -0400

    Hook it into the engine

--- src/engine/entity/helpers/PlayerHandling.hx
+++ src/engine/entity/helpers/PlayerHandling.hx
@@ -73,6 +73,10 @@ final class PlayerHandling {
 		Core.registerOnRespawnPlayer((player) -> {
 			return player.getPlayerLuaEntity().onRespawn();
 		});
+
+		Core.registerOnPlayerReceiveFields((player, formName, fields) -> {
+			player.getPlayerLuaEntity().onReceiveFields(formName, fields);
+		});
 	}
 
 	private static function mimicLuaEntityConstruction(name: String, player: Player) {
