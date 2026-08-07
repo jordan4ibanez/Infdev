73b152b38331b5a024ed03aacff41782f26550bc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-07 02:00:00 -0400

    And it's functioning

--- src/game/entity/player/PlayerWindowSizeWatcher.hx
+++ src/game/entity/player/PlayerWindowSizeWatcher.hx
@@ -5,14 +5,30 @@ import src.engine.vector.Vec2;
 
 final class PlayerWindowSizeWatcher {
 	var playerObject: ObjectRefPlayer;
-	var size: Vec2;
+	var size: Vec2 = new Vec2(0, 0);
+	var timer: Float = 0.0;
+
+	// Only update every second because how often is the player going to be scaling the window.
+	static inline final UPDATE_INTERVAL = 1.0;
 
 	public function new(playerObject: ObjectRefPlayer) {
 		this.playerObject = playerObject;
-		this.size = playerObject.getPlayerLuaEntity().getWindowInformation().size;
 	}
 
 	public function terminate(): Void {
 		this.playerObject = null;
+	}
+
+	public function update(delta: Float): Void {
+		this.timer += delta;
+		if (this.timer < UPDATE_INTERVAL) {
+			return;
+		}
+		this.timer -= UPDATE_INTERVAL;
+		var newSize = playerObject.getPlayerLuaEntity().getWindowInformation().size;
+		if (this.size != newSize) {
+			this.playerObject.getPlayerLuaEntity().onWindowSizeChange();
+		}
+		this.size = newSize;
 	}
 }
