713193fd7245b2322ddaf736b6a39fc485b9bb00
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-06 21:27:13 -0400

    Mix and match sounds

--- src/game/node/plant/PlantSound.hx
+++ src/game/node/plant/PlantSound.hx
@@ -8,13 +8,16 @@ abstract class PlantSound {
 	public static function get(): NodeSoundTable {
 		return new NodeSoundTable()
 			.setDig(new SimpleSoundSpecTable("plant_mine")
+				.setPitch(1.0)
+				.setGain(0.5))
+			.setDug(new SimpleSoundSpecTable("dirt_mine")
+				.setPitch(1.1)
+				.setGain(0.5))
+			.setFootstep(new SimpleSoundSpecTable("plant_mine")
+				.setGain(0.4)
 				.setPitch(0.9))
-			.setDug(new SimpleSoundSpecTable("plant_mine")
-				.setPitch(0.75))
-			.setFootstep(new SimpleSoundSpecTable("plant_step")
-				.setPitch(0.8))
 			.setPlace(new SimpleSoundSpecTable("plant_mine")
-				.setPitch(0.65))
+				.setPitch(1.0))
 			.setPlaceFailed(new SimpleSoundSpecTable("plant_mine")
 				.setPitch(0.4));
 	}
