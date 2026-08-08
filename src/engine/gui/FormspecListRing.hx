6ff0ef8df9f55670b6e0b9855f251295ec3dd18b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-07 21:01:42 -0400

    Create FormspecListRing.hx

---
+++ src/engine/gui/FormspecListRing.hx
@@ -0,0 +1,17 @@
+package src.engine.gui;
+
+import src.engine.gui.Formspec.FormspecElement;
+
+class FormspecListRing extends FormspecElement {
+	var inventoryLocation: String;
+	var listName: String;
+
+	public function new(inventoryLocation: String, listName: String) {
+		this.inventoryLocation = inventoryLocation;
+		this.listName = listName;
+	}
+
+	public function toFormspec(name: String): String {
+		return 'listring[${this.inventoryLocation};${this.listName}]';
+	}
+}
