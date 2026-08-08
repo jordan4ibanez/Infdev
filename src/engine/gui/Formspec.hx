71afca15795bcaa07f20bba2ab9081805c0d3226
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-07 20:20:31 -0400

    Set the default formspec list style to a very boring color scheme

--- src/engine/gui/Formspec.hx
+++ src/engine/gui/Formspec.hx
@@ -86,6 +86,8 @@ class Formspec {
 		append('size[${this.size.x},${this.size.y},${this.fixedSize}]');
 		append('bgcolor[#00000000;false;]');
 		append('background9[0,0;0,0;infdev_menu_background.png;true;40]');
+		append('style_type[list;spacing=0.1,size=1,slotbg_image=infdev_menu_background.png]');
+		append('listcolors[#636363;#545454;black;#141414;#ffff00]');
 
 		var windowScale = getTrueWindowScale(player);
 
