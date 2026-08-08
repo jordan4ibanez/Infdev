a52905a2c0562c17f1b01f78088fb57ead660f5a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-07 23:31:18 -0400

    Update PlayerInventoryFormspec.hx

--- src/game/entity/player/PlayerInventoryFormspec.hx
+++ src/game/entity/player/PlayerInventoryFormspec.hx
@@ -7,7 +7,7 @@ import src.engine.gui.FormspecList;
 
 // This will actually save what tab you're on between logins.
 final class PlayerInventoryFormspec {
-	var playerObject: ObjectRefPlayer;
+	var player: ObjectRefPlayer;
 
 	// This is REALLY, REALLY memory inefficient but I can't run a function when
 	// the player opens their inventory.
@@ -16,20 +16,21 @@ final class PlayerInventoryFormspec {
 		.addElement("inventory", "hot_bar", new FormspecList("current_player", "main", 0.09, 5.7, 12, 1)) // Hot bar.
 		.addElement("inventory", "main_inventory", new FormspecList("current_player", "main", 0.09, 6.6304, 12, 7, 12)); // Rest of inventory.
 
-	public function new(playerObject: ObjectRefPlayer) {
-		this.playerObject = playerObject;
+	public function new(player: ObjectRefPlayer) {
+		this.player = player;
+		this.formspec.setPlayer(player);
 	}
 
 	public function terminate(): Void {
-		this.playerObject = null;
+		this.player = null;
 	}
 
 	public function serialize(): String {
-		return formspec.serialize(this.playerObject);
+		return formspec.serialize();
 	}
 
 	public function updateScaling(): Void {
-		playerObject.setInventoryFormspec(this.serialize());
+		player.setInventoryFormspec(this.serialize());
 	}
 
 	public function process(fields: Table<String, String>): Void {
