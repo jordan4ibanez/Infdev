2472c0b610131d3a91e678881340adf691844a88
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-10 16:28:16 -0400

    Update FormspecDropDown.hx

--- src/engine/gui/FormspecDropDown.hx
+++ src/engine/gui/FormspecDropDown.hx
@@ -1,2 +1,11 @@
 package src.engine.gui;
 
+import src.engine.gui.Formspec.FormspecElement;
+
+class FormspecDropDown extends FormspecElement {
+	public function new() {}
+
+	public function toFormspec(name: String): String {
+		throw new haxe.exceptions.NotImplementedException();
+	}
+}
