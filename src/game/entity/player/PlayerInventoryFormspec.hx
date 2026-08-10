c1f39a6482b97098f154fee79c1ed7eae5e6192d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-09 21:41:24 -0400

    It is time to make this even nicer

--- src/game/entity/player/PlayerInventoryFormspec.hx
+++ src/game/entity/player/PlayerInventoryFormspec.hx
@@ -12,10 +12,14 @@ final class PlayerInventoryFormspec {
 	// This is REALLY, REALLY memory inefficient but I can't run a function when
 	// the player opens their inventory.
 	// todo: The main inventory list will need to expand sideways when a player levels up.
-	var formspec: Formspec = new Formspec("", "inventory")
-		.addElement("inventory", "hot_bar", new FormspecList("current_player", "main", 0.09, 5.7, 12, 1)) // Hot bar.
-		.addElement("inventory", "main_inventory", new FormspecList("current_player", "main", 0.09, 6.6304, 12, 7, 12)) // Rest of inventory.
-			;
+	var formspec: Formspec = (() -> {
+		var f = new Formspec("", "inventory");
+		// Hot bar.
+		f.addElement("inventory", "hot_bar", new FormspecList("current_player", "main", 0.09, 5.7, 12, 1));
+		// Rest of inventory.
+		f.addElement("inventory", "main_inventory", new FormspecList("current_player", "main", 0.09, 6.6304, 12, 7, 12));
+		return f;
+	})();
 
 	public function new(player: ObjectRefPlayer) {
 		this.player = player;
