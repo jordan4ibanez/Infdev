15101a571072768f387578df541ec7541db3e67f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-10 18:16:24 -0400

    Update FormspecDropDown.hx

--- src/engine/gui/FormspecDropDown.hx
+++ src/engine/gui/FormspecDropDown.hx
@@ -1,5 +1,6 @@
 package src.engine.gui;
 
+import haxe.Rest;
 import src.engine.gui.Formspec.FormspecElement;
 
 class FormspecDropDown extends FormspecElement {
@@ -8,10 +9,34 @@ class FormspecDropDown extends FormspecElement {
 	var width: Float; // ? Done.
 	var height: Float; // ? Done.
 
-	public function new() {}
+	// Items are a fixed size because I don't even want to think about making
+	// tabs dynamic. That sounds horrifying.
+	var items: String = "";
+
+	public function new(x: Float, y: Float, width: Float, height: Float, firstItem: String, restOfItems: Rest<String>) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+
+		var length = restOfItems.length;
+
+		this.items = firstItem;
+		if (length != 0) {
+			this.items += ",";
+		}
+
+		for (index => item in restOfItems) {
+			this.items += item;
+			// This may be a mess but I want it to be a nice mess.
+			if (index + 1 < length) {
+				this.items += ",";
+			}
+		}
+	}
 
 	public function toFormspec(name: String): String {
-		return 'dropdown[${this.x},${this.y};${this.width},${this.height};${name};<item 1>,<item 2>, ...,<item n>;<selected idx>;<index event>]';
+		return 'dropdown[${this.x},${this.y};${this.width},${this.height};${name};${this.items};<selected idx>;<index event>]';
 	}
 
 	public function setPos(x: Float, y: Float): FormspecDropDown {
