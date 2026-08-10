bf571e2d5a81a984c843adecc8f888723b059f96
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-09 23:48:22 -0400

    Create a duplicate for text testing

--- src/game/entity/player/PlayerInventoryFormspec.hx
+++ src/game/entity/player/PlayerInventoryFormspec.hx
@@ -4,6 +4,7 @@ import lua.Lua;
 import lua.Table;
 import src.engine.entity.objectref.ObjectRefPlayer;
 import src.engine.gui.Formspec;
+import src.engine.gui.FormspecLabel;
 import src.engine.gui.FormspecList;
 import src.engine.gui.FormspecTabHeader;
 
@@ -56,6 +57,17 @@ final class PlayerInventoryFormspec {
 			tabs[3].name,
 			tabs[4].name,
 			tabs[5].name).setAction(tabNavigationAction));
+		// todo: remove this duplicate.
+		f.addRootElement("debug", new FormspecTabHeader(
+			0.09, 0.6,
+			9.82, 0.5,
+			true,
+			tabs[0].name,
+			tabs[1].name,
+			tabs[2].name,
+			tabs[3].name,
+			tabs[4].name,
+			tabs[5].name));
 		// todo: this should check the player for a level to decide how wide it is.
 		// todo: that's gonna be complicated
 		//
