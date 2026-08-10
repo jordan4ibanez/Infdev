4c88eef1b4eb5112a982e62efa90614d50c82211
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-09 21:01:11 -0400

    Now make this nicer and immensely faster

--- LuantiCompilerFix.hx
+++ LuantiCompilerFix.hx
@@ -1,3 +1,4 @@
+import haxe.Timer;
 #if macro
 import haxe.macro.Context;
 import sys.io.File;
@@ -5,12 +6,27 @@ import sys.io.File;
 using StringTools;
 
 class LuantiCompilerFix {
+	// This is for testing very advanced features of the engine built on top of the luanti engine.
 	static final DEBUG_MODE = false;
+	// This is for removing thousands of lines of code out of the lua code.
+	static final RELEASE_COMPRESSION = false;
 
 	public static function patch(fileName: String) {
 		Context.onAfterGenerate(() -> {
 			// This can lock up the Haxe language server so just completely ignore this thing failing to patch it if the file doesn't exist.
 			final path = "mods/infdev/" + fileName;
+
+			trace('${fileName} post-compile processing time:');
+			Timer.measure(() -> {
+				if (!RELEASE_COMPRESSION) {
+					// This is an order of magnitude faster than release compression.
+					// The string is HUGE, so do not expect miracles.
+					try {
+						File.saveContent(path, StringTools.replace(File.getContent(path), "package.loaded.luv", "false"));
+					} catch (doNotCare) {
+						trace(path + " doesn't exist. Failed to patch.");
+					}
+				} else {
 			try {
 				var content: String = File.getContent(path);
 				var outputLines: Array<String> = [];
@@ -36,6 +52,8 @@ class LuantiCompilerFix {
 			} catch (doNotCare) {
 				trace(path + " doesn't exist. Failed to patch.");
 			}
+			}
+			});
 		});
 	}
 }
