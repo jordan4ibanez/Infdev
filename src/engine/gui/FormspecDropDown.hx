02f91a96cc735f3a715ed88d17ca3bcf9e4aa8fd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-10 16:29:01 -0400

    Update FormspecDropDown.hx

--- src/engine/gui/FormspecDropDown.hx
+++ src/engine/gui/FormspecDropDown.hx
@@ -3,9 +3,26 @@ package src.engine.gui;
 import src.engine.gui.Formspec.FormspecElement;
 
 class FormspecDropDown extends FormspecElement {
+	var x: Float; // ? Done.
+	var y: Float; // ? Done.
+	var width: Float; // ? Done.
+	var height: Float; // ? Done.
+
 	public function new() {}
 
 	public function toFormspec(name: String): String {
 		return 'dropdown[<X>,<Y>;<W>;<name>;<item 1>,<item 2>, ...,<item n>;<selected idx>;<index event>]';
+	}
+
+	public function setPos(x: Float, y: Float): FormspecDropDown {
+		this.x = x;
+		this.y = y;
+		return this;
+	}
+
+	public function setSize(width: Float, height: Float): FormspecDropDown {
+		this.width = width;
+		this.height = height;
+		return this;
 	}
 }
