7582cc24db0ef6e5d2d6b1139507ffa49c7c2de6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-09 22:04:41 -0400

    A debug test

--- src/engine/gui/FormspecTabHeader.hx
+++ src/engine/gui/FormspecTabHeader.hx
@@ -1,2 +1,10 @@
 package src.engine.gui;
 
+import src.engine.gui.Formspec.FormspecElement;
+
+class FormspecTabHeader extends FormspecElement {
+	public function toFormspec(name: String): String {
+		// return 'tabheader[<X>,<Y>;<W>,<H>;<name>;<caption 1>,<caption 2>,...,<caption n>;<current_tab>;<transparent>;<draw_border>]';
+        return 'tabheader[0,0;2,1;navigation;this,is,a,test;1;false;true]';
+	}
+}
