f76deca4a66e6e5eb3ba8f3dfd72bf07d2628145
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-12 17:36:03 -0400

    Create CheckBox.hx

---
+++ src/engine/gui/CheckBox.hx
@@ -0,0 +1,11 @@
+package src.engine.gui;
+
+import src.engine.gui.Gui.FormspecElement;
+
+class CheckBox extends FormspecElement {
+    
+
+    public function toFormspec(name:String):String {
+        throw new haxe.exceptions.NotImplementedException();
+    }
+}
