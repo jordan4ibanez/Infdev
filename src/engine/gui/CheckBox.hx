a64a1fd09ef33097f640d6020330a385019e6a0b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-12 17:43:20 -0400

    Update CheckBox.hx

--- src/engine/gui/CheckBox.hx
+++ src/engine/gui/CheckBox.hx
@@ -2,6 +2,19 @@ package src.engine.gui;
 
 import src.engine.gui.Gui.GuiElement;
 
+/*
+This is fucking horrible.
+For customization, as of 12/8/26 there is no:
+- font selection
+- font color
+- font size
+- checkbox button texture
+
+So before anyone complains about this implementation looking bland they can read this and go fucking
+look at the fucking documentation. No custom fucking checkbox will be implemented.
+This should be out of the box customizable and no fucking additional hackjobs should 
+need to be implemented for a fucking checkbox.
+*/
 class CheckBox extends GuiElement {
 	public function toFormspec(name: String): String {
 		throw new haxe.exceptions.NotImplementedException();
