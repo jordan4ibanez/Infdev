bc0f8e50f950a514bf6ee341585ce21b3b909a9e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-14 15:13:14 -0400

    Update LuaEntity.hx

--- src/engine/entity/LuaEntity.hx
+++ src/engine/entity/LuaEntity.hx
@@ -14,7 +14,9 @@ inline final MAX_ENTITY_LEVEL = MAX_LEVEL;
 @:build(src.engine.compilercode.EntityDuctTape.build())
 abstract class LuaEntity {
 	final object: ObjectRefEntity = null;
+
 	public final name: String = null;
+
 	var shadowEnabled = true;
 	var shadowEntity: Null<ObjectRefEntity> = null;
 
