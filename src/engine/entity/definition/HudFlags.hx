7dd52308598c59fd25bd904a854866e8189616be
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-08-15 23:59:25 -0400

    Update HudFlags.hx

--- src/engine/entity/definition/HudFlags.hx
+++ src/engine/entity/definition/HudFlags.hx
@@ -1,13 +1,22 @@
 package src.engine.entity.definition;
 
 typedef HudFlags = {
+	@:optional
 	var hotbar: Bool;
+	@:optional
 	var healthbar: Bool;
+	@:optional
 	var crosshair: Bool;
+	@:optional
 	var wielditem: Bool;
+	@:optional
 	var breathbar: Bool;
+	@:optional
 	var minimap: Bool;
+	@:optional
 	var minimap_radar: Bool;
+	@:optional
 	var basic_debug: Bool;
+	@:optional
 	var chat: Bool;
 }
